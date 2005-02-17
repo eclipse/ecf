cfe973a1881c491187c92ef24194ef98bfe6f170
slewis <slewis>
2005-02-17 01:06:09 +0000

    Moved IMessageViewer interface to new package.  Added Type inner class to IMessageViewer

--- framework/bundles/org.eclipse.ecf.ui/src/org/eclipse/ecf/ui/IMessageViewer.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.ecf.ui;
-
-import org.eclipse.ecf.core.identity.ID;
-
-public interface IMessageViewer {
-    
-    public void showMessage(ID fromID, ID toID, String message);
-}
