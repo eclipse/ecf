55719a108913b6ad6b622c22e6c0f87551dc626c
slewis <slewis>
2005-05-16 06:46:33 +0000

    Added access methods to IDiscoveryContainer

--- framework/bundles/org.eclipse.ecf.discovery/src/org/eclipse/ecf/discovery/IDiscoveryContainer.java
+++ framework/bundles/org.eclipse.ecf.discovery/src/org/eclipse/ecf/discovery/IDiscoveryContainer.java
@@ -18,7 +18,9 @@ public interface IDiscoveryContainer {
 	public void removeServiceListener(ServiceID type, IServiceListener listener);
 	public void addServiceTypeListener(IServiceTypeListener listener);
 	public void removeServiceTypeListener(IServiceTypeListener listener);
+	public void registerServiceType(ServiceID serviceType);
 	public void registerService(IServiceInfo serviceInfo) throws IOException;
+	public IServiceInfo getServiceInfo(ServiceID service, int timeout);
 	public void unregisterService(IServiceInfo serviceInfo);
 	public void unregisterAllServices();
 	public IServiceInfo [] getServices(ServiceID type);
