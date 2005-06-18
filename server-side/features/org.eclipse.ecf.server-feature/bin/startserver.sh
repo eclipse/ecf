116ecd8994a5b2ac2377155fb9fda0c7355dd381
slewis <slewis>
2005-06-18 22:17:47 +0000

    Updated versions from 0.3.0 to 0.3.1

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
@@ -2,17 +2,17 @@
 #
 # Relative path for accessing plugins
 RP=../../../plugins
-ECF=${RP}/org.eclipse.ecf_0.3.0/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.3.0/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.3.0/ecf.sdo.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.3.0/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.3.0/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.3.0/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.3.0/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.3.0/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.3.0/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.3.0/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.3.0/client.jar
+ECF=${RP}/org.eclipse.ecf_0.3.1/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.3.1/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.3.1/ecf.sdo.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.3.1/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.3.1/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.3.1/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.3.1/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.3.1/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.3.1/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.3.1/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.3.1/client.jar
 
 CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${HELLO}:${DISCOVERY}:${COLLAB}:."
 
