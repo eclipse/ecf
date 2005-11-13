76c5da22ff4f957a9bf405496fa9d18490ec9281
slewis <slewis>
2005-11-13 21:17:51 +0000

    Updates to version 0.5.1...plugins, features, and update sites

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
@@ -4,18 +4,18 @@ RP=../../../plugins
 CURRENT=`pwd`
 cd ${CURRENT}
 # Relative path for accessing plugins
-ECF=${RP}/org.eclipse.ecf_0.4.0/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.4.0/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.4.0/ecf.sdo.jar
-DS=${RP}/org.eclipse.ecf.datashare_0.4.0/datashare.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.4.0/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.4.0/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.4.0/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.4.0/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.4.0/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.4.0/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.4.0/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.4.0/client.jar
+ECF=${RP}/org.eclipse.ecf_0.5.1/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.5.1/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.5.1/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.5.1/datashare.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.5.1/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.5.1/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.5.1/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.5.1/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.5.1/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.5.1/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.5.1/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.5.1/client.jar
 
 CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${HELLO}:${DISCOVERY}:${COLLAB}:."
 
