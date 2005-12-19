f0d00719dba0f9ef37d2596ac0f378694b467fbf
slewis <slewis>
2005-12-19 00:02:11 +0000

    Update to manifests for 0.5.5 stable build

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
@@ -4,18 +4,18 @@ RP=../../../plugins
 CURRENT=`pwd`
 cd ${CURRENT}
 # Relative path for accessing plugins
-ECF=${RP}/org.eclipse.ecf_0.5.4/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.5.4/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.5.4/ecf.sdo.jar
-DS=${RP}/org.eclipse.ecf.datashare_0.5.4/datashare.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.5.4/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.5.4/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.5.4/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.5.4/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.5.4/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.5.4/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.5.4/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.5.4/client.jar
+ECF=${RP}/org.eclipse.ecf_0.5.5/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.5.5/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.5.5/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.5.5/datashare.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.5.5/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.5.5/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.5.5/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.5.5/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.5.5/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.5.5/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.5.5/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.5.5/client.jar
 
 CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${HELLO}:${DISCOVERY}:${COLLAB}:."
 
---
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
@@ -0,0 +1,32 @@
+# ECF Server Start Script
+#
+RP=../../../plugins
+CURRENT=`pwd`
+cd ${CURRENT}
+# Relative path for accessing plugins
+ECF=${RP}/org.eclipse.ecf_0.5.5/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.5.5/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.5.5/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.5.5/datashare.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.5.5/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.5.5/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.5.5/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.5.5/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.5.5/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.5.5/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.5.5/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.5.5/client.jar
+
+CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${HELLO}:${DISCOVERY}:${COLLAB}:."
+
+TRACE="-Dorg.eclipse.ecf.Trace=true -Dorg.eclipse.ecf.provider.Trace=true"
+
+OPTIONS=${TRACE}
+
+MAINCLASS=org.eclipse.ecf.provider.app.ServerApplication
+ARGS="-c ../conf/server.xml $*"
+
+# Start server
+echo "Starting server with options: ${OPTIONS} and args: ${ARGS}"
+java -cp ${CP} ${OPTIONS} ${MAINCLASS} ${ARGS} > ../../../logs/3282/`date +%s`.log &
+
