4a02b140392c1797c6a3af0e2ed7a55682550099
slewis <slewis>
2006-01-03 18:16:25 +0000

    Removed IReliableContainer superclass from IChannelContainer

--- framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/ds/IChannelContainer.java
+++ framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/ds/IChannelContainer.java
@@ -1,10 +1,9 @@
 package org.eclipse.ecf.ds;
 
-import org.eclipse.ecf.core.IReliableContainer;
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.util.ECFException;
 
-public interface IChannelContainer extends IReliableContainer {
+public interface IChannelContainer {
 	public IChannel createChannel(ID channelID) throws ECFException;
 	public IChannel getChannel(ID channelID);
 	public boolean disposeChannel(ID channelID);
