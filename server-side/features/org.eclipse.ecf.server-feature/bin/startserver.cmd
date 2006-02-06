db731a15fd35a6564c34481c16d4c79ae5cd44fe
slewis <slewis>
2006-02-06 05:24:03 +0000

    Fix for server startup scripts to include datashare and fileshare (and provider) paths

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.cmd
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.cmd
@@ -6,6 +6,9 @@ set ECF=%RP%\org.eclipse.ecf_0.6.2\ecf.jar
 set UI=%RP%\org.eclipse.ecf.ui_0.6.2\ui.jar
 set SDO=%RP%\org.eclipse.ecf.sdo_0.6.2\ecf.sdo.jar
 set DS=%RP%\org.eclipse.ecf.datashare_0.6.2\datashare.jar
+set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.6.2\dsprovider.jar
+set FS=%RP%\org.eclipse.ecf.fileshare_0.6.2\fileshare.jar
+set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.6.2\fsprovider.jar
 set PROVIDER=%RP%\org.eclipse.ecf.provider_0.6.2\provider.jar
 set PRESENCE=%RP%\org.eclipse.ecf.presence_0.6.2\presence.jar
 set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.6.2\editor.jar
@@ -15,7 +18,7 @@ set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.6.2\discovery.jar
 set HELLO=%RP%\org.eclipse.ecf.example.hello_0.6.2\hello.jar
 set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.6.2\client.jar
 
-set CP="..\lib\core.jar;..\lib\runtime.jar;..\lib\osgi.jar;%ECF%;%UI%;%SDO%;%PROVIDER%;%PRESENCE%;%GED%;%ED%;%LIBRARY%;%HELLO%;%DS%;%DISCOVERY%;%COLLAB%;."
+set CP="..\lib\core.jar;..\lib\runtime.jar;..\lib\osgi.jar;%ECF%;%UI%;%SDO%;%PROVIDER%;%PRESENCE%;%GED%;%ED%;%LIBRARY%;%HELLO%;%DS%;%DSP%;%FS%;%FsSP%;%DISCOVERY%;%COLLAB%;."
 
 set TRACE=-Dorg.eclipse.ecf.Trace=true -Dorg.eclipse.ecf.provider.Trace=true
 
--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.sh
@@ -8,6 +8,9 @@ ECF=${RP}/org.eclipse.ecf_0.6.2/ecf.jar
 UI=${RP}/org.eclipse.ecf.ui_0.6.2/ui.jar
 SDO=${RP}/org.eclipse.ecf.sdo_0.6.2/ecf.sdo.jar
 DS=${RP}/org.eclipse.ecf.datashare_0.6.2/datashare.jar
+DSP=${RP}/org.eclipse.ecf.provider.datashare_0.6.2/dsprovider.jar
+FS=${RP}/org.eclipse.ecf.fileshare_0.6.2/fileshare.jar
+FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.6.2/fsprovider.jar
 PROVIDER=${RP}/org.eclipse.ecf.provider_0.6.2/provider.jar
 PRESENCE=${RP}/org.eclipse.ecf.presence_0.6.2/presence.jar
 GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.6.2/editor.jar
@@ -17,7 +20,7 @@ DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.6.2/discovery.jar
 HELLO=${RP}/org.eclipse.ecf.example.hello_0.6.2/hello.jar
 COLLAB=${RP}/org.eclipse.ecf.example.collab_0.6.2/client.jar
 
-CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${HELLO}:${DISCOVERY}:${COLLAB}:."
+CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${DSP}:${FS}:${FSP}:${HELLO}:${DISCOVERY}:${COLLAB}:."
 
 TRACE="-Dorg.eclipse.ecf.Trace=true -Dorg.eclipse.ecf.provider.Trace=true" 
 
