6de639e5043e7c1dbcebb46c7c148c1d9706b53e
slewis <slewis>
2006-02-06 05:42:33 +0000

    Update to 0.6.3 version, regenerated javadocs

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserver.cmd
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserver.cmd
@@ -2,21 +2,21 @@ echo off
 setlocal
 cd %~dp0
 set RP=..\..\..\plugins
-set ECF=%RP%\org.eclipse.ecf_0.6.2\ecf.jar
-set UI=%RP%\org.eclipse.ecf.ui_0.6.2\ui.jar
-set SDO=%RP%\org.eclipse.ecf.sdo_0.6.2\ecf.sdo.jar
-set DS=%RP%\org.eclipse.ecf.datashare_0.6.2\datashare.jar
-set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.6.2\dsprovider.jar
-set FS=%RP%\org.eclipse.ecf.fileshare_0.6.2\fileshare.jar
-set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.6.2\fsprovider.jar
-set PROVIDER=%RP%\org.eclipse.ecf.provider_0.6.2\provider.jar
-set PRESENCE=%RP%\org.eclipse.ecf.presence_0.6.2\presence.jar
-set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.6.2\editor.jar
-set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.6.2\editor.jar
-set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.6.2\runtime\org.eclipse.ecf.example.library.jar
-set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.6.2\discovery.jar
-set HELLO=%RP%\org.eclipse.ecf.example.hello_0.6.2\hello.jar
-set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.6.2\client.jar
+set ECF=%RP%\org.eclipse.ecf_0.6.3\ecf.jar
+set UI=%RP%\org.eclipse.ecf.ui_0.6.3\ui.jar
+set SDO=%RP%\org.eclipse.ecf.sdo_0.6.3\ecf.sdo.jar
+set DS=%RP%\org.eclipse.ecf.datashare_0.6.3\datashare.jar
+set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.6.3\dsprovider.jar
+set FS=%RP%\org.eclipse.ecf.fileshare_0.6.3\fileshare.jar
+set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.6.3\fsprovider.jar
+set PROVIDER=%RP%\org.eclipse.ecf.provider_0.6.3\provider.jar
+set PRESENCE=%RP%\org.eclipse.ecf.presence_0.6.3\presence.jar
+set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.6.3\editor.jar
+set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.6.3\editor.jar
+set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.6.3\runtime\org.eclipse.ecf.example.library.jar
+set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.6.3\discovery.jar
+set HELLO=%RP%\org.eclipse.ecf.example.hello_0.6.3\hello.jar
+set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.6.3\client.jar
 
 set CP="..\lib\core.jar;..\lib\runtime.jar;..\lib\osgi.jar;%ECF%;%UI%;%SDO%;%PROVIDER%;%PRESENCE%;%GED%;%ED%;%LIBRARY%;%HELLO%;%DS%;%DSP%;%FS%;%FsSP%;%DISCOVERY%;%COLLAB%;."
 
