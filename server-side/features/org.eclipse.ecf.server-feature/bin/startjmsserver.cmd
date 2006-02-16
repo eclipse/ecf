937e33bb12a72a174f6afcec8918d327b3c74078
slewis <slewis>
2006-02-16 16:06:38 +0000

    Change to 0.7.0 version number plus update to javadocs

--- server-side/features/org.eclipse.ecf.server-feature/bin/startjmsserver.cmd
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startjmsserver.cmd
@@ -1,23 +1,23 @@
 setlocal
 cd %~dp0
 set RP=..\..\..\plugins
-set JMS=%RP%\org.eclipse.ecf.provider.jms_0.6.4
-set ECF=%RP%\org.eclipse.ecf_0.6.4\ecf.jar
+set JMS=%RP%\org.eclipse.ecf.provider.jms_0.7.0
+set ECF=%RP%\org.eclipse.ecf_0.7.0\ecf.jar
 set JMSP=%JMS%\jmsprovider.jar
-set UI=%RP%\org.eclipse.ecf.ui_0.6.4\ui.jar
-set SDO=%RP%\org.eclipse.ecf.sdo_0.6.4\ecf.sdo.jar
-set DS=%RP%\org.eclipse.ecf.datashare_0.6.4\datashare.jar
-set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.6.4\dsprovider.jar
-set FS=%RP%\org.eclipse.ecf.fileshare_0.6.4\fileshare.jar
-set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.6.4\fsprovider.jar
-set PROVIDER=%RP%\org.eclipse.ecf.provider_0.6.4\provider.jar
-set PRESENCE=%RP%\org.eclipse.ecf.presence_0.6.4\presence.jar
-set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.6.4\editor.jar
-set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.6.4\editor.jar
-set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.6.4\runtime\org.eclipse.ecf.example.library.jar
-set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.6.4\discovery.jar
-set HELLO=%RP%\org.eclipse.ecf.example.hello_0.6.4\hello.jar
-set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.6.4\client.jar
+set UI=%RP%\org.eclipse.ecf.ui_0.7.0\ui.jar
+set SDO=%RP%\org.eclipse.ecf.sdo_0.7.0\ecf.sdo.jar
+set DS=%RP%\org.eclipse.ecf.datashare_0.7.0\datashare.jar
+set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.7.0\dsprovider.jar
+set FS=%RP%\org.eclipse.ecf.fileshare_0.7.0\fileshare.jar
+set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.7.0\fsprovider.jar
+set PROVIDER=%RP%\org.eclipse.ecf.provider_0.7.0\provider.jar
+set PRESENCE=%RP%\org.eclipse.ecf.presence_0.7.0\presence.jar
+set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.7.0\editor.jar
+set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.7.0\editor.jar
+set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.7.0\runtime\org.eclipse.ecf.example.library.jar
+set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.7.0\discovery.jar
+set HELLO=%RP%\org.eclipse.ecf.example.hello_0.7.0\hello.jar
+set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.7.0\client.jar
 
 set JMSL=%JMS%\lib\activemq3.1M5
 
