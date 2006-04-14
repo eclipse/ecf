50a245a0891b6870dcd83b667c8364d5741238d6
slewis <slewis>
2006-04-14 04:45:38 +0000

    Added plugin org.jivesoftware.smack plugin (v 2.2.0).   Changed dependencies in org.eclipse.ecf.provider.xmpp to this new plugin rather than to smack libraries in org.eclipse.ecf.provider.xmpp/lib.  Added smack plugin to build.  Changed xmpp feature to include smack plugin.

---
+++ protocols/bundles/org.jivesoftware.smack/src/org/jivesoftware/smack/ConnectionListener2.java
@@ -0,0 +1,10 @@
+package org.jivesoftware.smack;
+
+public interface ConnectionListener2 {
+
+	/**
+	* Notification that the connection has been authenticated.
+	*/
+	public void connectionAuthenticated();
+
+}
