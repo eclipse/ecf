366cd545624902f3b5c2fac0b967b194c334ff2d
slewis <slewis>
2006-05-18 05:49:12 +0000

    Fixed about.html for all plugins and features.  Also updated version to 0.8.2.

--- server-side/features/org.eclipse.ecf.server-feature/bin/startjmsserver.cmd
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startjmsserver.cmd
@@ -1,29 +1,30 @@
 setlocal
 cd %~dp0
 set RP=..\..\..\plugins
-set JMS=%RP%\org.eclipse.ecf.provider.jms_0.8.1
-set ECF=%RP%\org.eclipse.ecf_0.8.1\ecf.jar
+set JMS=%RP%\org.eclipse.ecf.provider.jms_0.8.2
+set ECF=%RP%\org.eclipse.ecf_0.8.2\ecf.jar
 set JMSP=%JMS%\jmsprovider.jar
-set UI=%RP%\org.eclipse.ecf.ui_0.8.1\ui.jar
-set SDO=%RP%\org.eclipse.ecf.sdo_0.8.1\ecf.sdo.jar
-set DS=%RP%\org.eclipse.ecf.datashare_0.8.1\datashare.jar
-set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.8.1\dsprovider.jar
-set FS=%RP%\org.eclipse.ecf.fileshare_0.8.1\fileshare.jar
-set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.8.1\fsprovider.jar
-set PROVIDER=%RP%\org.eclipse.ecf.provider_0.8.1\provider.jar
-set PRESENCE=%RP%\org.eclipse.ecf.presence_0.8.1\presence.jar
-set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.8.1\editor.jar
-set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.8.1\editor.jar
-set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.8.1\runtime\org.eclipse.ecf.example.library.jar
-set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.8.1\discovery.jar
-set HELLO=%RP%\org.eclipse.ecf.example.hello_0.8.1\hello.jar
-set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.8.1\client.jar
+set UI=%RP%\org.eclipse.ecf.ui_0.8.2\ui.jar
+set SDO=%RP%\org.eclipse.ecf.sdo_0.8.2\ecf.sdo.jar
+set DS=%RP%\org.eclipse.ecf.datashare_0.8.2\datashare.jar
+set DSP=%RP%\org.eclipse.ecf.provider.datashare_0.8.2\dsprovider.jar
+set FS=%RP%\org.eclipse.ecf.fileshare_0.8.2\fileshare.jar
+set FSP=%RP%\org.eclipse.ecf.provider.fileshare_0.8.2\fsprovider.jar
+set PROVIDER=%RP%\org.eclipse.ecf.provider_0.8.2\provider.jar
+set PRESENCE=%RP%\org.eclipse.ecf.presence_0.8.2\presence.jar
+set GED=%RP%\org.eclipse.ecf.example.sdo.gefeditor_0.8.2\editor.jar
+set ED=%RP%\org.eclipse.ecf.example.sdo.editor_0.8.2\editor.jar
+set LIBRARY=%RP%\org.eclipse.ecf.example.sdo.library_0.8.2\runtime\org.eclipse.ecf.example.library.jar
+set DISCOVERY=%RP%\org.eclipse.ecf.discovery_0.8.2\discovery.jar
+set HELLO=%RP%\org.eclipse.ecf.example.hello_0.8.2\hello.jar
+set COLLAB=%RP%\org.eclipse.ecf.example.collab_0.8.2\client.jar
+set CED=%RP%\org.eclipse.ecf.example.collab.editor_0.8.2\collabeditor.jar
 
 set JMSL=%JMS%\lib\activemq3.1M5
 
 set JMSLIB=%JMSL%\activeio-1.1.jar;%JMSL%\activemq-core-3.1-M5.jar;%JMSL%\commons-logging-1.0.3.jar;%JMSL%\concurrent-1.3.4.jar;%JMSL%\geronimo-spec-j2ee-jacc-1.0-rc4.jar;%JMSL%\geronimo-spec-j2ee-management-1.0-rc4.jar;%JMSL%\geronimo-spec-jms-1.1-rc4.jar;%JMSL%\geronimo-spec-jta-1.0.1B-rc4.jar;%JMSL%\log4j-1.2.8.jar;%JMSL%\spring-1.2.2.jar
 
-set CP="..\lib\core.jar;..\lib\runtime.jar;..\lib\osgi.jar;%JMSLIB%;%ECF%;%PROVIDER%;%PRESENCE%;%JMSP%;%DISCOVERY%;%UI%;%SDO%;%DS%;%DSP%;%FS%;%FSP%;%GED%;%ED%;%LIBRARY%;%HELLO%;%COLLAB%."
+set CP="..\lib\core.jar;..\lib\runtime.jar;..\lib\osgi.jar;%JMSLIB%;%ECF%;%PROVIDER%;%PRESENCE%;%JMSP%;%DISCOVERY%;%UI%;%SDO%;%DS%;%DSP%;%FS%;%FSP%;%GED%;%ED%;%LIBRARY%;%HELLO%;%COLLAB%;%CED%;."
 
 rem set TRACE=-Dorg.eclipse.ecf.provider.jms.Trace=true -Dorg.eclipse.ecf.provider.Trace=true
 set TRACE=-Dorg.eclipse.ecf.provider.Trace=true -Dorg.eclipse.ecf.Trace=true -Dorg.eclipse.ecf.provider.jms.Trace=true
--- server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
@@ -4,23 +4,24 @@ RP=../../../plugins
 CURRENT=`pwd`
 cd ${CURRENT}
 # Relative path for accessing plugins
-ECF=${RP}/org.eclipse.ecf_0.8.1/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.8.1/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.8.1/ecf.sdo.jar
-DS=${RP}/org.eclipse.ecf.datashare_0.8.1/datashare.jar
-DSP=${RP}/org.eclipse.ecf.provider.datashare_0.8.1/dsprovider.jar
-FS=${RP}/org.eclipse.ecf.fileshare_0.8.1/fileshare.jar
-FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.8.1/fsprovider.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.8.1/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.8.1/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.8.1/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.8.1/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.8.1/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.8.1/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.8.1/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.8.1/client.jar
+ECF=${RP}/org.eclipse.ecf_0.8.2/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.8.2/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.8.2/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.8.2/datashare.jar
+DSP=${RP}/org.eclipse.ecf.provider.datashare_0.8.2/dsprovider.jar
+FS=${RP}/org.eclipse.ecf.fileshare_0.8.2/fileshare.jar
+FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.8.2/fsprovider.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.8.2/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.8.2/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.8.2/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.8.2/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.8.2/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.8.2/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.8.2/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.8.2/client.jar
+CED=${RP}/org.eclipse.ecf.example.collab.editor_0.8.2/collabeditor.jar
 
-CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${DSP}:${FS}:${FSP}:${HELLO}:${DISCOVERY}:${COLLAB}:."
+CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${DSP}:${FS}:${FSP}:${HELLO}:${DISCOVERY}:${COLLAB}:${CED}:."
 
 TRACE="-Dorg.eclipse.ecf.Trace=true -Dorg.eclipse.ecf.provider.Trace=true"
 
