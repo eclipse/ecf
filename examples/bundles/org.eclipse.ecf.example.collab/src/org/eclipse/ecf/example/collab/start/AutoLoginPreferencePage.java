5d9a5a17e9cc6221e77579a95a1c25963fbc011d
slewis <slewis>
2006-05-23 00:41:57 +0000

    Added ability to delete auto logins from Team->ECF Autologin preference page

--- examples/bundles/org.eclipse.ecf.example.collab/src/org/eclipse/ecf/example/collab/start/AutoLoginPreferencePage.java
+++ examples/bundles/org.eclipse.ecf.example.collab/src/org/eclipse/ecf/example/collab/start/AutoLoginPreferencePage.java
@@ -1,20 +1,27 @@
 package org.eclipse.ecf.example.collab.start;
 
+import java.util.Collection;
 import org.eclipse.ecf.example.collab.ClientPlugin;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class AutoLoginPreferencePage extends FieldEditorPreferencePage
-		implements IWorkbenchPreferencePage {
+public class AutoLoginPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+	
+	protected Collection contents = null;
+	protected ListViewer list = null;
+	protected Button delete = null;
+	
 	public AutoLoginPreferencePage() {
 		super(GRID);
 		setPreferenceStore(ClientPlugin.getDefault().getPreferenceStore());
 	}
-	protected void createFieldEditors() {
-		// TODO Auto-generated method stub
-	}
+
 	public void init(IWorkbench workbench) {
-		// TODO Auto-generated method stub
+	}
+	protected void createFieldEditors() {
+		addField(new URLListFieldEditor("urilisteditor","The following will be connected upon ECF start",getFieldEditorParent()));
 	}
 }
