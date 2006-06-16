da5c20b6e8c08a22a2fcb50711d319f6b48208c9
slewis <slewis>
2006-06-16 18:03:03 +0000

    Changed version over to 0.8.6 from 0.8.5

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
@@ -4,22 +4,22 @@ RP=../../../plugins
 CURRENT=`pwd`
 cd ${CURRENT}
 # Relative path for accessing plugins
-ECF=${RP}/org.eclipse.ecf_0.8.5/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.8.5/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.8.5/ecf.sdo.jar
-DS=${RP}/org.eclipse.ecf.datashare_0.8.5/datashare.jar
-DSP=${RP}/org.eclipse.ecf.provider.datashare_0.8.5/dsprovider.jar
-FS=${RP}/org.eclipse.ecf.fileshare_0.8.5/fileshare.jar
-FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.8.5/fsprovider.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.8.5/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.8.5/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.8.5/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.8.5/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.8.5/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.8.5/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.8.5/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.8.5/client.jar
-CED=${RP}/org.eclipse.ecf.example.collab.editor_0.8.5/collabeditor.jar
+ECF=${RP}/org.eclipse.ecf_0.8.6/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.8.6/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.8.6/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.8.6/datashare.jar
+DSP=${RP}/org.eclipse.ecf.provider.datashare_0.8.6/dsprovider.jar
+FS=${RP}/org.eclipse.ecf.fileshare_0.8.6/fileshare.jar
+FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.8.6/fsprovider.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.8.6/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.8.6/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.8.6/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.8.6/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.8.6/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.8.6/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.8.6/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.8.6/client.jar
+CED=${RP}/org.eclipse.ecf.example.collab.editor_0.8.6/collabeditor.jar
 
 CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${DSP}:${FS}:${FSP}:${HELLO}:${DISCOVERY}:${COLLAB}:${CED}:."
 
