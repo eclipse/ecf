56575d961ba409561482192e3c7b58bb7a0f27a6
slewis <slewis>
2006-09-06 01:26:03 +0000

    cleanup for remote service impl code

--- providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RemoteServiceImpl.java
+++ providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RemoteServiceImpl.java
@@ -1,6 +1,5 @@
 package org.eclipse.ecf.provider.remoteservice.generic;
 
-import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
