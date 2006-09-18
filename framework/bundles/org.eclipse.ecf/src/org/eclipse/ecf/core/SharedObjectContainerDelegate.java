67b791669aa1e0ca956ab869013eda4f0e117849
slewis <slewis>
2006-09-18 14:44:34 +0000

    Added abstract delegate class

---
+++ framework/bundles/org.eclipse.ecf/src/org/eclipse/ecf/core/SharedObjectContainerDelegate.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Composent, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.core;
+
+import java.util.Map;
+
+import org.eclipse.ecf.core.identity.ID;
+
+public abstract class SharedObjectContainerDelegate {
+
+	protected ISharedObjectManager sharedObjectManager;
+
+	protected ID delegateID;
+
+	protected SharedObjectContainerDelegate() {
+
+	}
+
+	protected SharedObjectContainerDelegate(ISharedObjectManager manager) {
+		this.sharedObjectManager = manager;
+		this.delegateID = addDelegate();
+	}
+
+	protected ISharedObjectManager getSharedObjectManager() {
+		return sharedObjectManager;
+	}
+
+	protected ID addDelegate() {
+		try {
+			return getSharedObjectManager().addSharedObject(
+					createSharedObjectID(), createSharedObjectInstance(),
+					createSharedObjectProperties());
+		} catch (SharedObjectAddException e) {
+			throw new RuntimeException(
+					"Exception adding shared object instance", e);
+		}
+	}
+
+	protected abstract ISharedObject createSharedObjectInstance();
+
+	protected abstract ID createSharedObjectID();
+
+	protected Map createSharedObjectProperties() {
+		return null;
+	}
+
+	public Object getDelegate() {
+		return getSharedObjectManager().getSharedObject(delegateID);
+	}
+
+	public void dispose() {
+		getSharedObjectManager().removeSharedObject(delegateID);
+	}
+}
