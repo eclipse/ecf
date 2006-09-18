57a98cfef2fc52ca5c76260fa747d46838e4b3fe
slewis <slewis>
2006-09-18 22:33:00 +0000

    Added RemoteServiceContainerFactory, added code for RemoteServiceContainerAdapterFactory, added copyrights for files missing

--- providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RemoteServiceContainerAdapterFactory.java
+++ providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RemoteServiceContainerAdapterFactory.java
@@ -1,21 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Composent, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.provider.remoteservice.generic;
 
-import org.eclipse.core.runtime.IAdapterFactory;
-import org.eclipse.ecf.core.ISharedObjectContainer;
+import org.eclipse.ecf.core.AbstractSharedObjectContainerAdapterFactory;
+import org.eclipse.ecf.core.ISharedObject;
 import org.eclipse.ecf.remoteservice.IRemoteServiceContainer;
 
-public class RemoteServiceContainerAdapterFactory implements IAdapterFactory {
+public class RemoteServiceContainerAdapterFactory extends AbstractSharedObjectContainerAdapterFactory {
 
-	public Object getAdapter(Object adaptableObject, Class adapterType) {
-		if (ISharedObjectContainer.class.isInstance(adaptableObject)) {
-			RemoteServiceContainerAdapter rsca = new RemoteServiceContainerAdapter(((ISharedObjectContainer) adaptableObject).getSharedObjectManager());
-			return rsca.getDelegate();
-		}
-		return null;
+	protected ISharedObject createAdapter(Class adapterType) {
+		if (adapterType.equals(IRemoteServiceContainer.class)) return new RegistrySharedObject();
+		else return null;
 	}
 
 	public Class[] getAdapterList() {
 		return new Class[] { IRemoteServiceContainer.class };
 	}
-
+	
 }
