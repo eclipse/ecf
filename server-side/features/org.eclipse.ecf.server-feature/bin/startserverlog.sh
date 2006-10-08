fb9b855dbbceedade4cc983f0f09c97d156ef5d5
slewis <slewis>
2006-10-08 05:29:18 +0000

    Change of meta file info to 0.9.3 version.  Also fixed copyright notices in source

--- server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
+++ server-side/features/org.eclipse.ecf.server-feature/bin/startserverlog.sh
@@ -4,22 +4,22 @@ RP=../../../plugins
 CURRENT=`pwd`
 cd ${CURRENT}
 # Relative path for accessing plugins
-ECF=${RP}/org.eclipse.ecf_0.9.2/ecf.jar
-UI=${RP}/org.eclipse.ecf.ui_0.9.2/ui.jar
-SDO=${RP}/org.eclipse.ecf.sdo_0.9.2/ecf.sdo.jar
-DS=${RP}/org.eclipse.ecf.datashare_0.9.2/datashare.jar
-DSP=${RP}/org.eclipse.ecf.provider.datashare_0.9.2/dsprovider.jar
-FS=${RP}/org.eclipse.ecf.fileshare_0.9.2/fileshare.jar
-FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.9.2/fsprovider.jar
-PROVIDER=${RP}/org.eclipse.ecf.provider_0.9.2/provider.jar
-PRESENCE=${RP}/org.eclipse.ecf.presence_0.9.2/presence.jar
-GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.9.2/editor.jar
-ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.9.2/editor.jar
-LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.9.2/runtime/org.eclipse.ecf.example.library.jar
-DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.9.2/discovery.jar
-HELLO=${RP}/org.eclipse.ecf.example.hello_0.9.2/hello.jar
-COLLAB=${RP}/org.eclipse.ecf.example.collab_0.9.2/client.jar
-CED=${RP}/org.eclipse.ecf.example.collab.editor_0.9.2/collabeditor.jar
+ECF=${RP}/org.eclipse.ecf_0.9.3/ecf.jar
+UI=${RP}/org.eclipse.ecf.ui_0.9.3/ui.jar
+SDO=${RP}/org.eclipse.ecf.sdo_0.9.3/ecf.sdo.jar
+DS=${RP}/org.eclipse.ecf.datashare_0.9.3/datashare.jar
+DSP=${RP}/org.eclipse.ecf.provider.datashare_0.9.3/dsprovider.jar
+FS=${RP}/org.eclipse.ecf.fileshare_0.9.3/fileshare.jar
+FSP=${RP}/org.eclipse.ecf.provider.fileshare_0.9.3/fsprovider.jar
+PROVIDER=${RP}/org.eclipse.ecf.provider_0.9.3/provider.jar
+PRESENCE=${RP}/org.eclipse.ecf.presence_0.9.3/presence.jar
+GED=${RP}/org.eclipse.ecf.example.sdo.gefeditor_0.9.3/editor.jar
+ED=${RP}/org.eclipse.ecf.example.sdo.editor_0.9.3/editor.jar
+LIBRARY=${RP}/org.eclipse.ecf.example.sdo.library_0.9.3/runtime/org.eclipse.ecf.example.library.jar
+DISCOVERY=${RP}/org.eclipse.ecf.discovery_0.9.3/discovery.jar
+HELLO=${RP}/org.eclipse.ecf.example.hello_0.9.3/hello.jar
+COLLAB=${RP}/org.eclipse.ecf.example.collab_0.9.3/client.jar
+CED=${RP}/org.eclipse.ecf.example.collab.editor_0.9.3/collabeditor.jar
 
 CP="../lib/core.jar:../lib/runtime.jar:../lib/osgi.jar:${ECF}:${UI}:${SDO}:${PROVIDER}:${PRESENCE}:${GED}:${ED}:${LIBRARY}:${DS}:${DSP}:${FS}:${FSP}:${HELLO}:${DISCOVERY}:${COLLAB}:${CED}:."
 
