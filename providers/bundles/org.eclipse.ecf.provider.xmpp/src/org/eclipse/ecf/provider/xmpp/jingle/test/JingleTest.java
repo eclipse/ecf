967ca37258e226105ff0394674b6deec49baf724
pperret <pperret>
2006-10-17 19:59:39 +0000

    *** empty log message ***

--- providers/bundles/org.eclipse.ecf.provider.xmpp/src/org/eclipse/ecf/provider/xmpp/jingle/test/JingleTest.java
+++ providers/bundles/org.eclipse.ecf.provider.xmpp/src/org/eclipse/ecf/provider/xmpp/jingle/test/JingleTest.java
@@ -9,19 +9,17 @@ public class JingleTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		try {
 			XMPPClientSOContainer client1= new XMPPClientSOContainer();
-			XMPPClientSOContainer client2= new XMPPClientSOContainer();
-			
-			ICallContainer jingle1= (ICallContainer)client1.getAdapter( ICallContainer.class );
-			ICallContainer jingle2= (ICallContainer)client2.getAdapter( ICallContainer.class );
-			
-			jingle1.createCallSession( client1.getID() );
-			jingle2.createCallSession( client2.getID() );
-			
+
 			// client1 make a call to client2
-			
+			// this installs request listener for incoming sessions
+			ICallContainer jingle1= (ICallContainer)client1.getAdapter( ICallContainer.class );
+
+			jingle1.createCallSession( client1.getID() );
+
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
