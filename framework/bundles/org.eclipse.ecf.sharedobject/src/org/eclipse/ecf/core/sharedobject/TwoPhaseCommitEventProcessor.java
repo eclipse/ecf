4fde635a9edcdeddcc4af0aac04738ab4ec4dcc0
slewis <slewis>
2006-10-25 03:34:00 +0000

    Initial checkin for new org.eclipse.ecf.sharedobject plugin

---
+++ framework/bundles/org.eclipse.ecf.sharedobject/src/org/eclipse/ecf/core/sharedobject/TwoPhaseCommitEventProcessor.java
@@ -0,0 +1,363 @@
+/****************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.core.sharedobject;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import org.eclipse.ecf.core.events.IContainerConnectedEvent;
+import org.eclipse.ecf.core.events.IContainerDisconnectedEvent;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.sharedobject.events.ISharedObjectActivatedEvent;
+import org.eclipse.ecf.core.sharedobject.events.ISharedObjectCommitEvent;
+import org.eclipse.ecf.core.sharedobject.events.ISharedObjectCreateResponseEvent;
+import org.eclipse.ecf.core.sharedobject.events.ISharedObjectMessageEvent;
+import org.eclipse.ecf.core.sharedobject.events.SharedObjectCommitEvent;
+import org.eclipse.ecf.core.util.Event;
+import org.eclipse.ecf.core.util.IEventProcessor;
+import org.eclipse.ecf.core.util.Trace;
+import org.eclipse.ecf.internal.core.sharedobject.Activator;
+import org.eclipse.ecf.internal.core.sharedobject.SharedObjectDebugOptions;
+
+/**
+ * Implementation of two-phase commit for transactional replication of shared objects.
+ *
+ */
+public class TwoPhaseCommitEventProcessor implements IEventProcessor,
+		ISharedObjectContainerTransaction {
+	AbstractSharedObject sharedObject = null;
+	byte transactionState = ISharedObjectContainerTransaction.ACTIVE;
+	Object lock = new Object();
+	List participants = new Vector();
+	Map failed = new HashMap();
+	int timeout = ISharedObjectTransactionConfig.DEFAULT_TIMEOUT;
+	int minFailedToAbort = 0;
+	long identifier = 0;
+	ISharedObjectTransactionParticipantsFilter participantsFilter = null;
+	
+	public TwoPhaseCommitEventProcessor(AbstractSharedObject bse, ISharedObjectTransactionConfig config) {
+		this.sharedObject = bse;
+		if (config == null) {
+			config = new TransactionSharedObjectConfiguration();
+		}
+		this.timeout = config.getTimeout();
+		this.participantsFilter = config.getParticipantsFilter();
+	}
+	protected void trace(String msg) {
+		Trace.trace(Activator.getDefault(),msg);
+	}
+	protected void traceStack(String msg, Throwable t) {
+		Trace.catching(Activator.getDefault(), SharedObjectDebugOptions.EXCEPTIONS_CATCHING, TwoPhaseCommitEventProcessor.class, "traceStack", t);;
+	}
+	protected int getTimeout() {
+		return timeout;
+	}
+	protected int getMinFailedToAbort() {
+		return minFailedToAbort;
+	}
+	protected boolean isPrimary() {
+		return getSharedObject().isPrimary();
+	}
+	protected AbstractSharedObject getSharedObject() {
+		return sharedObject;
+	}
+	protected ID getHomeID() {
+		return getSharedObject().getHomeContainerID();
+	}
+	protected void addParticipants(ID[] ids) {
+		if (ids != null) {
+			for (int i = 0; i < ids.length; i++) {
+				trace("addParticipant(" + ids[i] + ")");
+				if (!getHomeID().equals(ids[i]))
+					participants.add(ids[i]);
+			}
+		}
+	}
+	protected void removeParticipant(ID id) {
+		if (id != null) {
+			trace("removeParticipant(" + id + ")");
+			participants.remove(id);
+		}
+	}
+	protected void addFailed(ID remote, Throwable failure) {
+		if (remote != null && failure != null) {
+			trace("addFailed(" + remote + "," + failure + ")");
+			failed.put(remote, failure);
+		}
+	}
+	protected ISharedObjectContext getContext() {
+		return getSharedObject().getContext();
+	}
+	/*
+	 * Implementation of IEventProcessor. These methods are entry point methods
+	 * for AbstractSharedObject method dispatch to call
+	 */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ecf.core.util.IEventProcessor#processEvent(org.eclipse.ecf.core.util.Event)
+	 */
+	public boolean processEvent(Event event) {
+		if (event instanceof ISharedObjectActivatedEvent) {
+			handleActivated((ISharedObjectActivatedEvent) event);
+		} else if (event instanceof IContainerConnectedEvent) {
+			handleJoined((IContainerConnectedEvent) event);
+		} else if (event instanceof ISharedObjectCreateResponseEvent) {
+			handleCreateResponse((ISharedObjectCreateResponseEvent) event);
+		} else if (event instanceof IContainerDisconnectedEvent) {
+			handleDeparted((IContainerDisconnectedEvent) event);
+		} else if (event instanceof ISharedObjectMessageEvent) {
+			ISharedObjectMessageEvent some = (ISharedObjectMessageEvent) event;
+			Object data = some.getData();
+			if (data instanceof ISharedObjectCommitEvent) localCommitted();
+		}
+		// Let other event processors have a shot at this event
+		return false;
+	}
+	protected void handleActivated(ISharedObjectActivatedEvent event) {
+		trace("handleActivated(" + event + ")");
+		// No other state changes while this is going on
+		synchronized (lock) {
+			if (isPrimary()) {
+				// Primary
+				handlePrimaryActivated(event);
+			} else {
+				handleReplicaActivated(event);
+			}
+			// Notify any threads waiting on state change
+			lock.notifyAll();
+		}
+	}
+	protected void replicateTo(ID[] remotes) {
+		getSharedObject().replicateToRemoteContainers(remotes);
+	}
+	
+	protected void handlePrimaryActivated(ISharedObjectActivatedEvent event) {
+		trace("handlePrimaryActivated("+event+")");
+		// First get current group membership
+		if (getContext().getConnectedID() != null) {
+			ID[] groupMembers = getContext().getGroupMemberIDs();
+			// Now get participants
+			ID[] transactionParticipants = null;
+			// If there is a participants filter specified then use it and ask it to return an ID [] of participants (given
+			// the current group membership
+			if (participantsFilter != null) {
+				transactionParticipants = participantsFilter.filterParticipants(groupMembers);
+			}
+			// replicate
+			if (transactionParticipants == null) {
+				// This means that all current group members should be included as participants
+				replicateTo(null);
+				transactionParticipants = groupMembers;
+			} else {
+				// This means the participants filter provided us with an ID [] and so we replicate only to that ID []
+				replicateTo(transactionParticipants);
+			}
+			
+			// Add participants to the collection
+			addParticipants(transactionParticipants);
+			// Now set transaction state to VOTING
+			setTransactionState(ISharedObjectContainerTransaction.VOTING);
+		} else {
+			setTransactionState(ISharedObjectContainerTransaction.COMMITTED);
+		}
+	}
+	private long getNextIdentifier() {
+		return identifier++;
+	}
+	protected void handleReplicaActivated(ISharedObjectActivatedEvent event) {
+		trace("handleReplicaActivated("+event+")");
+		try {
+			// Try to respond with create success message back to host
+			getContext().sendCreateResponse(getHomeID(), null,
+					getNextIdentifier());
+			// If above succeeds, we're now in prepared state
+			setTransactionState(ISharedObjectContainerTransaction.PREPARED);
+		} catch (Exception except) {
+			// If throws exception, we're doomed
+			traceStack("handleReplicaActivated(" + event + ")", except);
+			setTransactionState(ISharedObjectContainerTransaction.ABORTED);
+		}
+	}
+	protected void handleJoined(IContainerConnectedEvent event) {
+		trace("handleJoined(" + event + ")");
+		// If we are primary then this event matters to us
+		if (isPrimary()) {
+			// If transactionstate is VOTING then we replicate ourselves to participants
+			if (getTransactionState() == ISharedObjectContainerTransaction.VOTING) {
+				synchronized (lock) {
+					// First send replicate message *no matter what state we are in*
+					ID [] newMember = new ID[] { event.getTargetID() };
+					replicateTo(newMember);
+					addParticipants(newMember);
+				}
+			}
+		}
+	}
+	protected void handleCreateResponse(ISharedObjectCreateResponseEvent event) {
+		trace("handleCreateResponse(" + event + ")");
+		if (isPrimary()) {
+			synchronized (lock) {
+				Throwable except = event.getException();
+				ID remoteID = event.getRemoteContainerID();
+				long ident = event.getSequence();
+				if (getTransactionState() == ISharedObjectContainerTransaction.VOTING) {
+					if (except == null) {
+						removeParticipant(remoteID);
+					} else {
+						addFailed(remoteID, except);
+					}
+				} else {
+					handleVotingCompletedCreateResponse(remoteID, except, ident);
+				}
+				lock.notifyAll();
+			}
+		} else {
+			// we don't care as we are note transaction monitor
+		}
+	}
+	protected void handleDeparted(IContainerDisconnectedEvent event) {
+		trace("handleDeparted(" + event + ")");
+		if (isPrimary()) {
+			ID remoteID = event.getTargetID();
+			synchronized (lock) {
+				if (getTransactionState() == ISharedObjectContainerTransaction.VOTING) {
+					addFailed(remoteID, new Exception("Container " + remoteID
+							+ " left"));
+				}
+				lock.notifyAll();
+			}
+		} else {
+			// we don't care as we are not transaction monitor
+		}
+	}
+	protected void handleVotingCompletedCreateResponse(ID fromID, Throwable e,
+			long identifier) {
+		trace("handleVotingCompletedCreateResponse(" + fromID + ","+e+","+identifier+")");
+		// If remote creation was successful, simply send commit message back.
+		if (e == null) {
+			try {
+				getSharedObject().getContext().sendMessage(fromID,
+						new SharedObjectCommitEvent(getSharedObject().getID()));
+			} catch (Exception e2) {
+				traceStack("Exception in sendCommit to " + fromID, e2);
+			}
+		} else {
+			// Too late to vote no
+			handlePostCommitFailure(fromID, e, identifier);
+		}
+	}
+	protected void handlePostCommitFailure(ID fromID, Throwable e,
+			long identifier) {
+		// Do nothing but report
+		trace("handlePostCommitFailure(" + fromID + "," + e + "," + identifier
+				+ ")");
+	}
+	protected void sendCommit() throws SharedObjectAddAbortException {
+		try {
+			getContext().sendMessage(null,
+					new SharedObjectCommitEvent(getSharedObject().getID()));
+		} catch (Exception e2) {
+			doTMAbort(new SharedObjectAddAbortException(
+					"Exception sending commit message", e2, getTimeout()));
+		}
+	}
+	public byte getTransactionState() {
+		synchronized (lock) {
+			return transactionState;
+		}
+	}
+	protected void setTransactionState(byte state) {
+		synchronized (lock) {
+			transactionState = state;
+		}
+	}
+	public void waitToCommit() throws SharedObjectAddAbortException {
+		if (getTransactionState() == ISharedObjectContainerTransaction.COMMITTED)
+			return;
+		synchronized (lock) {
+			long end = System.currentTimeMillis() + getTimeout();
+			try {
+				while (!isVotingCompleted()) {
+					long wait = end - System.currentTimeMillis();
+					trace("waitForFinish waiting " + wait + "ms on "
+							+ getSharedObject().getID());
+					if (wait <= 0L)
+						throw new SharedObjectAddAbortException(
+								"Timeout adding " + getSharedObject().getID()
+										+ " to " + getHomeID(),
+								(Throwable) null, getTimeout());
+					// Wait right here
+					lock.wait(wait);
+				}
+			} catch (Exception except) {
+				// Aborted for some reason. Clean up and throw
+				doTMAbort(except);
+			}
+			// Success. Send commit to remotes and clean up before returning.
+			doTMCommit();
+		}
+	}
+	protected void doTMAbort(Throwable except)
+			throws SharedObjectAddAbortException {
+		trace("doTMAbort:" + except);
+		// Set our own state variable to ABORTED
+		setTransactionState(ISharedObjectContainerTransaction.ABORTED);
+		// Send destroy message here so all remotes get destroyed, and we remove
+		// ourselves from local space as well.
+		getSharedObject().destroySelf();
+		// throw so caller gets exception and can deal with it
+		if (except instanceof SharedObjectAddAbortException)
+			throw (SharedObjectAddAbortException) except;
+		else
+			throw new SharedObjectAddAbortException("Aborted", except,
+					getTimeout());
+	}
+	protected void doTMCommit() throws SharedObjectAddAbortException {
+		trace("doTMCommit");
+		// Make sure we are connected.  If so then send commit message
+		if (getSharedObject().getGroupID() != null) {
+			sendCommit();
+		}
+		// Call local committed message
+		localCommitted();
+	}
+	protected void localCommitted() {
+		trace("localCommitted()");
+		// Set state variable to committed.
+		setTransactionState(ISharedObjectContainerTransaction.COMMITTED);
+		getSharedObject().creationCompleted();
+	}
+	protected boolean isVotingCompleted() throws SharedObjectAddAbortException {
+		// The test here is is we've received any indication of failed
+		// participants in the transaction. If so, we throw.
+		if (getTransactionState() == ISharedObjectContainerTransaction.COMMITTED) return true;
+		if (failed.size() > getMinFailedToAbort()) {
+			// Abort!
+			trace("isVotingCompleted:aborting:failed>"
+					+ getMinFailedToAbort() + ":failed=" + failed);
+			throw new SharedObjectAddAbortException("Abort received",
+					participants, failed, getTimeout());
+			// If no problems, and the number of participants to here from is 0,
+			// then we're done
+		} else if (getTransactionState() == ISharedObjectContainerTransaction.VOTING
+				&& participants.size() == 0) {
+			// Success!
+			trace("isVotingCompleted() returning true");
+			return true;
+		}
+		// Else continue waiting
+		trace("isVotingCompleted:false");
+		return false;
+	}
+}
---
+++ framework/bundles/org.eclipse.ecf.sharedobject/src/org/eclipse/ecf/internal/core/sharedobject/Activator.java
@@ -0,0 +1,295 @@
+package org.eclipse.ecf.internal.core.sharedobject;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtension;
+import org.eclipse.core.runtime.IExtensionDelta;
+import org.eclipse.core.runtime.IExtensionPoint;
+import org.eclipse.core.runtime.IExtensionRegistry;
+import org.eclipse.core.runtime.IRegistryChangeEvent;
+import org.eclipse.core.runtime.IRegistryChangeListener;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ecf.core.sharedobject.ISharedObjectFactory;
+import org.eclipse.ecf.core.sharedobject.SharedObjectFactory;
+import org.eclipse.ecf.core.sharedobject.SharedObjectTypeDescription;
+import org.eclipse.ecf.core.sharedobject.provider.ISharedObjectInstantiator;
+import org.eclipse.ecf.core.util.Trace;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.ecf.sharedobject";
+
+	protected static final String CLASS_ATTRIBUTE = "class";
+
+	protected static final String NAME_ATTRIBUTE = "name";
+
+	protected static final String PROPERTY_ELEMENT_NAME = "property";
+
+	protected static final String VALUE_ATTRIBUTE = "value";
+
+	protected static final String NAMESPACE_NAME = "sharedObjectFactory";
+
+	protected static final String SHAREDOBJECT_FACTORY_EPOINT = PLUGIN_ID + "."
+			+ NAMESPACE_NAME;
+
+	protected static final String DESCRIPTION_ATTRIBUTE = "description";
+
+	private static final int REMOVE_SHAREDOBJECT_ERRORCODE = 1001;
+
+	private static final int FACTORY_NAME_COLLISION_ERRORCODE = 2001;
+
+	// The shared instance
+	private static Activator plugin;
+
+	private IRegistryChangeListener registryManager = null;
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		setupSharedObjectExtensionPoint(context);
+		this.registryManager = new SharedObjectRegistryManager();
+		Platform.getExtensionRegistry().addRegistryChangeListener(
+				registryManager);
+		Trace.exiting(Activator.getDefault(),
+				SharedObjectDebugOptions.METHODS_ENTERING, Activator.class,
+				"start");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		Trace.entering(Activator.getDefault(),
+				SharedObjectDebugOptions.METHODS_EXITING, Activator.class,
+				"stop");
+		Platform.getExtensionRegistry().removeRegistryChangeListener(
+				registryManager);
+		registryManager = null;
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Remove extensions for shared object extension point
+	 * 
+	 * @param members
+	 *            the members to remove
+	 */
+	protected void removeSharedObjectExtensions(IConfigurationElement[] members) {
+		String bundleName = getDefault().getBundle().getSymbolicName();
+		for (int m = 0; m < members.length; m++) {
+			IConfigurationElement member = members[m];
+			String name = null;
+			IExtension extension = member.getDeclaringExtension();
+			try {
+				name = member.getAttribute(NAME_ATTRIBUTE);
+				if (name == null) {
+					name = member.getAttribute(CLASS_ATTRIBUTE);
+				}
+				if (name == null)
+					continue;
+				ISharedObjectFactory factory = SharedObjectFactory.getDefault();
+				SharedObjectTypeDescription sd = factory
+						.getDescriptionByName(name);
+				if (sd == null || !factory.containsDescription(sd)) {
+					continue;
+				}
+				// remove
+				factory.removeDescription(sd);
+				org.eclipse.ecf.core.util.Trace.trace(Activator.getDefault(),
+						SharedObjectDebugOptions.DEBUG,
+						"removeSharedObjectExtensions.removedDescription(" + sd
+								+ ")");
+			} catch (Exception e) {
+				org.eclipse.ecf.core.util.Trace.catching(
+						Activator.getDefault(),
+						SharedObjectDebugOptions.EXCEPTIONS_CATCHING,
+						Activator.class, "removeSharedObjectExtensions", e);
+				getDefault()
+						.getLog()
+						.log(
+								new Status(
+										IStatus.ERROR,
+										Activator.PLUGIN_ID,
+										REMOVE_SHAREDOBJECT_ERRORCODE,
+										"Exception removing sharedobject extension",
+										e));
+			}
+		}
+	}
+
+	/**
+	 * Add shared object extension point extensions
+	 * 
+	 * @param members
+	 *            to add
+	 */
+	protected void addSharedObjectExtensions(IConfigurationElement[] members) {
+		String bundleName = getDefault().getBundle().getSymbolicName();
+		// For each configuration element
+		for (int m = 0; m < members.length; m++) {
+			IConfigurationElement member = members[m];
+			// Get the label of the extender plugin and the ID of the extension.
+			IExtension extension = member.getDeclaringExtension();
+			ISharedObjectInstantiator exten = null;
+			String name = null;
+			try {
+				// The only required attribute is "class"
+				exten = (ISharedObjectInstantiator) member
+						.createExecutableExtension(CLASS_ATTRIBUTE);
+				name = member.getAttribute(NAME_ATTRIBUTE);
+				if (name == null) {
+					name = member.getAttribute(CLASS_ATTRIBUTE);
+				}
+				// Get description, if present
+				String description = member.getAttribute(DESCRIPTION_ATTRIBUTE);
+				if (description == null) {
+					description = "";
+				}
+				// Get any property elements
+				Map properties = getProperties(member
+						.getChildren(PROPERTY_ELEMENT_NAME));
+				// Now make description instance
+				SharedObjectTypeDescription scd = new SharedObjectTypeDescription(
+						name, exten, description, properties);
+				org.eclipse.ecf.core.util.Trace.trace(Activator.getDefault(),
+						SharedObjectDebugOptions.DEBUG,
+						"setupSharedObjectExtensionPoint:createdDescription("
+								+ scd + ")");
+				ISharedObjectFactory factory = SharedObjectFactory.getDefault();
+				if (factory.containsDescription(scd))
+					throw new CoreException(
+							new Status(
+									Status.ERROR,
+									bundleName,
+									FACTORY_NAME_COLLISION_ERRORCODE,
+									"name="
+											+ name
+											+ ";extension point id="
+											+ extension
+													.getExtensionPointUniqueIdentifier(),
+									null));
+
+				// Now add the description and we're ready to go.
+				factory.addDescription(scd);
+				org.eclipse.ecf.core.util.Trace.trace(Activator.getDefault(),
+						SharedObjectDebugOptions.DEBUG,
+						"setupSharedObjectExtensionPoint.addedDescriptionToFactory("
+								+ scd + ")");
+			} catch (CoreException e) {
+				getDefault().getLog().log(e.getStatus());
+				org.eclipse.ecf.core.util.Trace.catching(
+						Activator.getDefault(),
+						SharedObjectDebugOptions.EXCEPTIONS_CATCHING,
+						Activator.class, "addSharedObjectExtensions", e);
+			} catch (Exception e) {
+				getDefault()
+						.getLog()
+						.log(
+								new Status(
+										Status.ERROR,
+										bundleName,
+										FACTORY_NAME_COLLISION_ERRORCODE,
+										"name="
+												+ name
+												+ ";extension point id="
+												+ extension
+														.getExtensionPointUniqueIdentifier(),
+										null));
+				org.eclipse.ecf.core.util.Trace.catching(
+						Activator.getDefault(),
+						SharedObjectDebugOptions.EXCEPTIONS_CATCHING,
+						Activator.class, "addSharedObjectExtensions", e);
+			}
+		}
+	}
+
+	/**
+	 * Setup shared object extension point
+	 * 
+	 * @param context
+	 *            the BundleContext for this bundle
+	 */
+	protected void setupSharedObjectExtensionPoint(BundleContext bc) {
+		IExtensionRegistry reg = Platform.getExtensionRegistry();
+		IExtensionPoint extensionPoint = reg
+				.getExtensionPoint(SHAREDOBJECT_FACTORY_EPOINT);
+		if (extensionPoint == null) {
+			return;
+		}
+		addSharedObjectExtensions(extensionPoint.getConfigurationElements());
+	}
+
+	protected Map getProperties(IConfigurationElement[] propertyElements) {
+		Properties props = new Properties();
+		if (propertyElements != null) {
+			if (propertyElements.length > 0) {
+				for (int i = 0; i < propertyElements.length; i++) {
+					String name = propertyElements[i]
+							.getAttribute(NAME_ATTRIBUTE);
+					String value = propertyElements[i]
+							.getAttribute(VALUE_ATTRIBUTE);
+					if (name != null && !name.equals("") && value != null
+							&& !value.equals("")) {
+						props.setProperty(name, value);
+					}
+				}
+			}
+		}
+		return props;
+	}
+
+	protected class SharedObjectRegistryManager implements
+			IRegistryChangeListener {
+		public void registryChanged(IRegistryChangeEvent event) {
+			IExtensionDelta delta[] = event.getExtensionDeltas(PLUGIN_ID,
+					NAMESPACE_NAME);
+			for (int i = 0; i < delta.length; i++) {
+				switch (delta[i].getKind()) {
+				case IExtensionDelta.ADDED:
+					addSharedObjectExtensions(delta[i].getExtension()
+							.getConfigurationElements());
+					break;
+				case IExtensionDelta.REMOVED:
+					removeSharedObjectExtensions(delta[i].getExtension()
+							.getConfigurationElements());
+					break;
+				}
+			}
+		}
+	}
+
+}
