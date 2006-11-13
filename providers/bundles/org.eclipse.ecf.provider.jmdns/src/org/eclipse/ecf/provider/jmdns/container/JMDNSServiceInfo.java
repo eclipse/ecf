319405c3c3b84d4240bd61d7e0a1d13b6177daae
slewis <slewis>
2006-11-13 03:10:20 +0000

    Removed URI references from discovery API and replaced uses.  Changed ContainerTypeDescription constructors to thrown NullPointerExceptions rather than RuntimeExceptions with InstantiatiionExceptions as cause.

--- providers/bundles/org.eclipse.ecf.provider.jmdns/src/org/eclipse/ecf/provider/jmdns/container/JMDNSServiceInfo.java
+++ providers/bundles/org.eclipse.ecf.provider.jmdns/src/org/eclipse/ecf/provider/jmdns/container/JMDNSServiceInfo.java
@@ -11,8 +11,7 @@
 package org.eclipse.ecf.provider.jmdns.container;
 
 import java.net.InetAddress;
-import java.net.URI;
-import java.net.URISyntaxException;
+
 import org.eclipse.ecf.discovery.IServiceProperties;
 import org.eclipse.ecf.discovery.ServiceInfo;
 import org.eclipse.ecf.discovery.identity.ServiceID;
@@ -32,43 +31,4 @@ public class JMDNSServiceInfo extends ServiceInfo {
 		super(address,type,port,priority,weight,props);
 	}
 
-	protected String getProtocolFromType(String type) {
-		String res = type.trim();
-		while (res.startsWith("_")) {
-			res = res.substring(1);
-		}
-		int dotLoc = res.indexOf(".");
-		if (dotLoc != -1) {
-			res = res.substring(0,dotLoc);
-		}
-		return res;
-	}
-	protected URI getServiceURI0() throws URISyntaxException {
-		IServiceProperties props = this.getServiceProperties();
-		if (props == null) throw new URISyntaxException("invalid serviceinfo","no properties");
-		String protocol = null;
-		String path = null;
-		try {
-			protocol = (String) props.getPropertyString(PROP_PROTOCOL_NAME);
-			path = (String) props.getPropertyString(PROP_PATH_NAME);
-		} catch (Exception e) {
-			throw new URISyntaxException("invalid serviceinfo properties",e.getMessage());
-		}
-		if (protocol == null || protocol.equals("")) {
-			protocol = getProtocolFromType(getServiceID().getServiceType());
-		}
-		if (path == null || path.equals("")) {
-			path = SLASH;
-		} else if (!path.startsWith(SLASH)) {
-			path = SLASH+path;
-		}
-		return new URI(protocol,null,getAddress().getHostAddress(),getPort(),path,null,null);
-	}
-	public URI getServiceURI() throws URISyntaxException {
-		if (this.isResolved()) {
-			return getServiceURI0();
-		} else {
-			throw new URISyntaxException("service info not resolved","");
-		}
-	}
 }
