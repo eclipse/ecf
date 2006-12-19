5b1efe2d5f74f8b2ab6448c9100cf395fabee506
slewis <slewis>
2006-12-19 08:15:23 +0000

    Added methods to RosterAdapterFactory and added icon images to plugin

--- framework/bundles/org.eclipse.ecf.presence.ui/src/org/eclipse/ecf/presence/ui/RosterAdapterFactory.java
+++ framework/bundles/org.eclipse.ecf.presence.ui/src/org/eclipse/ecf/presence/ui/RosterAdapterFactory.java
@@ -10,6 +10,7 @@
  *****************************************************************************/
 package org.eclipse.ecf.presence.ui;
 
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -25,6 +26,24 @@ import org.eclipse.ui.model.IWorkbenchAdapter;
 
 public class RosterAdapterFactory implements IAdapterFactory {
 
+	private static final String LEFT_PAREN = "(";
+	private static final String RIGHT_PAREN = ")";
+	private static final String SLASH = "/";
+	private static final String ROSTER_DISCONNECTED_NAME = LEFT_PAREN
+			+ "disconnected" + RIGHT_PAREN;
+
+	protected String getRosterLabel(Roster roster) {
+		IUser user = roster.getUser();
+		if (user == null)
+			return ROSTER_DISCONNECTED_NAME;
+		else
+			return user.getName();
+	}
+
+	protected ImageDescriptor getRosterImageDescriptor(Roster roster) {
+		return null;
+	}
+
 	private IWorkbenchAdapter rosterAdapter = new IWorkbenchAdapter() {
 
 		public Object[] getChildren(Object o) {
@@ -33,19 +52,53 @@ public class RosterAdapterFactory implements IAdapterFactory {
 		}
 
 		public ImageDescriptor getImageDescriptor(Object object) {
-			return null;
+			return getRosterImageDescriptor((Roster) object);
 		}
 
 		public String getLabel(Object o) {
-			Roster roster = (Roster) o;
-			IUser user = roster.getUser();
-			if (user == null) return "(disconnected)";
-			else return user.getName();
+			return getRosterLabel((Roster) o);
 		}
 
 		public Object getParent(Object o) {
 			return null;
-		}};
+		}
+	};
+
+	protected int getEntriesAvailableCount(Collection entries) {
+		int count = 0;
+		for (Iterator i = entries.iterator(); i.hasNext();) {
+			Object o = i.next();
+			if (o instanceof RosterEntry) {
+				RosterEntry entry = (RosterEntry) o;
+				if (entry.getPresence().getMode().equals(
+						IPresence.Mode.AVAILABLE))
+					count++;
+			}
+		}
+		return count;
+	}
+
+	protected int getEntriesTotalCount(Collection entries) {
+		int count = 0;
+		for (Iterator i = entries.iterator(); i.hasNext();) {
+			Object o = i.next();
+			if (o instanceof RosterEntry)
+				count++;
+		}
+		return count;
+	}
+
+	protected String getRosterGroupLabel(RosterGroup group) {
+		Collection entries = group.getEntries();
+		StringBuffer buf = new StringBuffer(group.getName()).append(" ");
+		buf.append(LEFT_PAREN).append(getEntriesAvailableCount(entries)).append(SLASH);
+		buf.append(getEntriesTotalCount(entries)).append(RIGHT_PAREN);
+		return buf.toString();
+	}
+
+	protected ImageDescriptor getRosterGroupImageDescriptor(RosterGroup group) {
+		return null;
+	}
 	
 	private IWorkbenchAdapter rosterGroupAdapter = new IWorkbenchAdapter() {
 
@@ -54,18 +107,27 @@ public class RosterAdapterFactory implements IAdapterFactory {
 		}
 
 		public ImageDescriptor getImageDescriptor(Object object) {
-			return null;
+			return getRosterGroupImageDescriptor((RosterGroup) object);
 		}
 
 		public String getLabel(Object o) {
-			return ((RosterGroup) o).getName();
+			return getRosterGroupLabel((RosterGroup) o);
 		}
 
 		public Object getParent(Object o) {
 			return ((RosterGroup) o).getParent();
 		}
-		
+
 	};
+
+	protected String getRosterItemLabel(RosterItem item) {
+		return item.getName();
+	}
+
+	protected ImageDescriptor getRosterItemImageDescriptor(RosterItem item) {
+		return null;
+	}
+	
 
 	private IWorkbenchAdapter rosterItemAdapter = new IWorkbenchAdapter() {
 
@@ -74,34 +136,42 @@ public class RosterAdapterFactory implements IAdapterFactory {
 		}
 
 		public ImageDescriptor getImageDescriptor(Object object) {
-			return null;
+			return getRosterItemImageDescriptor((RosterItem) object);
 		}
 
 		public String getLabel(Object o) {
-			return ((RosterItem) o).getName();
+			return getRosterItemLabel((RosterItem) o);
 		}
 
 		public Object getParent(Object o) {
 			return ((RosterItem) o).getParent();
 		}
-		
+
 	};
 
-	protected Object [] getRosterEntryChildrenFromPresence(RosterEntry entry) {
+	protected Object[] getRosterEntryChildrenFromPresence(RosterEntry entry) {
 		IPresence presence = entry.getPresence();
 		Map properties = presence.getProperties();
-		Object [] children = new Object[4+properties.size()];
-		children[0] = new RosterItem(entry, "User: "+entry.getUser().getName());
-		children[1] = new RosterItem(entry, "Mode: "+presence.getMode().toString());
-		children[2] = new RosterItem(entry, "Type: "+presence.getType().toString());
-		children[3] = new RosterItem(entry, "Status: "+presence.getStatus());
-		int index = 4;
-		for(Iterator i=properties.keySet().iterator(); i.hasNext(); index++) {
-			children[index] = properties.get(i.next());
+		int fixedEntries = 2;
+		Object[] children = new Object[fixedEntries + properties.size()];
+		children[0] = new RosterItem(entry, "User: "
+				+ entry.getUser().getName());
+		children[1] = new RosterItem(entry, "Status: " + presence.getStatus());
+		for (Iterator i = properties.keySet().iterator(); i.hasNext(); fixedEntries++) {
+			children[fixedEntries] = properties.get(i.next());
 		}
 		return children;
 	}
+
+	protected String getRosterEntryLabel(RosterEntry entry) {
+		return entry.getName();
+	}
+
+	protected ImageDescriptor getRosterEntryImageDescriptor(RosterEntry entry) {
+		return null;
+	}
 	
+
 	private IWorkbenchAdapter rosterEntryAdapter = new IWorkbenchAdapter() {
 
 		public Object[] getChildren(Object o) {
@@ -109,25 +179,29 @@ public class RosterAdapterFactory implements IAdapterFactory {
 		}
 
 		public ImageDescriptor getImageDescriptor(Object object) {
-			return null;
+			return getRosterEntryImageDescriptor((RosterEntry) object);
 		}
 
 		public String getLabel(Object o) {
-			return ((RosterEntry) o).getName();
+			return getRosterEntryLabel((RosterEntry) o);
 		}
 
 		public Object getParent(Object o) {
 			return ((RosterEntry) o).getParent();
 		}
-		
+
 	};
 
 	public Object getAdapter(Object adaptableObject, Class adapterType) {
 		if (adapterType.equals(IWorkbenchAdapter.class)) {
-			if (adaptableObject instanceof Roster) return rosterAdapter;
-			if (adaptableObject instanceof RosterGroup) return rosterGroupAdapter;
-			if (adaptableObject instanceof RosterEntry) return rosterEntryAdapter;
-			if (adaptableObject instanceof RosterItem) return rosterItemAdapter;
+			if (adaptableObject instanceof Roster)
+				return rosterAdapter;
+			if (adaptableObject instanceof RosterGroup)
+				return rosterGroupAdapter;
+			if (adaptableObject instanceof RosterEntry)
+				return rosterEntryAdapter;
+			if (adaptableObject instanceof RosterItem)
+				return rosterItemAdapter;
 		}
 		return null;
 	}
