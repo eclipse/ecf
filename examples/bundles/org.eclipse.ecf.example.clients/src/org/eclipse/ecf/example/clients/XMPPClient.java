e9b34cacc3b83fdec9e7cadecfae42ae549670fb
slewis <slewis>
2006-12-30 20:55:56 +0000

    Removal of deprecated API in org.eclipse.ecf.presence

--- examples/bundles/org.eclipse.ecf.example.clients/src/org/eclipse/ecf/example/clients/XMPPClient.java
+++ examples/bundles/org.eclipse.ecf.example.clients/src/org/eclipse/ecf/example/clients/XMPPClient.java
@@ -86,7 +86,7 @@ public class XMPPClient {
 				}
 			});
 			if (presenceListener != null) {
-				presence.addPresenceListener(presenceListener);
+				presence.getRosterManager().addPresenceListener(presenceListener);
 			}
 		}
 	}
