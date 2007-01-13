0a2e29e322a3cf16cc8f043304cebe37c74a10f2
slewis <slewis>
2007-01-13 06:36:54 +0000

    Added classes for XMPPConnectWizard/XMPPSConnectWizard, XMPPConnectWizardPage/XMPPSConnectWizardPage

---
+++ providers/bundles/org.eclipse.ecf.provider.xmpp.ui/src/org/eclipse/ecf/provider/xmpp/ui/wizards/XMPPSConnectWizard.java
@@ -0,0 +1,20 @@
+/****************************************************************************
+ * Copyright (c) 2007 Remy Suen, Composent Inc., and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Remy Suen <remy.suen@gmail.com> - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.provider.xmpp.ui.wizards;
+
+
+public final class XMPPSConnectWizard extends XMPPConnectWizard {
+
+	public void addPages() {
+		addPage(new XMPPSConnectWizardPage());
+	}
+
+}
---
+++ providers/bundles/org.eclipse.ecf.provider.xmpp.ui/src/org/eclipse/ecf/provider/xmpp/ui/wizards/XMPPSConnectWizardPage.java
@@ -0,0 +1,69 @@
+/****************************************************************************
+ * Copyright (c) 2007 Remy Suen, Composent Inc., and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Remy Suen <remy.suen@gmail.com> - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.provider.xmpp.ui.wizards;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+final class XMPPSConnectWizardPage extends XMPPConnectWizardPage {
+
+	private Text connectText;
+
+	private Text passwordText;
+
+	XMPPSConnectWizardPage() {
+		super();
+		setTitle("XMPPS Connection Wizard");
+		setDescription("Specify a nickname and XMPPS server to connect to.");
+	}
+
+	public void createControl(Composite parent) {
+		parent.setLayout(new GridLayout());
+		GridData fillData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		GridData endData = new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1);
+
+		Label label = new Label(parent, SWT.LEFT);
+		label.setText("Connect ID:");
+
+		connectText = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		connectText.setLayoutData(fillData);
+		connectText.addModifyListener(new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				if (!connectText.getText().equals("")) { //$NON-NLS-1$
+					updateStatus(null);
+				} else {
+					updateStatus("An connect ID must be specified.");
+				}
+			}
+		});
+
+		label = new Label(parent, SWT.RIGHT);
+		label.setText("xmpps://<user>@<xmppserver>[:port][/<#channel>]");
+		label.setLayoutData(endData);
+
+		label = new Label(parent, SWT.LEFT);
+		label.setText("Password:");
+		passwordText = new Text(parent, SWT.SINGLE | SWT.PASSWORD | SWT.BORDER);
+		passwordText.setLayoutData(fillData);
+		label = new Label(parent, SWT.RIGHT | SWT.WRAP);
+		label.setText("This password is for password-protected XMPP servers.");
+		label.setLayoutData(endData);
+
+		setControl(parent);
+	}
+
+}
