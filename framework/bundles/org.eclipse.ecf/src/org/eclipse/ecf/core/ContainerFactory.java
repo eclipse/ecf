3344beda8de81a1e7909212d11f825ea6a9ad40f
rsuen <rsuen>
2007-01-22 01:09:03 +0000

    Remove unnecessary casts.

--- framework/bundles/org.eclipse.ecf/src/org/eclipse/ecf/core/ContainerFactory.java
+++ framework/bundles/org.eclipse.ecf/src/org/eclipse/ecf/core/ContainerFactory.java
@@ -1,10 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2004 Composent, Inc. and others. All rights reserved. This
- * program and the accompanying materials are made available under the terms of
- * the Eclipse Public License v1.0 which accompanies this distribution, and is
- * available at http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors: Composent, Inc. - initial API and implementation
+ * Copyright (c) 2004, 2007 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
  ******************************************************************************/
 package org.eclipse.ecf.core;
 
@@ -230,7 +232,7 @@ public class ContainerFactory implements IContainerFactory {
 					+ description.getName() + "' not found", null, method);
 		IContainerInstantiator instantiator = null;
 		try {
-			instantiator = (IContainerInstantiator) cd.getInstantiator();
+			instantiator = cd.getInstantiator();
 		} catch (Exception e) {
 			throwContainerCreateException(
 					"createContainer cannot get IContainerInstantiator for description : "
