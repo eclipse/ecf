cfa266b11400d33ed142bc87c1bdbc68946744a7
rsuen <rsuen>
2007-01-22 01:09:04 +0000

    Remove unnecessary casts.

--- framework/bundles/org.eclipse.ecf.provider/src/org/eclipse/ecf/provider/generic/TCPServerSOContainerGroup.java
+++ framework/bundles/org.eclipse.ecf.provider/src/org/eclipse/ecf/provider/generic/TCPServerSOContainerGroup.java
@@ -1,14 +1,13 @@
-/****************************************************************************
-* Copyright (c) 2004 Composent, Inc. and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Composent, Inc. - initial API and implementation
-*****************************************************************************/
-
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.provider.generic;
 
 import java.io.IOException;
@@ -23,7 +22,6 @@ import java.net.URI;
 import org.eclipse.ecf.core.util.Trace;
 import org.eclipse.ecf.internal.provider.ECFProviderDebugOptions;
 import org.eclipse.ecf.internal.provider.ProviderPlugin;
-import org.eclipse.ecf.provider.comm.IConnectRequestHandler;
 import org.eclipse.ecf.provider.comm.tcp.Client;
 import org.eclipse.ecf.provider.comm.tcp.ConnectRequestMessage;
 import org.eclipse.ecf.provider.comm.tcp.ConnectResultMessage;
@@ -31,125 +29,131 @@ import org.eclipse.ecf.provider.comm.tcp.ISocketAcceptHandler;
 import org.eclipse.ecf.provider.comm.tcp.Server;
 
 public class TCPServerSOContainerGroup extends SOContainerGroup implements
-        ISocketAcceptHandler {
-	
+		ISocketAcceptHandler {
+
 	public static final int DEFAULT_SOCKET_KEEPALIVE = 30000;
-    public static final String INVALID_CONNECT = "Invalid connect request.  ";
-    public static final String DEFAULT_GROUP_NAME = TCPServerSOContainerGroup.class
-            .getName();
-    protected int port;
-    Server listener;
-    boolean isOnTheAir = false;
-    ThreadGroup threadGroup;
-    int socketKeepAlive = DEFAULT_SOCKET_KEEPALIVE;
-    
-    public TCPServerSOContainerGroup(String name, ThreadGroup group, int port, int socketKeepAlive) {
-    	super(name);
-    	threadGroup = group;
-    	this.port = port;
-    	this.socketKeepAlive = socketKeepAlive;
-    }
-    public TCPServerSOContainerGroup(String name, ThreadGroup group, int port) {
-        super(name);
-        threadGroup = group;
-        this.port = port;
-    }
+	public static final String INVALID_CONNECT = "Invalid connect request.  ";
+	public static final String DEFAULT_GROUP_NAME = TCPServerSOContainerGroup.class
+			.getName();
+	protected int port;
+	Server listener;
+	boolean isOnTheAir = false;
+	ThreadGroup threadGroup;
+	int socketKeepAlive = DEFAULT_SOCKET_KEEPALIVE;
 
-    public TCPServerSOContainerGroup(String name, int port) {
-        this(name, null, port);
-    }
+	public TCPServerSOContainerGroup(String name, ThreadGroup group, int port,
+			int socketKeepAlive) {
+		super(name);
+		threadGroup = group;
+		this.port = port;
+		this.socketKeepAlive = socketKeepAlive;
+	}
 
-    public TCPServerSOContainerGroup(int port) {
-        this(DEFAULT_GROUP_NAME, null, port);
-    }
+	public TCPServerSOContainerGroup(String name, ThreadGroup group, int port) {
+		super(name);
+		threadGroup = group;
+		this.port = port;
+	}
+
+	public TCPServerSOContainerGroup(String name, int port) {
+		this(name, null, port);
+	}
+
+	public TCPServerSOContainerGroup(int port) {
+		this(DEFAULT_GROUP_NAME, null, port);
+	}
 
 	protected void debug(String msg) {
-		Trace.trace(ProviderPlugin.getDefault(), ECFProviderDebugOptions.DEBUG,msg);
+		Trace.trace(ProviderPlugin.getDefault(), ECFProviderDebugOptions.DEBUG,
+				msg);
 	}
-	
+
 	protected void traceStack(String msg, Throwable e) {
 		Trace.catching(ProviderPlugin.getDefault(),
-				ECFProviderDebugOptions.EXCEPTIONS_CATCHING, TCPServerSOContainerGroup.class,
-				msg, e);
+				ECFProviderDebugOptions.EXCEPTIONS_CATCHING,
+				TCPServerSOContainerGroup.class, msg, e);
 	}
-	
-    public synchronized void putOnTheAir() throws IOException {
-        debug("group at port " + port + " on the air");
-        listener = new Server(threadGroup, port, this);
-        port = listener.getLocalPort();
-        isOnTheAir = true;
-    }
 
-    public synchronized boolean isOnTheAir() {
-        return isOnTheAir;
-    }
+	public synchronized void putOnTheAir() throws IOException {
+		debug("group at port " + port + " on the air");
+		listener = new Server(threadGroup, port, this);
+		port = listener.getLocalPort();
+		isOnTheAir = true;
+	}
 
-    private void setSocketOptions(Socket aSocket) throws SocketException {
+	public synchronized boolean isOnTheAir() {
+		return isOnTheAir;
+	}
+
+	private void setSocketOptions(Socket aSocket) throws SocketException {
 		aSocket.setTcpNoDelay(true);
-    	if (socketKeepAlive > 0) {
-    		aSocket.setKeepAlive(true);
-    		aSocket.setSoTimeout(socketKeepAlive);
-    	}    	
-    }
-    public void handleAccept(Socket aSocket) throws Exception {
-    	// Set socket options
-    	setSocketOptions(aSocket);
-    	ObjectOutputStream oStream = new ObjectOutputStream(aSocket.getOutputStream());
-        oStream.flush();
-        ObjectInputStream iStream = new ObjectInputStream(aSocket.getInputStream());
-        ConnectRequestMessage req = (ConnectRequestMessage) iStream
-                .readObject();
-        debug("serverrecv:" + req);
-        if (req == null)
-            throw new InvalidObjectException(INVALID_CONNECT
-                    + "ConnectRequestMessage is null");
-        URI uri = req.getTarget();
-        if (uri == null)
-            throw new InvalidObjectException(INVALID_CONNECT
-                    + "Target URI is null");
-        String path = uri.getPath();
-        if (path == null)
-            throw new InvalidObjectException(INVALID_CONNECT
-                    + "Target path is null");
-        TCPServerSOContainer srs = (TCPServerSOContainer) get(path);
-        if (srs == null)
-            throw new InvalidObjectException("Container for target " + path
-                    + " not found!");
-        debug("found container:" + srs.getID().getName() + " for target " + uri);
-        // Create our local messaging interface
-        Client newClient = new Client(aSocket, iStream, oStream, srs
-                .getReceiver(), srs.keepAlive);
-        // No other threads can access messaging interface until space has
-        // accepted/rejected
-        // connect request
-        synchronized (newClient) {
-            // Call checkConnect
-            Serializable resp = (Serializable) ((IConnectRequestHandler) srs)
-                    .handleConnectRequest(aSocket, path, req.getData(), newClient);
-            // Create connect response wrapper and send it back
-            oStream.writeObject(new ConnectResultMessage(resp));
-            oStream.flush();
-        }
-    }
+		if (socketKeepAlive > 0) {
+			aSocket.setKeepAlive(true);
+			aSocket.setSoTimeout(socketKeepAlive);
+		}
+	}
 
-    public synchronized void takeOffTheAir() {
-        if (listener != null) {
-            debug("Taking " + getName() + " off the air.");
-            try {
-                listener.close();
-            } catch (IOException e) {
-                traceStack("Exception in closeListener", e);
-            }
-            listener = null;
-        }
-        isOnTheAir = false;
-    }
+	public void handleAccept(Socket aSocket) throws Exception {
+		// Set socket options
+		setSocketOptions(aSocket);
+		ObjectOutputStream oStream = new ObjectOutputStream(aSocket
+				.getOutputStream());
+		oStream.flush();
+		ObjectInputStream iStream = new ObjectInputStream(aSocket
+				.getInputStream());
+		ConnectRequestMessage req = (ConnectRequestMessage) iStream
+				.readObject();
+		debug("serverrecv:" + req);
+		if (req == null)
+			throw new InvalidObjectException(INVALID_CONNECT
+					+ "ConnectRequestMessage is null");
+		URI uri = req.getTarget();
+		if (uri == null)
+			throw new InvalidObjectException(INVALID_CONNECT
+					+ "Target URI is null");
+		String path = uri.getPath();
+		if (path == null)
+			throw new InvalidObjectException(INVALID_CONNECT
+					+ "Target path is null");
+		TCPServerSOContainer srs = (TCPServerSOContainer) get(path);
+		if (srs == null)
+			throw new InvalidObjectException("Container for target " + path
+					+ " not found!");
+		debug("found container:" + srs.getID().getName() + " for target " + uri);
+		// Create our local messaging interface
+		Client newClient = new Client(aSocket, iStream, oStream, srs
+				.getReceiver(), srs.keepAlive);
+		// No other threads can access messaging interface until space has
+		// accepted/rejected
+		// connect request
+		synchronized (newClient) {
+			// Call checkConnect
+			Serializable resp = srs.handleConnectRequest(aSocket, path, req
+					.getData(), newClient);
+			// Create connect response wrapper and send it back
+			oStream.writeObject(new ConnectResultMessage(resp));
+			oStream.flush();
+		}
+	}
 
-    public int getPort() {
-        return port;
-    }
+	public synchronized void takeOffTheAir() {
+		if (listener != null) {
+			debug("Taking " + getName() + " off the air.");
+			try {
+				listener.close();
+			} catch (IOException e) {
+				traceStack("Exception in closeListener", e);
+			}
+			listener = null;
+		}
+		isOnTheAir = false;
+	}
 
-    public String toString() {
-        return super.toString() + ";port:" + port;
-    }
+	public int getPort() {
+		return port;
+	}
+
+	public String toString() {
+		return super.toString() + ";port:" + port;
+	}
 }
--- framework/bundles/org.eclipse.ecf.provider/src/org/eclipse/ecf/provider/generic/gmm/GMMImpl.java
+++ framework/bundles/org.eclipse.ecf.provider/src/org/eclipse/ecf/provider/generic/gmm/GMMImpl.java
@@ -1,14 +1,13 @@
-/****************************************************************************
-* Copyright (c) 2004 Composent, Inc. and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*    Composent, Inc. - initial API and implementation
-*****************************************************************************/
-
+/*******************************************************************************
+ * Copyright (c) 2004, 2007 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.provider.generic.gmm;
 
 import java.util.Observable;
@@ -64,7 +63,7 @@ public class GMMImpl extends Observable {
         Iterator iter = newSet.iterator();
         int j = 0;
         while (iter.hasNext()) {
-            ids[j++] = (ID) ((Member) iter.next()).getID();
+            ids[j++] = ((Member) iter.next()).getID();
         }
         return ids;
     }
