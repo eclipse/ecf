b7a54d791bbb4646214dc95dc058ab90621b7f84
slewis <slewis>
2007-02-01 18:10:49 +0000

    changed to urlconnection factory test

--- tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/filetransfer/FileTransferNamespaceTest.java
+++ tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/filetransfer/FileTransferNamespaceTest.java
@@ -77,7 +77,21 @@ public class FileTransferNamespaceTest extends ECFAbstractTestCase {
 	}
 
 	public final void testGetURLConnection() throws Exception {
-		URL anURL = new URL("org.eclipse.ecf.tests://lala");
+		URL anURL = new URL("foobar:http://slewis@lala.lala.com:3333/foo/bar/lala.txt?artifact=one&group=two");
+		/*
+		System.out.println("protocol="+anURL.getProtocol());
+		System.out.println("auth="+anURL.getAuthority());
+		System.out.println("defport="+anURL.getDefaultPort());
+		System.out.println("port="+anURL.getPort());
+		System.out.println("host="+anURL.getHost());
+		System.out.println("file="+anURL.getFile());
+		System.out.println("path="+anURL.getPath());
+		System.out.println("ref="+anURL.getRef());
+		System.out.println("query="+anURL.getQuery());
+		System.out.println("userinfo="+anURL.getUserInfo());
+		System.out.println("externalform="+anURL.toExternalForm());
+		System.out.println("tostring="+anURL.toString());
+		*/
 		URLConnection connection = anURL.openConnection();
 		assertNotNull(connection);
 	}
