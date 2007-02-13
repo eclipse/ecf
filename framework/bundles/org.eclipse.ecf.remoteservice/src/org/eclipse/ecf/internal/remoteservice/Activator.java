e3ec10d24def853f3249e27c46d6fd37a044e8f6
slewis <slewis>
2007-02-13 23:42:20 +0000

    String externalization

--- framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/internal/remoteservice/Activator.java
+++ framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/internal/remoteservice/Activator.java
@@ -20,7 +20,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.ecf.remoteservice";
+	public static final String PLUGIN_ID = "org.eclipse.ecf.remoteservice"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
