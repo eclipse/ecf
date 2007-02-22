c4c67b4b93324a2ddfafd226ef938b402082fd37
slewis <slewis>
2007-02-22 00:35:13 +0000

    Initial checkin of org.eclipse.ecf.presence.bot

---
+++ framework/bundles/org.eclipse.ecf.presence.bot/src/org/eclipse/ecf/internal/presence/bot/Activator.java
@@ -0,0 +1,109 @@
+package org.eclipse.ecf.internal.presence.bot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.ecf.presence.bot.handler.ICommandHandler;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.ecf.presence.bot";
+	public static final String COMMAND_HANDLER_EPOINT_NAME = "commandHandler";
+	public static final String COMMAND_HANDLER_EPOINT = PLUGIN_ID + "." + COMMAND_HANDLER_EPOINT_NAME;
+	
+	public static final String BOT_EPOINT_NAME = "bot";
+	public static final String BOT_EPOINT = PLUGIN_ID + "." + BOT_EPOINT_NAME;
+	
+	// The shared instance
+	private static Activator plugin;
+	
+	private Map bots = new HashMap();
+	private Map commands = new HashMap();
+
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		loadExtensions();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	public Map getBots() {
+		return this.bots;
+	}
+
+
+	private void loadExtensions() throws CoreException {
+
+		// load the command handlers
+		IConfigurationElement[] elements = Platform.getExtensionRegistry().getConfigurationElementsFor(COMMAND_HANDLER_EPOINT);
+		for(int i = 0; i < elements.length; i++) {
+			String id = elements[i].getAttribute("botId");
+			String expression = elements[i].getAttribute("expression");
+			ICommandHandler handler = (ICommandHandler) elements[i].createExecutableExtension("class");
+			List c = (List) commands.get(id);
+			if(c == null) {
+				c = new ArrayList();
+				c.add(new CommandEntry(expression, handler));
+				commands.put(id, c);
+			} else {
+				c.add(new CommandEntry(expression, handler));
+				commands.put(id, c);
+			}
+		}
+
+		// load the bots
+		elements = Platform.getExtensionRegistry().getConfigurationElementsFor(BOT_EPOINT);
+		for(int i = 0; i < elements.length; i++) {
+			String id = elements[i].getAttribute("id");
+			String name = elements[i].getAttribute("name");
+			String server = elements[i].getAttribute("server");
+			String channel = elements[i].getAttribute("channel");
+			List c = (List) commands.get(id);
+			if(c == null)
+				c = new ArrayList();
+			IBotEntry bot = new BotEntry(id, name, server, channel, c);
+			bots.put(id, bot);
+		}
+
+	}
+
+
+}
---
+++ framework/bundles/org.eclipse.ecf.presence.bot/src/org/eclipse/ecf/internal/presence/bot/ICommandEntry.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.ecf.internal.presence.bot;
+
+import org.eclipse.ecf.presence.bot.handler.ICommandHandler;
+import org.eclipse.ecf.presence.chatroom.IChatRoomMessageSender;
+
+public interface ICommandEntry {
+	
+	public String getExpression();
+	
+	public ICommandHandler getHandler();
+	
+	public void execute(String message, IChatRoomMessageSender sender);
+
+}
