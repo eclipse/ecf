d91bcdc1b47a8851469c4c7c6502fc3440067210
slewis <slewis>
2007-02-27 17:40:34 +0000

    Initial checkin to support bbapi

---
+++ incubation/bundles/org.eclipse.ecf.bulletinboard.commons/src/org/eclipse/ecf/internal/bulletinboard/commons/AbstractBulletinBoard.java
@@ -0,0 +1,196 @@
+package org.eclipse.ecf.internal.bulletinboard.commons;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ecf.bulletinboard.BBException;
+import org.eclipse.ecf.bulletinboard.IBulletinBoardContainerAdapter;
+import org.eclipse.ecf.bulletinboard.IMember;
+import org.eclipse.ecf.bulletinboard.IMemberGroup;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.internal.bulletinboard.commons.webapp.WebRequest;
+
+public abstract class AbstractBulletinBoard implements
+		IBulletinBoardContainerAdapter {
+
+	private AbstractBBContainer mainContainer;
+
+	protected AbstractParser parser;
+
+	protected URL url;
+
+	protected Namespace namespace;
+
+	protected HttpClient httpClient;
+
+	protected ID loggedInMemberId;
+
+	protected Map<ID, IMember> cachedMembers;
+
+	protected Map<ID, IMemberGroup> cachedMemberGroups;
+
+	public AbstractBulletinBoard(AbstractBBContainer mainContainer) {
+		super();
+		this.mainContainer = mainContainer;
+		this.namespace = mainContainer.getConnectNamespace();
+		reset();
+	}
+
+	protected void reset() {
+		this.url = null;
+		this.cachedMembers = new HashMap<ID, IMember>();
+		this.cachedMemberGroups = new HashMap<ID, IMemberGroup>();
+	}
+
+	public void postConnect() {
+		try {
+			this.url = new URL(getID().toExternalForm());
+			MultiThreadedHttpConnectionManager connectionManager = new MultiThreadedHttpConnectionManager();
+			httpClient = new HttpClient(connectionManager);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void postDisconnect() {
+		httpClient = null;
+		reset();
+	}
+
+	/**
+	 * @deprecated Connection status should be the business of the main
+	 *             container.
+	 */
+	public boolean isConnected() {
+		return getID() != null;
+	}
+
+	/**
+	 * @deprecated Connection status should be the business of the main
+	 *             container.
+	 */
+	public void close() throws BBException {
+		if (isConnected()) {
+			mainContainer.disconnect();
+		} else {
+			throw new BBException("Connection already closed.");
+		}
+	}
+
+	public ID getID() {
+		return mainContainer.getConnectedID();
+	}
+
+	public Object getAdapter(Class adapter) {
+		return Platform.getAdapterManager().getAdapter(this, adapter);
+	}
+
+	public URL getURL() {
+		return url;
+	}
+
+	public Namespace getNamespace() {
+		return namespace;
+	}
+
+	public HttpClient getHttpClient() {
+		return httpClient;
+	}
+
+	public AbstractParser getParser() {
+		return parser;
+	}
+
+	protected abstract WebRequest createMemberPageRequest(ID id);
+
+	public IMember getMember(ID id) throws BBException {
+		if (cachedMembers.containsKey(id)) {
+			return cachedMembers.get(id);
+		} else {
+			WebRequest request = createMemberPageRequest(id);
+
+			try {
+				request.execute();
+				String str = request.getResponseBodyAsString();
+				request.releaseConnection();
+				IMember member = parser.parseMemberPageForName(str, id);
+				if (member != null) {
+					((AbstractBBObject) member).setBulletinBoard(this);
+					cachedMembers.put(member.getID(), member);
+					return member;
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+	}
+
+	public IMember getLoggedInMember() throws BBException {
+		if (loggedInMemberId == null) {
+			return null;
+		}
+		return getMember(loggedInMemberId);
+	}
+
+	protected abstract WebRequest createMemberListRequest();
+
+	public List<IMember> getMembers() throws BBException {
+		// TODO: this only returns first page
+		if (cachedMembers.isEmpty()) {
+			WebRequest request = createMemberListRequest();
+			try {
+				request.execute();
+				String str = request.getResponseBodyAsString();
+				request.releaseConnection();
+				cachedMembers = parser.parseMembers(str);
+				for (IMember member : cachedMembers.values()) {
+					((AbstractBBObject) member).setBulletinBoard(this);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return new ArrayList<IMember>(cachedMembers.values());
+	}
+
+	protected abstract WebRequest createMemberGroupListRequest();
+
+	public Collection<IMemberGroup> getMemberGroups() throws BBException {
+		if (cachedMemberGroups.isEmpty()) {
+			WebRequest request = createMemberGroupListRequest();
+			try {
+				request.execute();
+				String str = request.getResponseBodyAsString();
+				request.releaseConnection();
+				cachedMemberGroups = parser.parseMemberGroups(str);
+				for (IMemberGroup grp : cachedMemberGroups.values()) {
+					((AbstractBBObject) grp).setBulletinBoard(this);
+				}
+			} catch (IOException e) {
+				// log.error(e);
+			}
+		}
+		return new HashSet<IMemberGroup>(cachedMemberGroups.values());
+	}
+
+	public IMemberGroup getMemberGroup(ID id) throws BBException {
+		if (cachedMemberGroups.isEmpty()) {
+			getMemberGroups();
+		}
+		return cachedMemberGroups.get(id);
+	}
+
+}
