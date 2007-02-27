7be76ac9260cc3b01d7eba9db3adb9f2269ca9e7
slewis <slewis>
2007-02-27 17:44:52 +0000

    Initial checkin to support bbapi

---
+++ incubation/bundles/org.eclipse.ecf.provider.vbulletin/src/org/eclipse/ecf/internal/provider/vbulletin/Activator.java
@@ -0,0 +1,55 @@
+package org.eclipse.ecf.internal.provider.vbulletin;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+    public static final String NAMESPACE_IDENTIFIER = "ecf.vbulletin";
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.ecf.internal.provider.vbulletin";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+    public static String getNamespaceIdentifier() {
+    	return NAMESPACE_IDENTIFIER;
+    }
+
+}
