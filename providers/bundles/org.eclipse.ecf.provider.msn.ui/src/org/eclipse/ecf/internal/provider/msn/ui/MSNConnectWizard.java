77a3191120dadf837091d729ebdaee916e57549b
rsuen <rsuen>
2007-03-10 03:20:37 +0000

    Initial commit.

---
+++ providers/bundles/org.eclipse.ecf.provider.msn.ui/src/org/eclipse/ecf/internal/provider/msn/ui/MSNConnectWizard.java
@@ -0,0 +1,159 @@
+/****************************************************************************
+ * Copyright (c) 2007 Remy Suen and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Remy Suen <remy.suen@gmail.com> - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.internal.provider.msn.ui;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.core.IContainerListener;
+import org.eclipse.ecf.core.events.IContainerConnectedEvent;
+import org.eclipse.ecf.core.events.IContainerEvent;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDCreateException;
+import org.eclipse.ecf.core.identity.IDFactory;
+import org.eclipse.ecf.core.security.ConnectContextFactory;
+import org.eclipse.ecf.core.security.IConnectContext;
+import org.eclipse.ecf.core.util.IExceptionHandler;
+import org.eclipse.ecf.presence.IIMMessageEvent;
+import org.eclipse.ecf.presence.IIMMessageListener;
+import org.eclipse.ecf.presence.IPresenceContainerAdapter;
+import org.eclipse.ecf.presence.im.IChatManager;
+import org.eclipse.ecf.presence.im.IChatMessage;
+import org.eclipse.ecf.presence.im.IChatMessageEvent;
+import org.eclipse.ecf.presence.im.IChatMessageSender;
+import org.eclipse.ecf.presence.ui.MessagesView;
+import org.eclipse.ecf.presence.ui.MultiRosterView;
+import org.eclipse.ecf.ui.IConnectWizard;
+import org.eclipse.ecf.ui.actions.AsynchContainerConnectAction;
+import org.eclipse.ecf.ui.dialogs.ContainerConnectErrorDialog;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.PartInitException;
+
+public class MSNConnectWizard extends Wizard implements IConnectWizard {
+
+	private IWorkbench workbench;
+
+	private MSNConnectWizardPage page;
+
+	private IContainer container;
+
+	private IChatMessageSender icms;
+
+	private ID targetID;
+
+	private IConnectContext connectContext;
+
+	private ID userID;
+
+	public void addPages() {
+		page = new MSNConnectWizardPage();
+		addPage(page);
+	}
+
+	public void init(IWorkbench workbench, IContainer container) {
+		this.workbench = workbench;
+		this.container = container;
+	}
+
+	private void openView() {
+		try {
+			MultiRosterView view = (MultiRosterView) workbench
+					.getActiveWorkbenchWindow().getActivePage().showView(
+							MultiRosterView.VIEW_ID);
+			view.addContainer(container);
+		} catch (PartInitException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void displayMessage(IChatMessageEvent e) {
+		final IChatMessage message = e.getChatMessage();
+		Display.getDefault().asyncExec(new Runnable() {
+			public void run() {
+				try {
+					MessagesView view = (MessagesView) workbench
+							.getActiveWorkbenchWindow().getActivePage()
+							.showView(MessagesView.VIEW_ID);
+					view.showMessage(icms, userID, message.getFromID(), message
+							.getThreadID(), message.getBody());
+				} catch (PartInitException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public boolean performFinish() {
+		connectContext = ConnectContextFactory
+				.createPasswordConnectContext(page.getPassword());
+
+		try {
+			targetID = IDFactory.getDefault().createStringID(page.getEmail());
+		} catch (IDCreateException e) {
+			new ContainerConnectErrorDialog(workbench
+					.getActiveWorkbenchWindow().getShell(), 1,
+					"The target ID to connect to could not be created",
+					targetID.getName(), e).open();
+			return true;
+		}
+
+		final IPresenceContainerAdapter adapter = (IPresenceContainerAdapter) container
+				.getAdapter(IPresenceContainerAdapter.class);
+		container.addListener(new IContainerListener() {
+			public void handleEvent(IContainerEvent event) {
+				if (event instanceof IContainerConnectedEvent) {
+					userID = adapter.getRosterManager().getRoster().getUser()
+							.getID();
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
+							openView();
+						}
+					});
+				}
+			}
+		});
+
+		IChatManager icm = adapter.getChatManager();
+		icms = icm.getChatMessageSender();
+
+		icm.addMessageListener(new IIMMessageListener() {
+			public void handleMessageEvent(IIMMessageEvent e) {
+				if (e instanceof IChatMessageEvent) {
+					displayMessage((IChatMessageEvent) e);
+				}
+			}
+		});
+
+		new AsynchContainerConnectAction(container, targetID, connectContext,
+				new IExceptionHandler() {
+					public IStatus handleException(final Throwable exception) {
+						if (exception != null) {
+							exception.printStackTrace();
+							Display.getDefault().asyncExec(new Runnable() {
+								public void run() {
+									new ContainerConnectErrorDialog(workbench
+											.getActiveWorkbenchWindow()
+											.getShell(), 1, "See Details",
+											targetID.getName(), exception)
+											.open();
+								}
+							});
+						}
+						return Status.OK_STATUS;
+					}
+
+				}).run(null);
+
+		return true;
+	}
+}
---
+++ providers/bundles/org.eclipse.ecf.provider.msn.ui/src/org/eclipse/ecf/internal/provider/msn/ui/MSNConnectWizardPage.java
@@ -0,0 +1,87 @@
+/****************************************************************************
+ * Copyright (c) 2007 Remy Suen and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Remy Suen <remy.suen@gmail.com> - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.internal.provider.msn.ui;
+
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+final class MSNConnectWizardPage extends WizardPage {
+
+	private Text emailText;
+
+	private Text passwordText;
+
+	MSNConnectWizardPage() {
+		super("");
+		setTitle("MSN Connection Wizard");
+		setPageComplete(false);
+	}
+
+	private void addListeners() {
+		ModifyListener listener = new ModifyListener() {
+			public void modifyText(ModifyEvent e) {
+				String email = emailText.getText().trim();
+				if (email.equals("")) { //$NON-NLS-1$
+					setErrorMessage("An e-mail address must be entered.");
+				} else if (email.indexOf('@') == -1) {
+					setErrorMessage("A valid e-mail address must be entered.");
+				} else if (passwordText.getText().trim().equals("")) { //$NON-NLS-1$
+					setErrorMessage("A password must be entered.");
+				} else {
+					setErrorMessage(null);
+				}
+			}
+		};
+
+		emailText.addModifyListener(listener);
+		passwordText.addModifyListener(listener);
+	}
+
+	public void createControl(Composite parent) {
+		parent.setLayout(new GridLayout(2, false));
+
+		GridData data = new GridData(SWT.FILL, SWT.CENTER, true, false);
+
+		Label label = new Label(parent, SWT.LEFT);
+		label.setText("E-mail address:");
+		emailText = new Text(parent, SWT.SINGLE | SWT.BORDER);
+		emailText.setLayoutData(data);
+
+		label = new Label(parent, SWT.LEFT);
+		label.setText("Password:");
+		passwordText = new Text(parent, SWT.SINGLE | SWT.PASSWORD | SWT.BORDER);
+		passwordText.setLayoutData(data);
+
+		addListeners();
+		setControl(parent);
+	}
+	
+	String getEmail() {
+		return emailText.getText();
+	}
+	
+	String getPassword() {
+		return passwordText.getText();
+	}
+	
+	public void setErrorMessage(String message) {
+		super.setErrorMessage(message);
+		setPageComplete(message == null);
+	}
+
+}
