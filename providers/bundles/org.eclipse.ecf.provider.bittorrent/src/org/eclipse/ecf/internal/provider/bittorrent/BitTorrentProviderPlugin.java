65e65844dab2673d2d00b3f4b50280a6d94148d4
slewis <slewis>
2007-03-20 21:09:00 +0000

    Changes to deal with bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=176322#c16

--- providers/bundles/org.eclipse.ecf.provider.bittorrent/src/org/eclipse/ecf/internal/provider/bittorrent/BitTorrentProviderPlugin.java
+++ providers/bundles/org.eclipse.ecf.provider.bittorrent/src/org/eclipse/ecf/internal/provider/bittorrent/BitTorrentProviderPlugin.java
@@ -10,19 +10,61 @@
  ******************************************************************************/
 package org.eclipse.ecf.internal.provider.bittorrent;
 
-import org.eclipse.bittorrent.TorrentConfiguration;
-import org.eclipse.core.runtime.Plugin;
-import org.osgi.framework.BundleContext;
+import java.io.File;
 
-public final class BitTorrentProviderPlugin extends Plugin {
+import org.eclipse.bittorrent.TorrentConfiguration;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.util.tracker.ServiceTracker;
+
+public final class BitTorrentProviderPlugin implements BundleActivator {
+
+	/* package */static final String F_META_AREA = ".metadata"; //$NON-NLS-1$
+	/* package */static final String F_PLUGIN_DATA = ".plugins"; //$NON-NLS-1$
 
 	static final String CONTAINER_ID = "ecf.filetransfer.bittorrent"; //$NON-NLS-1$
 
 	static final String NAMESPACE_ID = "ecf.bittorrent"; //$NON-NLS-1$
 
+	private BundleContext context = null;
+
+	private File getConfigurationPath() {
+		Filter filter = null;
+		try {
+			filter = context.createFilter(Location.INSTANCE_FILTER);
+		} catch (InvalidSyntaxException e) {
+			// ignore this. It should never happen as we have tested the above
+			// format.
+		}
+		ServiceTracker instanceLocationTracker = new ServiceTracker(context,
+				filter, null);
+		instanceLocationTracker.open();
+		Location l = (Location) instanceLocationTracker.getService();
+		instanceLocationTracker.close();
+		if (l == null)
+			return null;
+
+		Path path = new Path(l.getURL().getPath());
+		return path.append(F_META_AREA).append(F_PLUGIN_DATA)
+				.append(context.getBundle().getSymbolicName()).toFile();
+	}
+
 	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		TorrentConfiguration.setConfigurationPath(getStateLocation().toFile());
+		this.context = context;
+		TorrentConfiguration.setConfigurationPath(getConfigurationPath());
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		this.context = null;
 	}
 
 }
