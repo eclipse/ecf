4c0c47c56697821d31f0abcdc7d0f04a1b6078fb
slewis <slewis>
2007-03-30 05:36:36 +0000

    Added IMBot and immessagehandlerextension points, interfaces, and implementation classes.

---
+++ framework/bundles/org.eclipse.ecf.presence.bot/src/org/eclipse/ecf/presence/bot/DefaultIMMessageHandler.java
@@ -0,0 +1,48 @@
+/****************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.ecf.presence.bot;
+
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.presence.im.IChatMessage;
+
+/**
+ * Default im message handler that does nothing in response to
+ * notifications.
+ */
+public class DefaultIMMessageHandler implements IIMMessageHandler {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ecf.presence.bot.handler.IIMMessageHandler#handleRoomMessage(org.eclipse.ecf.presence.im.IChatMessage)
+	 */
+	public void handleIMMessage(IChatMessage message) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ecf.presence.bot.handler.IContainerAdvisor#init(org.eclipse.ecf.core.IContainer)
+	 */
+	public void init(IContainer container) {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ecf.presence.bot.handler.IContainerAdvisor#preContainerConnect(org.eclipse.ecf.core.identity.ID)
+	 */
+	public void preContainerConnect(ID targetID) {
+	}
+
+}
