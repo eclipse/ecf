25222bd1fa701cc52321792a1123bfa8d82b0d36
slewis <slewis>
2007-04-09 17:01:33 +0000

    Added remote service service

---
+++ framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/service/IRemoteServiceService.java
@@ -0,0 +1,21 @@
+/****************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.ecf.remoteservice.service;
+
+import org.eclipse.ecf.remoteservice.IRemoteServiceContainerAdapter;
+
+/**
+ * Remote Service OSGI service.
+ */
+public interface IRemoteServiceService extends IRemoteServiceContainerAdapter {
+
+}
