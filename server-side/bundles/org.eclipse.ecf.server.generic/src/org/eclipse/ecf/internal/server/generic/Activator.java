7ede54a73e30a6a9b573293885a3b5238a1ae662
slewis <slewis>
2007-04-20 22:29:48 +0000

    Changes to have all activator classes return non-null with calls to getDefault(), even in non-OSGi environments

--- server-side/bundles/org.eclipse.ecf.server.generic/src/org/eclipse/ecf/internal/server/generic/Activator.java
+++ server-side/bundles/org.eclipse.ecf.server.generic/src/org/eclipse/ecf/internal/server/generic/Activator.java
@@ -116,7 +116,10 @@ public class Activator implements BundleActivator {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public synchronized static Activator getDefault() {
+		if (plugin == null) {
+			plugin = new Activator();
+		}
 		return plugin;
 	}
 
