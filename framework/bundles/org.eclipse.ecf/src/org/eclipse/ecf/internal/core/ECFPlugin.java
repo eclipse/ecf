78607eebb237c28115603f44bbb3331fcaad50d8
slewis <slewis>
2007-04-20 16:42:05 +0000

    Changed back initialization of plugin

--- framework/bundles/org.eclipse.ecf/src/org/eclipse/ecf/internal/core/ECFPlugin.java
+++ framework/bundles/org.eclipse.ecf/src/org/eclipse/ecf/internal/core/ECFPlugin.java
@@ -107,10 +107,6 @@ public class ECFPlugin implements BundleActivator {
 		return adapterManager;
 	}
 
-	static {
-		plugin = new ECFPlugin();
-	}
-	
 	public ECFPlugin() {
 	}
 
@@ -369,6 +365,7 @@ public class ECFPlugin implements BundleActivator {
 	 * This method is called upon plug-in activation
 	 */
 	public void start(BundleContext context) throws Exception {
+		plugin = this;
 		this.context = context;
 		this.extensionRegistryTracker = new ServiceTracker(context,
 				IExtensionRegistry.class.getName(), null);
