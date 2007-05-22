ffb55dd11a39afa55dfd4ad79f7e833747795bd3
slewis <slewis>
2007-05-22 20:53:08 +0000

    Changed icons with new ones for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=183015.  Also updated and added to code references.

--- providers/bundles/org.eclipse.ecf.provider.msn.ui/src/org/eclipse/ecf/internal/provider/msn/ui/MSNConnectWizardPage.java
+++ providers/bundles/org.eclipse.ecf.provider.msn.ui/src/org/eclipse/ecf/internal/provider/msn/ui/MSNConnectWizardPage.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.ecf.ui.SharedImages;
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
@@ -39,6 +40,7 @@ final class MSNConnectWizardPage extends WizardPage {
 		setTitle(Messages.MSNConnectWizardPage_Title);
 		setDescription(Messages.MSNConnectWizardPage_WIZARD_PAGE_DESCRIPTION);
 		setPageComplete(false);
+		setImageDescriptor(SharedImages.getImageDescriptor(SharedImages.IMG_CHAT_WIZARD));
 	}
 
 	MSNConnectWizardPage(String username) {
