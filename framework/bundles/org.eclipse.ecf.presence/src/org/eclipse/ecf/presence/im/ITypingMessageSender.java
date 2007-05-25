a829da6a1b2b0058144e1da892e68b78592a4715
rsuen <rsuen>
2007-05-25 20:47:51 +0000

    Correct the javadocs.

--- framework/bundles/org.eclipse.ecf.presence/src/org/eclipse/ecf/presence/im/ITypingMessageSender.java
+++ framework/bundles/org.eclipse.ecf.presence/src/org/eclipse/ecf/presence/im/ITypingMessageSender.java
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (c) 2004 Composent, Inc. and others.
+ * Copyright (c) 2004, 2007 Composent, Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -34,8 +34,8 @@ public interface ITypingMessageSender {
 	 *            <code>null</code>.
 	 * 
 	 * @throws ECFException
-	 *             thrown if toID or typingMessage parameters are
-	 *             <code>null</code>, or if provider not currently connected.
+	 *             thrown if toID is <code>null</code>, or if provider not
+	 *             currently connected.
 	 */
 	public void sendTypingMessage(ID toID, boolean isTyping, String body)
 			throws ECFException;
