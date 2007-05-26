6d2c794adf9e8c68463fa3e6bcd6e66bc57b2f97
rsuen <rsuen>
2007-05-26 02:12:18 +0000

    Add a license header.

--- examples/bundles/org.eclipse.ecf.example.collab.editor/src/org/eclipse/ecf/example/collab/editor/message/AbstractMessage.java
+++ examples/bundles/org.eclipse.ecf.example.collab.editor/src/org/eclipse/ecf/example/collab/editor/message/AbstractMessage.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2006 Ken Gilmer. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Ken Gilmer - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.ecf.example.collab.editor.message;
 
 import java.io.ByteArrayOutputStream;
