f9b22f757829d77fc12b11cb679780ce54f7be52
slewis <slewis>
2007-05-29 20:39:43 +0000

    Moved RosterViewerDropTargetText to internal package

--- framework/bundles/org.eclipse.ecf.presence.collab.ui/src/org/eclipse/ecf/internal/presence/collab/ui/RosterViewerDropTargetText.java
+++ framework/bundles/org.eclipse.ecf.presence.collab.ui/src/org/eclipse/ecf/internal/presence/collab/ui/RosterViewerDropTargetText.java
@@ -1,4 +1,4 @@
-package org.eclipse.ecf.presence.collab.ui;
+package org.eclipse.ecf.internal.presence.collab.ui;
 
 import org.eclipse.ecf.presence.roster.IRosterEntry;
 import org.eclipse.ecf.presence.roster.IRosterItem;
