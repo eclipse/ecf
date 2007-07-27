aa46dced08abff3a24e6cbba7f6a5f56a6d10752
caniszczyk <caniszczyk>
2007-07-27 14:48:41 +0000

    bug 192574: Communications preference page isn't empty enough https://bugs.eclipse.org/bugs/show_bug.cgi?id=192574

--- framework/bundles/org.eclipse.ecf.ui/src/org/eclipse/ecf/internal/ui/CategoryPreferencePage.java
+++ framework/bundles/org.eclipse.ecf.ui/src/org/eclipse/ecf/internal/ui/CategoryPreferencePage.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Chris Aniszczyk and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Chris Aniszczyk <caniszczyk@gmail.com> - initial API and implementation
+ *    Jörn Dinkla <devnull@dinkla.com> - bug 192574
+ ******************************************************************************/
 package org.eclipse.ecf.internal.ui;
 
 import org.eclipse.jface.preference.PreferencePage;
@@ -22,8 +33,8 @@ public class CategoryPreferencePage extends PreferencePage implements
 	}
 
 	protected Control createContents(Composite parent) {
+		noDefaultAndApplyButton();
 		return null;
-
 	}
 
 	public void init(IWorkbench workbench) {
