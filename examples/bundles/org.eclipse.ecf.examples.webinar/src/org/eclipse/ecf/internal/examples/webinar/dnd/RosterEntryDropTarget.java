b8d15bc90ffa94ddcd5629b9575fe9938dc46782
slewis <slewis>
2007-07-30 22:48:55 +0000

    roster entry drop target addition

--- examples/bundles/org.eclipse.ecf.examples.webinar/src/org/eclipse/ecf/internal/examples/webinar/dnd/RosterEntryDropTarget.java
+++ examples/bundles/org.eclipse.ecf.examples.webinar/src/org/eclipse/ecf/internal/examples/webinar/dnd/RosterEntryDropTarget.java
@@ -5,6 +5,7 @@ import org.eclipse.ecf.presence.roster.IRosterItem;
 import org.eclipse.ecf.presence.ui.dnd.IRosterViewerDropTarget;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.dnd.TransferData;
+import org.eclipse.ui.PlatformUI;
 
 public class RosterEntryDropTarget implements IRosterViewerDropTarget {
 
@@ -27,9 +28,8 @@ public class RosterEntryDropTarget implements IRosterViewerDropTarget {
 		if (data instanceof String) {
 			// Right here, send data to channel
 			// sendString(rosterEntry.getUser().getID(),(String) data);
-			MessageDialog.openInformation(null, "Dropped on Contact",
-					"The following text was dropped on "
-							+ rosterEntry.getUser().getName() + ":\n\n" + data);
+			MessageDialog.openInformation(PlatformUI.getWorkbench().getDisplay().getActiveShell(),"Roster Drop","The following text was dropped on "
+					+ rosterEntry.getUser().getName() + ":\n\n" + data);
 			return true;
 		}
 		return false;
