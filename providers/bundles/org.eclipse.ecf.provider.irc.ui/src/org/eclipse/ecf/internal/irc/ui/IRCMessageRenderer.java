4bcd3951221491980185776c174ad33c6874baca
rsuen <rsuen>
2007-09-02 02:17:38 +0000

    Allow the messages in the chat room view to be customizable for bug 190851 (Allow to customize fonts and colors for all messages views).

--- providers/bundles/org.eclipse.ecf.provider.irc.ui/src/org/eclipse/ecf/internal/irc/ui/IRCMessageRenderer.java
+++ providers/bundles/org.eclipse.ecf.provider.irc.ui/src/org/eclipse/ecf/internal/irc/ui/IRCMessageRenderer.java
@@ -6,15 +6,21 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Jacek Pospychala <jacek.pospychala@pl.ibm.com> - bug 197329
+ *    Jacek Pospychala <jacek.pospychala@pl.ibm.com> - bug 197329, 190851
  *****************************************************************************/
 package org.eclipse.ecf.internal.irc.ui;
 
 import org.eclipse.ecf.presence.ui.chatroom.MessageRenderer;
-import org.eclipse.swt.SWT;
 
 public class IRCMessageRenderer extends MessageRenderer {
 
+	/**
+	 * Messages sent by local user using /me command
+	 */
+	protected static final String IRC_ME_COLOR = "org.eclipse.ecf.presence.irc.ui.meColor"; //$NON-NLS-1$
+	protected static final String IRC_ME_FONT = "org.eclipse.ecf.presence.irc.ui.meFont"; //$NON-NLS-1$
+	
+	
 	private final static String ME_PREFIX = "\01ACTION "; //$NON-NLS-1$
 	private final static String ME_SUFFIX = "\01"; //$NON-NLS-1$
 
@@ -34,7 +40,7 @@ public class IRCMessageRenderer extends MessageRenderer {
 	protected void appendNickname() {
 		if (isActionMessage) {
 			String message = originator + " "; //$NON-NLS-1$
-			append(message, null, null, SWT.ITALIC);
+			append(message, IRC_ME_COLOR, null, IRC_ME_FONT);
 		} else {
 			super.appendNickname();
 		}
@@ -42,7 +48,7 @@ public class IRCMessageRenderer extends MessageRenderer {
 	
 	protected void appendMessage() {
 		if (isActionMessage) {
-			append(message, null, null, SWT.ITALIC);
+			append(message, IRC_ME_COLOR, null, IRC_ME_FONT);
 		} else {
 			super.appendMessage();
 		}
