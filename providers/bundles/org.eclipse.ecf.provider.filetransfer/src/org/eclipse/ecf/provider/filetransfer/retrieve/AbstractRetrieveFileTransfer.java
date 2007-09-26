3b8b1823e04e6495f258d3115cae7d6e5e2e456b
slewis <slewis>
2007-09-26 04:18:58 +0000

    Added pause and resume support to api (receive paused event/receiveresumedevent)

--- providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/retrieve/AbstractRetrieveFileTransfer.java
+++ providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/retrieve/AbstractRetrieveFileTransfer.java
@@ -8,6 +8,9 @@
  ******************************************************************************/
 package org.eclipse.ecf.provider.filetransfer.retrieve;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -30,6 +33,9 @@ import org.eclipse.ecf.filetransfer.IncomingFileTransferException;
 import org.eclipse.ecf.filetransfer.UserCancelledException;
 import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceiveDataEvent;
 import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceiveDoneEvent;
+import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceivePausedEvent;
+import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceiveResumedEvent;
+import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceiveStartEvent;
 import org.eclipse.ecf.filetransfer.identity.IFileID;
 import org.eclipse.ecf.filetransfer.service.IRetrieveFileTransfer;
 import org.eclipse.ecf.internal.provider.filetransfer.Activator;
@@ -69,6 +75,8 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 
 	protected Map options = null;
 
+	protected boolean paused = false;
+
 	protected URL getRemoteFileURL() {
 		return remoteFileURL;
 	}
@@ -103,14 +111,14 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 		}
 
 		protected IStatus run(IProgressMonitor monitor) {
-			byte[] buf = new byte[buff_length];
-			int totalWork = ((fileLength == -1) ? 100 : (int) fileLength);
+			final byte[] buf = new byte[buff_length];
+			final int totalWork = ((fileLength == -1) ? 100 : (int) fileLength);
 			monitor.beginTask(getRemoteFileURL().toString() + Messages.AbstractRetrieveFileTransfer_Progress_Data, totalWork);
 			try {
-				while (!isDone()) {
+				while (!isDone() && !isPaused()) {
 					if (monitor.isCanceled())
 						throw new UserCancelledException(Messages.AbstractRetrieveFileTransfer_Exception_User_Cancelled);
-					int bytes = remoteFileContents.read(buf);
+					final int bytes = remoteFileContents.read(buf);
 					if (bytes != -1) {
 						bytesReceived += bytes;
 						localFileContents.write(buf, 0, bytes);
@@ -123,13 +131,16 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 						}
 					}
 				}
-			} catch (Exception e) {
+			} catch (final Exception e) {
 				exception = e;
 				done = true;
 			} finally {
 				hardClose();
 				monitor.done();
-				fireTransferReceiveDoneEvent();
+				if (isPaused())
+					fireTransferReceivePausedEvent();
+				else
+					fireTransferReceiveDoneEvent();
 			}
 			return getFinalStatus(exception);
 		}
@@ -147,20 +158,36 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 
 	protected void hardClose() {
 		try {
-			if (remoteFileContents != null) {
+			if (remoteFileContents != null)
 				remoteFileContents.close();
-			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 		}
 		try {
-			if (localFileContents != null && closeOutputStream) {
+			if (localFileContents != null && closeOutputStream)
 				localFileContents.close();
-			}
-		} catch (IOException e) {
+		} catch (final IOException e) {
 		}
 		job = null;
 		remoteFileContents = null;
 		localFileContents = null;
+	}
+
+	protected void fireTransferReceivePausedEvent() {
+		listener.handleTransferEvent(new IIncomingFileTransferReceivePausedEvent() {
+
+			private static final long serialVersionUID = -1317411290525985140L;
+
+			public IIncomingFileTransfer getSource() {
+				return AbstractRetrieveFileTransfer.this;
+			}
+
+			public String toString() {
+				final StringBuffer sb = new StringBuffer("IIncomingFileTransferReceivePausedEvent["); //$NON-NLS-1$
+				sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
+						.append("fileLength=").append(fileLength).append("]"); //$NON-NLS-1$ //$NON-NLS-2$
+				return sb.toString();
+			}
+		});
 	}
 
 	protected void fireTransferReceiveDoneEvent() {
@@ -177,7 +204,7 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 			}
 
 			public String toString() {
-				StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveDoneEvent["); //$NON-NLS-1$
+				final StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveDoneEvent["); //$NON-NLS-1$
 				sb.append("isDone=").append(done).append(";"); //$NON-NLS-1$ //$NON-NLS-2$
 				sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
 						.append("]"); //$NON-NLS-1$
@@ -195,12 +222,12 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 			}
 
 			public String toString() {
-				StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveDataEvent["); //$NON-NLS-1$
+				final StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveDataEvent["); //$NON-NLS-1$
 				sb.append("isDone=").append(done).append(";"); //$NON-NLS-1$ //$NON-NLS-2$
 				sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
 						.append(";"); //$NON-NLS-1$
 				sb.append("percentComplete=").append( //$NON-NLS-1$
-						getPercentComplete() * 100).append("]"); //$NON-NLS-1$
+						getPercentComplete()).append("]"); //$NON-NLS-1$
 				return sb.toString();
 			}
 		});
@@ -211,7 +238,12 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 	}
 
 	public void cancel() {
-		if (job != null)
+		if (isPaused()) {
+			done = true;
+			this.exception = new UserCancelledException(Messages.AbstractRetrieveFileTransfer_Exception_User_Cancelled);
+			hardClose();
+			fireTransferReceiveDoneEvent();
+		} else if (job != null)
 			job.cancel();
 	}
 
@@ -219,8 +251,14 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 		return exception;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.filetransfer.IFileTransfer#getPercentComplete()
+	 */
 	public double getPercentComplete() {
-		return ((double) bytesReceived / (double) fileLength);
+		if (fileLength == -1 || fileLength == 0)
+			return fileLength;
+		else
+			return ((double) bytesReceived / (double) fileLength);
 	}
 
 	public boolean isDone() {
@@ -233,13 +271,16 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 		if (adapter.isInstance(this)) {
 			return this;
 		} else {
-			IAdapterManager adapterManager = Activator.getDefault().getAdapterManager();
+			final IAdapterManager adapterManager = Activator.getDefault().getAdapterManager();
 			return (adapterManager == null) ? null : adapterManager.loadAdapter(this, adapter.getName());
 		}
 	}
 
 	/**
-	 * Open incoming and outgoing streams associated with this file transfer.
+	 * Open incoming and outgoing streams associated with this file transfer.  Subclasses must
+	 * implement this method to open input and output streams.  The <code>remoteFileContents</code>
+	 * and <code>localFileContent</code> must be non-<code>null</code> after successful completion
+	 * of the implementation of this method.
 	 * 
 	 * @throws IncomingFileTransferException
 	 */
@@ -263,7 +304,7 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 
 		try {
 			this.remoteFileURL = remoteFileID.getURL();
-		} catch (MalformedURLException e) {
+		} catch (final MalformedURLException e) {
 			throw new IncomingFileTransferException(NLS.bind(Messages.AbstractRetrieveFileTransfer_MalformedURLException, remoteFileID), e);
 		}
 		this.listener = transferListener;
@@ -274,41 +315,120 @@ public abstract class AbstractRetrieveFileTransfer implements IIncomingFileTrans
 		return IDFactory.getDefault().getNamespaceByName(FileTransferNamespace.PROTOCOL);
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.filetransfer.IFileTransferPausable#isPaused()
+	 */
 	public boolean isPaused() {
-		if (job == null)
-			return false;
-		else
-			synchronized (job) {
-				if (job.getState() == Job.SLEEPING)
-					return true;
-				return false;
-			}
+		return paused;
 	}
 
+	/**
+	 * Subclass overridable version of {@link #pause()}.  Subclasses must provide
+	 * an implementation of this method to support {@link IFileTransferPausable}.
+	 * @return true if the pause is successful.  <code>false</code> otherwise.
+	 */
+	protected abstract boolean doPause();
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.filetransfer.IFileTransferPausable#pause()
+	 */
 	public boolean pause() {
-		if (job == null)
-			return false;
-		else
-			synchronized (job) {
-				return job.sleep();
-			}
+		return doPause();
 	}
 
+	/**
+	 * Subclass overridable version of {@link #resume()}.  Subclasses must provide
+	 * an implementation of this method to support {@link IFileTransferPausable}.
+	 * @return true if the resume is successful.  <code>false</code> otherwise.
+	 */
+	protected abstract boolean doResume();
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.filetransfer.IFileTransferPausable#resume()
+	 */
 	public boolean resume() {
-		if (job == null)
-			return false;
-		else
-			synchronized (job) {
-				if (job.getState() == Job.SLEEPING) {
-					job.wakeUp();
-					return true;
-				} else
-					return false;
-			}
+		return doResume();
 	}
 
 	public IFileTransferListener getListener() {
 		return listener;
+	}
+
+	protected void fireReceiveStartEvent() {
+		listener.handleTransferEvent(new IIncomingFileTransferReceiveStartEvent() {
+			private static final long serialVersionUID = -59096575294481755L;
+
+			public IFileID getFileID() {
+				return remoteFileID;
+			}
+
+			public IIncomingFileTransfer receive(File localFileToSave) throws IOException {
+				setOutputStream(new BufferedOutputStream(new FileOutputStream(localFileToSave)));
+				job = new FileTransferJob(getRemoteFileURL().toString());
+				job.schedule();
+				return AbstractRetrieveFileTransfer.this;
+			}
+
+			public String toString() {
+				final StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveStartEvent["); //$NON-NLS-1$
+				sb.append("isdone=").append(done).append(";"); //$NON-NLS-1$ //$NON-NLS-2$
+				sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
+						.append("]"); //$NON-NLS-1$
+				return sb.toString();
+			}
+
+			public void cancel() {
+				hardClose();
+			}
+
+			public IIncomingFileTransfer receive(OutputStream streamToStore) throws IOException {
+				setOutputStream(streamToStore);
+				setCloseOutputStream(false);
+				job = new FileTransferJob(getRemoteFileURL().toString());
+				job.schedule();
+				return AbstractRetrieveFileTransfer.this;
+			}
+
+		});
+	}
+
+	protected void fireReceiveResumedEvent() {
+		listener.handleTransferEvent(new IIncomingFileTransferReceiveResumedEvent() {
+
+			private static final long serialVersionUID = 7111739642849612839L;
+
+			public IFileID getFileID() {
+				return remoteFileID;
+			}
+
+			public IIncomingFileTransfer receive(File localFileToSave) throws IOException {
+				setOutputStream(new BufferedOutputStream(new FileOutputStream(localFileToSave)));
+				job = new FileTransferJob(getRemoteFileURL().toString());
+				job.schedule();
+				return AbstractRetrieveFileTransfer.this;
+			}
+
+			public String toString() {
+				final StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveResumedEvent["); //$NON-NLS-1$
+				sb.append("isdone=").append(done).append(";"); //$NON-NLS-1$ //$NON-NLS-2$
+				sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
+						.append("]"); //$NON-NLS-1$
+				return sb.toString();
+			}
+
+			public void cancel() {
+				hardClose();
+			}
+
+			public IIncomingFileTransfer receive(OutputStream streamToStore) throws IOException {
+				setOutputStream(streamToStore);
+				setCloseOutputStream(false);
+				job = new FileTransferJob(getRemoteFileURL().toString());
+				job.schedule();
+				return AbstractRetrieveFileTransfer.this;
+			}
+
+		});
 	}
 
 }
--- providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/retrieve/UrlConnectionRetrieveFileTransfer.java
+++ providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/retrieve/UrlConnectionRetrieveFileTransfer.java
@@ -8,24 +8,22 @@
  ******************************************************************************/
 package org.eclipse.ecf.provider.filetransfer.retrieve;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.security.IConnectContext;
 import org.eclipse.ecf.core.util.Proxy;
-import org.eclipse.ecf.filetransfer.IIncomingFileTransfer;
+import org.eclipse.ecf.filetransfer.IFileTransferPausable;
 import org.eclipse.ecf.filetransfer.IncomingFileTransferException;
-import org.eclipse.ecf.filetransfer.events.IIncomingFileTransferReceiveStartEvent;
 import org.eclipse.ecf.filetransfer.identity.IFileID;
 import org.eclipse.ecf.provider.filetransfer.identity.FileTransferID;
 
 public class UrlConnectionRetrieveFileTransfer extends AbstractRetrieveFileTransfer {
+
+	private static final int HTTP_RANGE_RESPONSE = 206;
 
 	protected URLConnection urlConnection;
 
@@ -36,60 +34,92 @@ public class UrlConnectionRetrieveFileTransfer extends AbstractRetrieveFileTrans
 
 	protected IFileID fileid = null;
 
+	protected long lastModifiedTime = 0L;
+
+	protected int httpVersion = 1;
+
+	protected int responseCode = -1;
+
+	protected String responseMessage = null;
+
+	protected void connect(URL theURL) throws IOException {
+		urlConnection = theURL.openConnection();
+	}
+
+	protected boolean isConnected() {
+		return (urlConnection != null);
+	}
+
+	protected void setResumeRequestHeaderValues() throws IOException {
+		if (this.bytesReceived <= 0 || this.fileLength <= this.bytesReceived)
+			throw new IOException("resume start error");
+		urlConnection.setRequestProperty("Range:", "bytes=" + this.bytesReceived + "-");
+	}
+
+	public int getResponseCode() {
+		if (responseCode != -1)
+			return responseCode;
+		final String response = urlConnection.getHeaderField(0);
+		if (response == null) {
+			responseCode = -1;
+			httpVersion = 1;
+			return responseCode;
+		}
+		if (response == null || !response.startsWith("HTTP/")) //$NON-NLS-1$
+			return -1;
+		response.trim();
+		final int mark = response.indexOf(" ") + 1; //$NON-NLS-1$
+		if (mark == 0)
+			return -1;
+		if (response.charAt(mark - 2) != '1')
+			httpVersion = 0;
+		int last = mark + 3;
+		if (last > response.length())
+			last = response.length();
+		responseCode = Integer.parseInt(response.substring(mark, last));
+		if (last + 1 <= response.length())
+			responseMessage = response.substring(last + 1);
+		return responseCode;
+	}
+
+	private boolean isHTTP11() {
+		return (httpVersion >= 1);
+	}
+
+	protected void getResponseHeaderValues(URL theURL) throws IOException {
+		if (!isConnected())
+			throw new ConnectException("not connected");
+		if (getResponseCode() == -1)
+			throw new IOException("invalid server response");
+		lastModifiedTime = urlConnection.getLastModified();
+		setFileLength(urlConnection.getContentLength());
+		fileid = new FileTransferID(getRetrieveNamespace(), theURL);
+	}
+
+	protected void getResumeResponseHeaderValues(URL theURL) throws IOException {
+		if (!isConnected())
+			throw new ConnectException("not connected");
+		if (getResponseCode() != HTTP_RANGE_RESPONSE)
+			throw new IOException("invalid server response to partial range request");
+		if (lastModifiedTime != urlConnection.getLastModified())
+			throw new IOException("file modified since last access");
+	}
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.ecf.provider.filetransfer.retrieve.AbstractRetrieveFileTransfer#openStreams()
 	 */
 	protected void openStreams() throws IncomingFileTransferException {
-		URL theURL = null;
-
+		final URL theURL = getRemoteFileURL();
 		try {
-			theURL = getRemoteFileURL();
-			urlConnection = theURL.openConnection();
+			connect(theURL);
+			// Make actual GET request
 			setInputStream(urlConnection.getInputStream());
-			setFileLength(urlConnection.getContentLength());
-
-			fileid = new FileTransferID(getRetrieveNamespace(), theURL);
-
-			listener.handleTransferEvent(new IIncomingFileTransferReceiveStartEvent() {
-				private static final long serialVersionUID = -59096575294481755L;
-
-				public IFileID getFileID() {
-					return remoteFileID;
-				}
-
-				public IIncomingFileTransfer receive(File localFileToSave) throws IOException {
-					setOutputStream(new BufferedOutputStream(new FileOutputStream(localFileToSave)));
-					job = new FileTransferJob(getRemoteFileURL().toString());
-					job.schedule();
-					return UrlConnectionRetrieveFileTransfer.this;
-				}
-
-				public String toString() {
-					StringBuffer sb = new StringBuffer("IIncomingFileTransferReceiveStartEvent["); //$NON-NLS-1$
-					sb.append("isdone=").append(done).append(";"); //$NON-NLS-1$ //$NON-NLS-2$
-					sb.append("bytesReceived=").append(bytesReceived) //$NON-NLS-1$
-							.append("]"); //$NON-NLS-1$
-					return sb.toString();
-				}
-
-				public void cancel() {
-					hardClose();
-				}
-
-				public IIncomingFileTransfer receive(OutputStream streamToStore) throws IOException {
-					setOutputStream(streamToStore);
-					setCloseOutputStream(false);
-					job = new FileTransferJob(getRemoteFileURL().toString());
-					job.schedule();
-					return UrlConnectionRetrieveFileTransfer.this;
-				}
-
-			});
-		} catch (Exception e) {
+			getResponseHeaderValues(theURL);
+			fireReceiveStartEvent();
+		} catch (final Exception e) {
 			throw new IncomingFileTransferException("Exception connecting to " //$NON-NLS-1$
 					+ theURL.toString(), e);
 		}
-
 	}
 
 	/*
@@ -129,4 +159,63 @@ public class UrlConnectionRetrieveFileTransfer extends AbstractRetrieveFileTrans
 		return fileid;
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.provider.filetransfer.retrieve.AbstractRetrieveFileTransfer#doPause()
+	 */
+	protected boolean doPause() {
+		if (isPaused() || !isConnected() || isDone())
+			return false;
+		hardClose();
+		this.paused = true;
+		return this.paused;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.provider.filetransfer.retrieve.AbstractRetrieveFileTransfer#doResume()
+	 */
+	protected boolean doResume() {
+		if (!isPaused() || isConnected() || isDone())
+			return false;
+		return openStreamsForResume();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.provider.filetransfer.retrieve.AbstractRetrieveFileTransfer#getAdapter(java.lang.Class)
+	 */
+	public Object getAdapter(Class adapter) {
+		if (adapter != null)
+			return null;
+		if (adapter.equals(IFileTransferPausable.class)) {
+			final String protocol = getRemoteFileURL().getProtocol();
+			if ((protocol.equals("http") || protocol.equals("https")) && isHTTP11())
+				return this;
+			else
+				return null;
+		} else
+			return super.getAdapter(adapter);
+	}
+
+	/**
+	 * @return
+	 */
+	private boolean openStreamsForResume() {
+		final URL theURL = getRemoteFileURL();
+		try {
+			connect(theURL);
+			setResumeRequestHeaderValues();
+			// Make actual GET request
+			setInputStream(urlConnection.getInputStream());
+			getResumeResponseHeaderValues(theURL);
+			this.paused = false;
+			fireReceiveResumedEvent();
+			return true;
+		} catch (final Exception e) {
+			this.exception = e;
+			this.done = true;
+			this.paused = false;
+			hardClose();
+			fireTransferReceiveDoneEvent();
+			return false;
+		}
+	}
 }
