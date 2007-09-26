bf7f57c1ba524fc630ac04c33a7c1809cb268c5c
slewis <slewis>
2007-09-26 04:18:57 +0000

    Added pause and resume support to api (receive paused event/receiveresumedevent)

---
+++ framework/bundles/org.eclipse.ecf.filetransfer/src/org/eclipse/ecf/filetransfer/events/IIncomingFileTransferReceiveResumedEvent.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2004 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Composent, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.filetransfer.events;
+
+import org.eclipse.ecf.filetransfer.IFileTransferListener;
+import org.eclipse.ecf.filetransfer.IIncomingFileTransfer;
+
+/**
+ * Event sent to {@link IFileTransferListener} associated with
+ * {@link IIncomingFileTransfer} instances when file transfer
+ * is resumed.
+ * 
+ */
+public interface IIncomingFileTransferReceiveResumedEvent extends IIncomingFileTransferReceiveStartEvent {
+
+}
