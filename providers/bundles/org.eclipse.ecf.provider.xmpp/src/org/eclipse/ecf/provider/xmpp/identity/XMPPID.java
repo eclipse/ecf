7fe55d66dfd3cfa09166bbdc706d41e5d0400727
slewis <slewis>
2007-10-10 20:53:41 +0000

    Fix for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=205967

--- providers/bundles/org.eclipse.ecf.provider.xmpp/src/org/eclipse/ecf/provider/xmpp/identity/XMPPID.java
+++ providers/bundles/org.eclipse.ecf.provider.xmpp/src/org/eclipse/ecf/provider/xmpp/identity/XMPPID.java
@@ -24,56 +24,64 @@ public class XMPPID extends BaseID implements IChatID {
 	public static final char USER_HOST_DELIMITER = '@';
 	public static final char PORT_DELIMITER = ':';
 	public static final char PATH_DELIMITER = '/';
-	
+
 	URI uri;
 	String username;
 	String hostname;
 	String resourcename = ECFConnection.CLIENT_TYPE;
 	int port = -1;
-	
+
 	protected static String fixEscape(String src) {
-		if (src == null) return null;
-		return src.replaceAll("%","%25");
+		if (src == null)
+			return null;
+		return src.replaceAll("%", "%25");
 	}
-	
+
 	public XMPPID(Namespace namespace, String unamehost) throws URISyntaxException {
 		super(namespace);
 		unamehost = fixEscape(unamehost);
-		if (unamehost == null) throw new URISyntaxException(unamehost,"username/host string cannot be null");
+		if (unamehost == null)
+			throw new URISyntaxException(unamehost, "username/host string cannot be null");
 		// Handle parsing of user@host/resource string
 		int atIndex = unamehost.lastIndexOf(USER_HOST_DELIMITER);
-		if (atIndex == -1) throw new URISyntaxException(unamehost,"username/host string not valid.  Must be of form <username>@<hostname>[:port]");
-		username = unamehost.substring(0,atIndex);
-		hostname = unamehost.substring(atIndex+1);
+		if (atIndex == -1)
+			throw new URISyntaxException(unamehost, "username/host string not valid.  Must be of form <username>@<hostname>[:port]");
+		username = unamehost.substring(0, atIndex);
+		final String remainder = unamehost.substring(atIndex + 1);
 		// Handle parsing of host:port
-		atIndex = hostname.lastIndexOf(PORT_DELIMITER);
+		atIndex = remainder.lastIndexOf(PORT_DELIMITER);
 		if (atIndex != -1) {
 			try {
-				port = Integer.parseInt(hostname.substring(atIndex+1));
-			} catch (NumberFormatException e) {
-				throw new URISyntaxException(unamehost,"invalid port value");
-			}			
-			hostname = hostname.substring(0,atIndex);
+				final int slashLoc = remainder.indexOf(PATH_DELIMITER);
+				if (slashLoc != -1)
+					port = Integer.parseInt(remainder.substring(atIndex + 1, slashLoc));
+				else
+					port = Integer.parseInt(remainder.substring(atIndex + 1));
+			} catch (final NumberFormatException e) {
+				throw new URISyntaxException(unamehost, "invalid port value");
+			}
+			hostname = remainder.substring(0, atIndex);
 		}
-		atIndex = hostname.lastIndexOf(PATH_DELIMITER);
+		atIndex = remainder.indexOf(PATH_DELIMITER);
 		if (atIndex != -1) {
-			resourcename = PATH_DELIMITER+hostname.substring(atIndex+1);
-			hostname = hostname.substring(0,atIndex);
+			if (hostname == null)
+				hostname = remainder.substring(0, atIndex);
+			resourcename = PATH_DELIMITER + remainder.substring(atIndex + 1);
 		} else {
 			resourcename = PATH_DELIMITER + ECFConnection.CLIENT_TYPE;
 		}
-		uri = new URI(namespace.getScheme(),username,hostname,port,resourcename,null,null);
+		uri = new URI(namespace.getScheme(), username, hostname, port, resourcename, null, null);
 	}
-	
+
 	protected int namespaceCompareTo(BaseID o) {
-        return getName().compareTo(o.getName());
+		return getName().compareTo(o.getName());
 	}
 
 	protected boolean namespaceEquals(BaseID o) {
 		if (!(o instanceof XMPPID)) {
 			return false;
 		}
-		XMPPID other = (XMPPID) o;
+		final XMPPID other = (XMPPID) o;
 		return getUsernameAtHost().equals(other.getUsernameAtHost());
 	}
 
@@ -88,35 +96,37 @@ public class XMPPID extends BaseID implements IChatID {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public String getHostname() {
 		return hostname;
 	}
-	
+
 	public String getResourceName() {
 		return resourcename;
 	}
-	
+
 	public int getPort() {
 		return port;
 	}
-	
+
 	public String getUsernameAtHost() {
-		return getUsername()+"@"+getHostname();
+		return getUsername() + "@" + getHostname();
 	}
-	
+
 	public String getFQName() {
-		return getUsernameAtHost()+getResourceName();
+		return getUsernameAtHost() + getResourceName();
 	}
-	
+
 	public String toString() {
-		StringBuffer sb = new StringBuffer("XMPPID[");
+		final StringBuffer sb = new StringBuffer("XMPPID[");
 		sb.append(uri.toString()).append("]");
 		return sb.toString();
 	}
+
 	public Object getAdapter(Class clazz) {
-	    if (clazz.isInstance(this)) {
-	    	return this;
-	    } else return super.getAdapter(clazz);
+		if (clazz.isInstance(this)) {
+			return this;
+		} else
+			return super.getAdapter(clazz);
 	}
 }
