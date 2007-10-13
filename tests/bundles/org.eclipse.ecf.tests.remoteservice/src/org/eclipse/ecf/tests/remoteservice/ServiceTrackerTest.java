4ee330040142529b88d19c3a10b091b25646706d
slewis <slewis>
2007-10-13 23:26:05 +0000

    Added ServiceTrackerTest

---
+++ tests/bundles/org.eclipse.ecf.tests.remoteservice/src/org/eclipse/ecf/tests/remoteservice/ServiceTrackerTest.java
@@ -0,0 +1,125 @@
+/****************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.ecf.tests.remoteservice;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import org.eclipse.ecf.remoteservice.Constants;
+import org.eclipse.ecf.remoteservice.IRemoteCall;
+import org.eclipse.ecf.remoteservice.IRemoteCallListener;
+import org.eclipse.ecf.remoteservice.IRemoteService;
+import org.eclipse.ecf.remoteservice.IRemoteServiceContainerAdapter;
+import org.eclipse.ecf.remoteservice.events.IRemoteCallEvent;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+public class ServiceTrackerTest extends AbstractRemoteServiceTestCase {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		setClientCount(2);
+		createServerAndClients();
+		connectClients();
+		setupRemoteServiceAdapters();
+		addRemoteServiceListeners();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		cleanUpServerAndClients();
+		super.tearDown();
+	}
+
+	protected Object createService() {
+		return new IConcatService() {
+			public String concat(String string1, String string2) {
+				final String result = string1.concat(string2);
+				System.out.println("SERVICE.concat(" + string1 + "," + string2 + ") returning " + result);
+				return string1.concat(string2);
+			}
+		};
+	}
+
+	public void testServiceTracker() throws Exception {
+		final IRemoteServiceContainerAdapter[] adapters = getRemoteServiceAdapters();
+		// client [0]/adapter[0] is the service 'server'
+		// client [1]/adapter[1] is the service target (client)
+		final Dictionary props = new Hashtable();
+		props.put(Constants.SERVICE_REGISTRATION_TARGETS, getClients()[1].getConnectedID());
+		props.put(Constants.LOCAL_SERVICE_REGISTRATION, "true");
+		// Register
+		adapters[0].registerRemoteService(new String[] {IConcatService.class.getName()}, createService(), props);
+		// Give some time for propagation
+		sleep(3000);
+
+		final ServiceTracker st = new ServiceTracker(Activator.getDefault().getContext(), IConcatService.class.getName(), null);
+		assertNotNull(st);
+		st.open();
+		final IConcatService concatService = (IConcatService) st.getService();
+		assertNotNull(concatService);
+		System.out.println("proxy call start");
+		final String result = concatService.concat("OSGi ", "is cool");
+		System.out.println("proxy call end. result=" + result);
+		sleep(3000);
+		st.close();
+		sleep(3000);
+	}
+
+	protected IRemoteCall createRemoteConcat(String first, String second) {
+		return createRemoteCall("concat", new Object[] {first, second});
+	}
+
+	protected IRemoteCallListener createRemoteCallListener() {
+		return new IRemoteCallListener() {
+			public void handleEvent(IRemoteCallEvent event) {
+				System.out.println("CLIENT.handleEvent(" + event + ")");
+			}
+		};
+	}
+
+	public void testRemoteServiceWithServiceTracker() throws Exception {
+		final IRemoteServiceContainerAdapter[] adapters = getRemoteServiceAdapters();
+		// client [0]/adapter[0] is the service 'server'
+		// client [1]/adapter[1] is the service target (client)
+		final Dictionary props = new Hashtable();
+		props.put(Constants.SERVICE_REGISTRATION_TARGETS, getClients()[1].getConnectedID());
+		props.put(Constants.LOCAL_SERVICE_REGISTRATION, "true");
+		// Register
+		adapters[0].registerRemoteService(new String[] {IConcatService.class.getName()}, createService(), props);
+		// Give some time for propagation
+		sleep(3000);
+
+		final ServiceTracker st = new ServiceTracker(Activator.getDefault().getContext(), IConcatService.class.getName(), null);
+		assertNotNull(st);
+		st.open();
+		final ServiceReference ref = st.getServiceReference();
+		assertNotNull(ref);
+		final IRemoteService remoteService = (IRemoteService) ref.getProperty(Constants.REMOTE_SERVICE);
+		assertNotNull(remoteService);
+		// Call it asynch with listener
+		remoteService.callAsynch(createRemoteConcat("OSGi ", "Sucks (sic)"), createRemoteCallListener());
+
+		sleep(3000);
+		st.close();
+	}
+
+}
