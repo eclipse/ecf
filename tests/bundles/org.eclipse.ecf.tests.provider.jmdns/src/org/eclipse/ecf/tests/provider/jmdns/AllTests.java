24b64219281bf6ee48d75cce220af281ec1cb58d
mkuppe <mkuppe>
2007-11-30 13:01:02 +0000

    NEW - bug 211587: [Discovery] Move enhancements/refactorings to dev.eclipse.org https://bugs.eclipse.org/bugs/show_bug.cgi?id=211587

---
+++ tests/bundles/org.eclipse.ecf.tests.provider.jmdns/src/org/eclipse/ecf/tests/provider/jmdns/AllTests.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2007 Versant Corp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Markus Kuppe (mkuppe <at> versant <dot> com) - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.tests.provider.jmdns;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.eclipse.ecf.tests.provider.jmdns.identity.JMDNSServiceIDTest;
+
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.eclipse.ecf.tests.discovery.jmdns");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(JMDNSServiceIDTest.class);
+		suite.addTestSuite(JMDNSDiscoveryServiceTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
