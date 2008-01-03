defe87bec8303d61c2ab3da8c51f898717fea056
slewis <slewis>
2008-01-03 07:03:42 +0000

    Created screen capture capability in org.eclipse.ecf.presence.collab.ui.  Created screencapture support classes and interfaces in org.eclipse.ecf.ui.  Moved code out of org.eclipse.ecf.examples.collab to org.eclipse.ecf.ui.screencapture.

--- framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/datashare/AbstractShare.java
+++ framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/datashare/AbstractShare.java
@@ -93,7 +93,7 @@ public abstract class AbstractShare {
 	 */
 	protected abstract void handleMessage(ID fromContainerID, byte[] data);
 
-	protected synchronized void sendMessage(ID toID, byte[] data) throws ECFException {
+	public synchronized void sendMessage(ID toID, byte[] data) throws ECFException {
 		if (channel != null)
 			channel.sendMessage(toID, data);
 	}
