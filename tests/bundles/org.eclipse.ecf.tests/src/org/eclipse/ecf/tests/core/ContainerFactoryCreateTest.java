5743e824ecce9d2dae0387e8ec72ef0b5f802ad2
slewis <slewis>
2008-01-13 22:46:02 +0000

    String externalization in BaseContainer.  Added to functionality of BaseContainerInstantiator and added test code.

--- tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/core/ContainerFactoryCreateTest.java
+++ tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/core/ContainerFactoryCreateTest.java
@@ -18,6 +18,7 @@ import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.ContainerTypeDescription;
 import org.eclipse.ecf.core.IContainer;
 import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDFactory;
 import org.eclipse.ecf.core.identity.Namespace;
 import org.eclipse.ecf.core.provider.IContainerInstantiator;
 import org.eclipse.ecf.core.security.IConnectContext;
@@ -25,7 +26,9 @@ import org.eclipse.ecf.core.security.IConnectContext;
 public class ContainerFactoryCreateTest extends ContainerFactoryAbstractTestCase {
 
 	protected static final String CONTAINER_TYPE_NAME = ContainerFactoryCreateTest.class.getName();
-	
+
+	protected static final String BASE_CONTAINER_TYPE_NAME = "ecf.root";
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.ecf.tests.core.ContainerFactoryAbstractTestCase#setUp()
 	 */
@@ -33,7 +36,7 @@ public class ContainerFactoryCreateTest extends ContainerFactoryAbstractTestCase
 		super.setUp();
 		getFixture().addDescription(createContainerTypeDescription());
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.ecf.tests.core.ContainerFactoryAbstractTestCase#tearDown()
 	 */
@@ -41,69 +44,61 @@ public class ContainerFactoryCreateTest extends ContainerFactoryAbstractTestCase
 		getFixture().removeDescription(createContainerTypeDescription());
 		super.tearDown();
 	}
-	
+
 	protected ContainerTypeDescription createContainerTypeDescription() {
-		return new ContainerTypeDescription(CONTAINER_TYPE_NAME,
-				new IContainerInstantiator() {
-					public IContainer createInstance(
-							ContainerTypeDescription description,
-							Object[] parameters)
-							throws ContainerCreateException {
-						return new AbstractContainer() {
-							public void connect(ID targetID,
-									IConnectContext connectContext)
-									throws ContainerConnectException {
-							}
-
-							public void disconnect() {
-							}
-
-							public Namespace getConnectNamespace() {
-								return null;
-							}
-
-							public ID getConnectedID() {
-								return null;
-							}
-
-							public ID getID() {
-								return null;
-							}
-
-						};
+		return new ContainerTypeDescription(CONTAINER_TYPE_NAME, new IContainerInstantiator() {
+			public IContainer createInstance(ContainerTypeDescription description, Object[] parameters) throws ContainerCreateException {
+				return new AbstractContainer() {
+					public void connect(ID targetID, IConnectContext connectContext) throws ContainerConnectException {
 					}
 
-					public String[] getSupportedAdapterTypes(
-							ContainerTypeDescription description) {
-						return new String[] { "one" };
+					public void disconnect() {
 					}
 
-					public Class[][] getSupportedParameterTypes(
-							ContainerTypeDescription description) {
-						return new Class[][] { { String.class , Class.class }};
+					public Namespace getConnectNamespace() {
+						return null;
 					}
-				}, DESCRIPTION);
+
+					public ID getConnectedID() {
+						return null;
+					}
+
+					public ID getID() {
+						return null;
+					}
+
+				};
+			}
+
+			public String[] getSupportedAdapterTypes(ContainerTypeDescription description) {
+				return new String[] {"one"};
+			}
+
+			public Class[][] getSupportedParameterTypes(ContainerTypeDescription description) {
+				return new Class[][] {{String.class, Class.class}};
+			}
+		}, DESCRIPTION);
 	}
-	
+
 	public void testCreateContainer0() throws Exception {
-		IContainer container = ContainerFactory.getDefault().createContainer();
+		final IContainer container = ContainerFactory.getDefault().createContainer();
 		assertNotNull(container);
 	}
-	
+
 	public void testCreateContainer1() throws Exception {
-		IContainer container = ContainerFactory.getDefault().createContainer(CONTAINER_TYPE_NAME);
+		final IContainer container = ContainerFactory.getDefault().createContainer(CONTAINER_TYPE_NAME);
 		assertNotNull(container);
 	}
-	
+
 	public void testCreateContainer2() throws Exception {
-		IContainer container = ContainerFactory.getDefault().createContainer(CONTAINER_TYPE_NAME, null);
+		final IContainer container = ContainerFactory.getDefault().createContainer(CONTAINER_TYPE_NAME, null);
 		assertNotNull(container);
 	}
-	
+
 	public void testCreateContainer3() throws Exception {
-		ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
 		assertNotNull(desc);
-		IContainer container = ContainerFactory.getDefault().createContainer(desc, null);
+		final IContainer container = ContainerFactory.getDefault().createContainer(desc, null);
 		assertNotNull(container);
 	}
 
@@ -111,7 +106,7 @@ public class ContainerFactoryCreateTest extends ContainerFactoryAbstractTestCase
 		try {
 			ContainerFactory.getDefault().createContainer((String) null, null);
 			fail();
-		} catch (ContainerCreateException e) {
+		} catch (final ContainerCreateException e) {
 		}
 	}
 
@@ -119,30 +114,49 @@ public class ContainerFactoryCreateTest extends ContainerFactoryAbstractTestCase
 		try {
 			ContainerFactory.getDefault().createContainer((ContainerTypeDescription) null, null);
 			fail();
-		} catch (ContainerCreateException e) {
+		} catch (final ContainerCreateException e) {
 		}
 	}
 
+	public void testCreateBaseContainer() throws Exception {
+		final IContainer base = ContainerFactory.getDefault().createContainer();
+		assertNotNull(base);
+	}
+
+	public void testCreateBaseContainer0() throws Exception {
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(BASE_CONTAINER_TYPE_NAME);
+		assertNotNull(desc);
+		final IContainer base = ContainerFactory.getDefault().createContainer(desc, new Object[] {IDFactory.getDefault().createGUID()});
+		assertNotNull(base);
+	}
+
+	public void testCreateBaseContainer1() throws Exception {
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(BASE_CONTAINER_TYPE_NAME);
+		assertNotNull(desc);
+		final IContainer base = ContainerFactory.getDefault().createContainer(desc, new Object[] {IDFactory.getDefault().createGUID().getName()});
+		assertNotNull(base);
+	}
+
 	public void testContainerTypeDescriptionGetName() {
-		ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
 		assertTrue(desc.getName().equals(CONTAINER_TYPE_NAME));
 	}
 
 	public void testContainerTypeDescriptionGetDescription() {
-		ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
 		assertTrue(desc.getDescription().equals(DESCRIPTION));
 	}
 
 	public void testContainerTypeDescriptionGetSupportedAdapterTypes() {
-		ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
-		String [] adapterTypes = desc.getSupportedAdapterTypes();
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
+		final String[] adapterTypes = desc.getSupportedAdapterTypes();
 		assertTrue(adapterTypes.length == 1);
 		assertTrue(adapterTypes[0] == "one");
 	}
-	
+
 	public void testContainerTypeDescriptionGetSupportedParemeterTypes() {
-		ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
-		Class [] [] parameterTypes = desc.getSupportedParameterTypes();
+		final ContainerTypeDescription desc = ContainerFactory.getDefault().getDescriptionByName(CONTAINER_TYPE_NAME);
+		final Class[][] parameterTypes = desc.getSupportedParameterTypes();
 		assertTrue(parameterTypes.length == 1);
 		assertTrue(parameterTypes[0].length == 2);
 		assertTrue(parameterTypes[0][0].equals(String.class));
