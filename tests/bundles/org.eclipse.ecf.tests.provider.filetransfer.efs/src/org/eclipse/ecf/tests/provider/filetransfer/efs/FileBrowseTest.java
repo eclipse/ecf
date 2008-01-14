99b67a5ca71b124bde8bb7c3a50bd254a85d4144
slewis <slewis>
2008-01-14 23:56:20 +0000

    API change for IRemoteFileSystemBrowser to support both requests for files and directories.  This to addtress bug:  https://bugs.eclipse.org/bugs/show_bug.cgi?id=215250

--- tests/bundles/org.eclipse.ecf.tests.provider.filetransfer.efs/src/org/eclipse/ecf/tests/provider/filetransfer/efs/FileBrowseTest.java
+++ tests/bundles/org.eclipse.ecf.tests.provider.filetransfer.efs/src/org/eclipse/ecf/tests/provider/filetransfer/efs/FileBrowseTest.java
@@ -11,7 +11,10 @@
 
 package org.eclipse.ecf.tests.provider.filetransfer.efs;
 
+import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.ecf.filetransfer.IRemoteFile;
 import org.eclipse.ecf.filetransfer.events.IRemoteFileSystemDirectoryEvent;
@@ -20,7 +23,7 @@ import org.eclipse.ecf.tests.filetransfer.AbstractBrowseTestCase;
 /**
  *
  */
-public class BrowseTest extends AbstractBrowseTestCase {
+public class FileBrowseTest extends AbstractBrowseTestCase {
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.ecf.tests.filetransfer.AbstractBrowseTestCase#handleDirectoryEvent(org.eclipse.ecf.filetransfer.events.IRemoteFileSystemDirectoryEvent)
@@ -35,9 +38,50 @@ public class BrowseTest extends AbstractBrowseTestCase {
 		done = true;
 	}
 
-	public void testBrowseRoot() throws Exception {
-		super.testListDirectory(new URL("efs:file:///c:/"));
-		waitForDone(5000);
+	protected File[] roots;
+
+	protected File[] files;
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.tests.filetransfer.AbstractBrowseTestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		roots = File.listRoots();
+		final List files = new ArrayList();
+		for (int i = 0; i < roots.length; i++) {
+			final File[] fs = roots[i].listFiles();
+			if (fs != null)
+				for (int j = 0; j < fs.length; j++) {
+					if (fs[j].exists())
+						files.add(fs[j]);
+				}
+		}
+		this.files = (File[]) files.toArray(new File[] {});
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.tests.filetransfer.AbstractBrowseTestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		this.roots = null;
+		this.files = null;
+	}
+
+	public void testBrowseRoots() throws Exception {
+		for (int i = 0; i < roots.length; i++) {
+			if (roots[i].exists())
+				testBrowse(new URL("efs:" + roots[i].toURL().toString()));
+			Thread.sleep(100);
+		}
+	}
+
+	public void testFileBrowse() throws Exception {
+		for (int i = 0; i < files.length; i++) {
+			testBrowse(new URL("efs:" + files[i].toURL().toString()));
+			Thread.sleep(100);
+		}
 	}
 
 }
