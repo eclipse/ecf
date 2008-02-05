93b61d0c06e4b6b67a1fc0d2feec3b8c9bbc4a82
slewis <slewis>
2008-02-05 16:54:02 +0000

    additions to abstract remote service access handler

--- framework/bundles/org.eclipse.ecf.discovery.ui/src/org/eclipse/ecf/discovery/ui/handlers/AbstractRemoteServiceAccessHandler.java
+++ framework/bundles/org.eclipse.ecf.discovery.ui/src/org/eclipse/ecf/discovery/ui/handlers/AbstractRemoteServiceAccessHandler.java
@@ -1,18 +1,17 @@
 package org.eclipse.ecf.discovery.ui.handlers;
 
 import java.util.*;
-import org.eclipse.ecf.core.IContainer;
-import org.eclipse.ecf.core.IContainerManager;
-import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.core.*;
+import org.eclipse.ecf.core.identity.*;
+import org.eclipse.ecf.core.security.ConnectContextFactory;
 import org.eclipse.ecf.core.util.AdapterContainerFilter;
 import org.eclipse.ecf.discovery.IServiceInfo;
-import org.eclipse.ecf.discovery.IServiceProperties;
-import org.eclipse.ecf.discovery.identity.IServiceTypeID;
 import org.eclipse.ecf.discovery.ui.views.IServiceAccessHandler;
 import org.eclipse.ecf.internal.discovery.ui.Activator;
 import org.eclipse.ecf.internal.discovery.ui.Messages;
 import org.eclipse.ecf.remoteservice.*;
 import org.eclipse.jface.action.*;
+import org.osgi.framework.InvalidSyntaxException;
 
 public abstract class AbstractRemoteServiceAccessHandler implements IServiceAccessHandler {
 
@@ -21,6 +20,8 @@ public abstract class AbstractRemoteServiceAccessHandler implements IServiceAcce
 	protected static IContributionItem[] NOT_AVAILABLE_CONTRIBUTION;
 
 	protected final AdapterContainerFilter containerFilter;
+
+	protected IServiceInfo serviceInfo;
 
 	public AbstractRemoteServiceAccessHandler() {
 		containerFilter = new AdapterContainerFilter(IRemoteServiceContainerAdapter.class);
@@ -51,57 +52,104 @@ public abstract class AbstractRemoteServiceAccessHandler implements IServiceAcce
 		return (containerNamespace.getName().equals(targetNamespace));
 	}
 
-	protected boolean matchServiceType(IServiceTypeID serviceTypeID, String service) {
-		return Arrays.asList(serviceTypeID.getServices()).contains(service);
+	protected boolean matchServiceType(String service) {
+		return Arrays.asList(this.serviceInfo.getServiceID().getServiceTypeID().getServices()).contains(service);
 	}
 
-	protected List getRemoteServiceContainerAdapters(IServiceInfo serviceInfo, String targetNamespace) {
+	protected List getRemoteServiceContainerAdapters() {
 		final List remoteServicesContainerAdapters = new ArrayList();
 		final IContainerManager containerManager = getContainerManager();
 		if (containerManager == null)
 			return remoteServicesContainerAdapters;
 		final IContainer[] containers = containerManager.getAllContainers();
 		for (int i = 0; i < containers.length; i++) {
-			if (containerFilter.match(containers[i]) && isConnected(containers[i]) && matchTargetNamespace(containers[i], targetNamespace)) {
+			if (containerFilter.match(containers[i]) && matchTargetNamespace(containers[i], getConnectNamespace())) {
 				remoteServicesContainerAdapters.add(containerFilter.getMatchResult());
 			}
 		}
 		return remoteServicesContainerAdapters;
 	}
 
-	public IContributionItem[] getContributionsForService(IServiceInfo serviceInfo) {
-		if (matchServiceType(serviceInfo.getServiceID().getServiceTypeID(), Constants.DISCOVERY_SERVICE_TYPE)) {
+	protected String getContainerFactory() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_CONTAINER_FACTORY_PROPERTY);
+	}
+
+	protected String getRemoteServiceClass() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_OBJECTCLASS_PROPERTY);
+	}
+
+	protected String getConnectNamespace() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_CONNECT_ID_NAMESPACE_PROPERTY);
+	}
+
+	protected String getConnectID() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_CONNECT_ID_PROPERTY);
+	}
+
+	protected String getServiceNamespace() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_SERVICE_ID_NAMESPACE_PROPERTY);
+	}
+
+	protected String getServiceID() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_SERVICE_ID_PROPERTY);
+	}
+
+	protected String getFilter() {
+		return this.serviceInfo.getServiceProperties().getPropertyString(Constants.DISCOVERY_FILTER_PROPERTY);
+	}
+
+	protected ID createID(String namespace, String value) throws IDCreateException {
+		return IDFactory.getDefault().createID(namespace, value);
+	}
+
+	public IContributionItem[] getContributionsForService(IServiceInfo svcInfo) {
+		if (svcInfo == null)
+			return EMPTY_CONTRIBUTION;
+		this.serviceInfo = svcInfo;
+		if (matchServiceType(Constants.DISCOVERY_SERVICE_TYPE)) {
 			// First get container manager...if we don't have one, then we're outta here
-			final IServiceProperties serviceProperties = serviceInfo.getServiceProperties();
-			final String targetNamespace = serviceProperties.getPropertyString(Constants.DISCOVERY_TARGET_ID_NAMESPACE_PROPERTY);
-			final String clazz = serviceProperties.getPropertyString(Constants.DISCOVERY_OBJECTCLASS_PROPERTY);
-			if (targetNamespace == null || clazz == null)
-				return EMPTY_CONTRIBUTION;
-			final List remoteServicesContainerAdapters = getRemoteServiceContainerAdapters(serviceInfo, targetNamespace);
+			final List remoteServicesContainerAdapters = getRemoteServiceContainerAdapters();
 			// If we've got none, then we return 
 			if (remoteServicesContainerAdapters.size() == 0)
 				return NOT_AVAILABLE_CONTRIBUTION;
-			final String filter = serviceProperties.getPropertyString(Constants.DISCOVERY_FILTER_PROPERTY);
-			final String target = serviceProperties.getPropertyString(Constants.DISCOVERY_TARGET_ID_PROPERTY);
 			// If we've got one, then we do our thing
 			final List contributions = new ArrayList();
 			for (final Iterator i = remoteServicesContainerAdapters.iterator(); i.hasNext();) {
-				contributions.add(getContributionsForService((IRemoteServiceContainerAdapter) i.next(), targetNamespace, target, clazz, filter));
+				IRemoteServiceContainerAdapter adapter = (IRemoteServiceContainerAdapter) i.next();
+				IContributionItem[] menuContributions = getContributionItemsForService(adapter);
+				if (menuContributions == null)
+					continue;
+				for (int j = 0; j < menuContributions.length; j++)
+					contributions.add(menuContributions[j]);
 			}
 			return (IContributionItem[]) contributions.toArray(new IContributionItem[] {});
 		}
 		return EMPTY_CONTRIBUTION;
 	}
 
+	protected IRemoteServiceReference[] getRemoteServiceReferences(IRemoteServiceContainerAdapter adapter) throws InvalidSyntaxException, IDCreateException {
+		ID serviceID = null;
+		String serviceNamespace = getServiceNamespace();
+		String serviceid = getServiceID();
+		if (serviceNamespace != null && serviceid != null) {
+			serviceID = createID(serviceNamespace, serviceid);
+		}
+		return adapter.getRemoteServiceReferences(new ID[] {serviceID}, getRemoteServiceClass(), getFilter());
+	}
+
+	protected IContainer createContainer() throws ContainerCreateException {
+		return ContainerFactory.getDefault().createContainer(getContainerFactory());
+	}
+
+	protected void connectContainer(IContainer container, String password) throws IDCreateException, ContainerConnectException {
+		container.connect(createID(getConnectNamespace(), getConnectID()), (password == null) ? null : ConnectContextFactory.createPasswordConnectContext(password));
+	}
+
 	/**
 	 * @param adapter the IRemoteServiceContainerAdapter to use to lookup the {@link IRemoteServiceReference}.  Will not be <code>null</code>.
-	 * @param targetNamespace the target namespace for the idFilter parameter for the call to {@link IRemoteServiceContainerAdapter#getRemoteServiceReferences(org.eclipse.ecf.core.identity.ID[], String, String)}.  Will not be <code>null</code>.
-	 * @param target the target id for the idFilter parameter for the call to {@link IRemoteServiceContainerAdapter#getRemoteServiceReferences(org.eclipse.ecf.core.identity.ID[], String, String)}.  May be <code>null</code>.
-	 * @param clazz the clazz parameter for the call to {@link IRemoteServiceContainerAdapter#getRemoteServiceReferences(org.eclipse.ecf.core.identity.ID[], String, String)}.  Will not be <code>null</code>.
-	 * @param filter the filter parameter for the call to {@link IRemoteServiceContainerAdapter#getRemoteServiceReferences(org.eclipse.ecf.core.identity.ID[], String, String)}.  May be <code>null</code>.
-	 * @return IContributionItem the menu contribution item to be added to the menu.  May be <code>null</code>.  If <code>null</code> then no item is added to the
+	 * @return IContributionItem the menu contribution items to be added to the menu.  May be <code>null</code>.  If <code>null</code> then no item is added to the
 	 * menu.
 	 */
-	protected abstract IContributionItem getContributionsForService(final IRemoteServiceContainerAdapter adapter, final String targetNamespace, final String target, final String clazz, final String filter);
+	protected abstract IContributionItem[] getContributionItemsForService(final IRemoteServiceContainerAdapter adapter);
 
 }
