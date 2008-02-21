0d4f0aa392851e9ee9935cd5802633a79d6014cb
slewis <slewis>
2008-02-21 22:36:14 +0000

    Fixes for Bundle-ActivationPolicy header.  bug 219878

--- protocols/bundles/org.jivesoftware.smack/src/org/jivesoftware/smackx/provider/DiscoverInfoProvider.java
+++ protocols/bundles/org.jivesoftware.smack/src/org/jivesoftware/smackx/provider/DiscoverInfoProvider.java
@@ -33,51 +33,48 @@ import org.xmlpull.v1.XmlPullParser;
 */
 public class DiscoverInfoProvider implements IQProvider {
 
-    public IQ parseIQ(XmlPullParser parser) throws Exception {
-        DiscoverInfo discoverInfo = new DiscoverInfo();
-        boolean done = false;
-        DiscoverInfo.Feature feature = null;
-        DiscoverInfo.Identity identity = null;
-        String category = "";
-        String name = "";
-        String type = "";
-        String variable = "";
-        discoverInfo.setNode(parser.getAttributeValue("", "node"));
-        while (!done) {
-            int eventType = parser.next();
-            if (eventType == XmlPullParser.START_TAG) {
-                if (parser.getName().equals("identity")) {
-                    // Initialize the variables from the parsed XML
-                    category = parser.getAttributeValue("", "category");
-                    name = parser.getAttributeValue("", "name");
-                    type = parser.getAttributeValue("", "type");
-                }
-                else if (parser.getName().equals("feature")) {
-                    // Initialize the variables from the parsed XML
-                    variable = parser.getAttributeValue("", "var");
-                }
-                // Otherwise, it must be a packet extension.
-                else {
-                    discoverInfo.addExtension(PacketParserUtils.parsePacketExtension(parser
-                            .getName(), parser.getNamespace(), parser));
-                }
-            } else if (eventType == XmlPullParser.END_TAG) {
-                if (parser.getName().equals("identity")) {
-                    // Create a new identity and add it to the discovered info.
-                    identity = new DiscoverInfo.Identity(category, name);
-                    identity.setType(type);
-                    discoverInfo.addIdentity(identity);
-                }
-                if (parser.getName().equals("feature")) {
-                    // Create a new feature and add it to the discovered info.
-                    discoverInfo.addFeature(variable);
-                }
-                if (parser.getName().equals("query")) {
-                    done = true;
-                }
-            }
-        }
+	public IQ parseIQ(XmlPullParser parser) throws Exception {
+		final DiscoverInfo discoverInfo = new DiscoverInfo();
+		boolean done = false;
+		DiscoverInfo.Identity identity = null;
+		String category = "";
+		String name = "";
+		String type = "";
+		String variable = "";
+		discoverInfo.setNode(parser.getAttributeValue("", "node"));
+		while (!done) {
+			final int eventType = parser.next();
+			if (eventType == XmlPullParser.START_TAG) {
+				if (parser.getName().equals("identity")) {
+					// Initialize the variables from the parsed XML
+					category = parser.getAttributeValue("", "category");
+					name = parser.getAttributeValue("", "name");
+					type = parser.getAttributeValue("", "type");
+				} else if (parser.getName().equals("feature")) {
+					// Initialize the variables from the parsed XML
+					variable = parser.getAttributeValue("", "var");
+				}
+				// Otherwise, it must be a packet extension.
+				else {
+					discoverInfo.addExtension(PacketParserUtils.parsePacketExtension(parser.getName(), parser.getNamespace(), parser));
+				}
+			} else if (eventType == XmlPullParser.END_TAG) {
+				if (parser.getName().equals("identity")) {
+					// Create a new identity and add it to the discovered info.
+					identity = new DiscoverInfo.Identity(category, name);
+					identity.setType(type);
+					discoverInfo.addIdentity(identity);
+				}
+				if (parser.getName().equals("feature")) {
+					// Create a new feature and add it to the discovered info.
+					discoverInfo.addFeature(variable);
+				}
+				if (parser.getName().equals("query")) {
+					done = true;
+				}
+			}
+		}
 
-        return discoverInfo;
-    }
+		return discoverInfo;
+	}
 }
