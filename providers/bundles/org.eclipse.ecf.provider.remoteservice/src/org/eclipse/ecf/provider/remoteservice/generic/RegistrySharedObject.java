eb2f5d340ce34dae50783fcb1584639b9f146965
slewis <slewis>
2008-04-28 17:12:55 +0000

    Removed use of bogus property that allowed access to IRemoteService via the service proxy.

--- providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RegistrySharedObject.java
+++ providers/bundles/org.eclipse.ecf.provider.remoteservice/src/org/eclipse/ecf/provider/remoteservice/generic/RegistrySharedObject.java
@@ -558,7 +558,6 @@ public class RegistrySharedObject extends BaseSharedObject implements IRemoteSer
 			}
 			final ID remoteContainerID = registration.getContainerID();
 			properties.put(org.eclipse.ecf.remoteservice.Constants.REMOTE_SERVICE_CONTAINER_ID, remoteContainerID.getName());
-			properties.put(org.eclipse.ecf.remoteservice.Constants.REMOTE_SERVICE, remoteServiceImpl);
 			final ServiceRegistration serviceRegistration = context.registerService(registration.getClasses(), service, properties);
 			addLocalServiceRegistration(remoteContainerID, serviceRegistration);
 		}
