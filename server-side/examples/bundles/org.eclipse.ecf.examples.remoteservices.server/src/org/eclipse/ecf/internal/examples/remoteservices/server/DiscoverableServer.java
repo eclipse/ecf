86a5ea5e39a17909387bf0e8a03157dea142a746
slewis <slewis>
2008-05-29 03:40:23 +0000

    Added  launch configuration for r-OSGi Remote EnvironmentInfo Server

--- server-side/examples/bundles/org.eclipse.ecf.examples.remoteservices.server/src/org/eclipse/ecf/internal/examples/remoteservices/server/DiscoverableServer.java
+++ server-side/examples/bundles/org.eclipse.ecf.examples.remoteservices.server/src/org/eclipse/ecf/internal/examples/remoteservices/server/DiscoverableServer.java
@@ -11,19 +11,16 @@
 
 package org.eclipse.ecf.internal.examples.remoteservices.server;
 
-import java.security.InvalidParameterException;
 import java.util.Map;
 import java.util.Properties;
 import org.eclipse.ecf.core.ContainerFactory;
 import org.eclipse.ecf.core.IContainer;
-import org.eclipse.ecf.core.identity.ID;
-import org.eclipse.ecf.core.identity.IDFactory;
-import org.eclipse.ecf.core.security.ConnectContextFactory;
-import org.eclipse.ecf.core.security.IConnectContext;
+import org.eclipse.ecf.core.identity.*;
 import org.eclipse.ecf.core.util.ECFException;
 import org.eclipse.ecf.discovery.*;
 import org.eclipse.ecf.discovery.identity.IServiceID;
 import org.eclipse.ecf.discovery.identity.ServiceIDFactory;
+import org.eclipse.ecf.discovery.service.IDiscoveryService;
 import org.eclipse.ecf.examples.remoteservices.common.IRemoteEnvironmentInfo;
 import org.eclipse.ecf.remoteservice.Constants;
 import org.eclipse.ecf.remoteservice.IRemoteServiceContainerAdapter;
@@ -38,17 +35,23 @@ import org.eclipse.equinox.app.IApplicationContext;
 public class DiscoverableServer implements IApplication {
 
 	private static final String ECF_GENERIC_CLIENT = "ecf.generic.client"; //$NON-NLS-1$
+	private static final String ECF_GENERIC_SERVER = "ecf.generic.server"; //$NON-NLS-1$
+	private static final String ECF_GENERIC_SERVER_ID_NAMESPACE = StringID.class.getName();
+	private static final String ECF_GENERIC_SERVER_ID = "ecftcp://localhost:3285/server"; //$NON-NLS-1$
 
 	private IContainer serviceHostContainer;
 
 	private IServiceInfo serviceInfo;
 
-	private IDiscoveryContainerAdapter discovery;
+	private IDiscoveryService discoveryService;
 
-	private String containerType;
-	private String connectTarget;
-	private String connectPassword;
-	private String serviceType;
+	// Parameters
+	private String serviceHostContainerType = ECF_GENERIC_SERVER;
+	private String serviceHostNamespace = ECF_GENERIC_SERVER_ID_NAMESPACE;
+	private String serviceHostID = ECF_GENERIC_SERVER_ID;
+	private String clientContainerType = ECF_GENERIC_CLIENT;
+	private String clientConnectTarget = ECF_GENERIC_SERVER_ID;
+	private String serviceType = Constants.DISCOVERY_SERVICE_TYPE;
 
 	private boolean done = false;
 
@@ -63,17 +66,15 @@ public class DiscoverableServer implements IApplication {
 		final Map args = ctxt.getArguments();
 		initializeFromArguments((String[]) args.get("application.args")); //$NON-NLS-1$
 
-		serviceHostContainer = ContainerFactory.getDefault().createContainer(containerType);
-		final ID targetID = IDFactory.getDefault().createID(serviceHostContainer.getConnectNamespace(), connectTarget);
-		final IConnectContext connectContext = (connectPassword == null) ? null : ConnectContextFactory.createPasswordConnectContext(connectPassword);
-		serviceHostContainer.connect(targetID, connectContext);
-
-		discovery = Activator.getDefault().getDiscoveryService(5000);
-
+		ID hostID = IDFactory.getDefault().createID(serviceHostNamespace, serviceHostID);
+		serviceHostContainer = ContainerFactory.getDefault().createContainer(serviceHostContainerType, hostID);
 		final IRemoteServiceContainerAdapter containerAdapter = (IRemoteServiceContainerAdapter) serviceHostContainer.getAdapter(IRemoteServiceContainerAdapter.class);
+
+		discoveryService = Activator.getDefault().getDiscoveryService(5000);
+
 		// register remote service
 		final String className = IRemoteEnvironmentInfo.class.getName();
-		Properties props = new RemoteServiceProperties(containerType, serviceHostContainer);
+		Properties props = new RemoteServiceProperties(serviceHostContainerType, serviceHostContainer);
 		// Add auto registration of remote proxy
 		props.put(Constants.AUTOREGISTER_REMOTE_PROXY, "true"); //$NON-NLS-1$
 
@@ -82,10 +83,10 @@ public class DiscoverableServer implements IApplication {
 
 		// then register for discovery
 		final String serviceName = System.getProperty("user.name") + System.currentTimeMillis(); //$NON-NLS-1$
-		final IServiceID serviceID = ServiceIDFactory.getDefault().createServiceID(discovery.getServicesNamespace(), getCompleteServiceType(), serviceName);
-		serviceInfo = new ServiceInfo(serviceType, null, 80, serviceID, new ServiceProperties(new DiscoveryProperties(className, containerType, serviceHostContainer)));
+		final IServiceID serviceID = ServiceIDFactory.getDefault().createServiceID(discoveryService.getServicesNamespace(), getCompleteServiceType(), serviceName);
+		serviceInfo = new ServiceInfo(serviceType, null, 80, serviceID, createServiceProperties(className));
 		// register discovery here
-		discovery.registerService(serviceInfo);
+		discoveryService.registerService(serviceInfo);
 		System.out.println("discovery publish\n\tserviceName=" + serviceID.getServiceName() + "\n\tserviceTypeID=" + serviceID.getServiceTypeID()); //$NON-NLS-1$ //$NON-NLS-2$
 
 		// wait until done
@@ -97,24 +98,28 @@ public class DiscoverableServer implements IApplication {
 		return new Integer(0);
 	}
 
+	protected IServiceProperties createServiceProperties(String className) {
+		return new ServiceProperties(new DiscoveryProperties(className, clientContainerType, serviceHostNamespace, clientConnectTarget, null, null));
+	}
+
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#stop()
 	 */
 	public void stop() {
 		if (serviceInfo != null) {
-			if (discovery != null) {
+			if (discoveryService != null) {
 				try {
-					discovery.unregisterService(serviceInfo);
+					discoveryService.unregisterService(serviceInfo);
 				} catch (final ECFException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 				serviceInfo = null;
-				final IContainer container = (IContainer) discovery.getAdapter(IContainer.class);
+				final IContainer container = (IContainer) discoveryService.getAdapter(IContainer.class);
 				if (container != null) {
 					container.disconnect();
 				}
-				discovery = null;
+				discoveryService = null;
 			}
 		}
 		if (serviceHostContainer != null) {
@@ -132,34 +137,34 @@ public class DiscoverableServer implements IApplication {
 		if (args == null)
 			return;
 		for (int i = 0; i < args.length; i++) {
-			if (!args[i].startsWith("-")) { //$NON-NLS-1$
-				connectTarget = args[i++];
-			} else {
-				if (args[i - 1].equalsIgnoreCase("-containerType")) //$NON-NLS-1$
-					containerType = args[++i];
-				else if (args[i - 1].equalsIgnoreCase("-connectPassword")) //$NON-NLS-1$
-					connectPassword = args[++i];
-				else if (args[i - 1].equalsIgnoreCase("-serviceType")) //$NON-NLS-1$
-					serviceType = args[++i];
+			if (args[i].equalsIgnoreCase("-serviceHostContainerType")) //$NON-NLS-1$
+				serviceHostContainerType = args[++i];
+			else if (args[i].equalsIgnoreCase("-serviceHostNamespace")) //$NON-NLS-1$
+				serviceHostNamespace = args[++i];
+			else if (args[i].equalsIgnoreCase("-serviceHostID")) //$NON-NLS-1$
+				serviceHostID = args[++i];
+			else if (args[i].equalsIgnoreCase("-clientContainerType")) //$NON-NLS-1$
+				clientContainerType = args[++i];
+			else if (args[i].equalsIgnoreCase("-clientConnectTarget")) //$NON-NLS-1$
+				clientConnectTarget = args[++i];
+			else if (args[i].equalsIgnoreCase("-serviceType")) //$NON-NLS-1$
+				serviceType = args[++i];
+			else {
+				usage();
+				throw new IllegalArgumentException("Invalid argument"); //$NON-NLS-1$
 			}
 		}
-		if (connectTarget == null) {
-			usage();
-			throw new InvalidParameterException("connectTarget cannot be null"); //$NON-NLS-1$
-		}
-		if (containerType == null)
-			containerType = ECF_GENERIC_CLIENT;
-		if (serviceType == null)
-			serviceType = Constants.DISCOVERY_SERVICE_TYPE;
 	}
 
 	/**
 	 * 
 	 */
 	private void usage() {
-		System.out.println("usage: eclipse -console [options] -application org.eclipse.ecf.examples.remoteservices.server.remoteServicesServer <connectTarget>"); //$NON-NLS-1$
-		System.out.println("   options: [-containerType <typename>] default=ecf.generic.client"); //$NON-NLS-1$
-		System.out.println("            [-connectPassword <password>] default=none"); //$NON-NLS-1$
+		System.out.println("usage: eclipse -console [options] -application org.eclipse.ecf.examples.remoteservices.server.remoteServicesServer"); //$NON-NLS-1$
+		System.out.println("   options: [-serviceHostContainerType <typename>] default=ecf.generic.server"); //$NON-NLS-1$
+		System.out.println("            [-serviceHostID <hostID>] default=ecftcp://localhost:3285/server"); //$NON-NLS-1$
+		System.out.println("            [-clientContainerType <typename>] default=ecf.generic.client"); //$NON-NLS-1$
+		System.out.println("            [-clientConnectTarget <target>] default=<serviceHostID>"); //$NON-NLS-1$
 		System.out.println("            [-serviceType <serviceType>] default=remotesvcs"); //$NON-NLS-1$
 	}
 
