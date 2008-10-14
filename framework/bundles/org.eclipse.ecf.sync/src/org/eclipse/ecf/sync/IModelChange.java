0b43eaa4df0caff688734c2cd136485fa2206be7
slewis <slewis>
2008-10-14 03:15:11 +0000

    Refactoring of sync interfaces to create IModelSynchronizationStrategy.

--- framework/bundles/org.eclipse.ecf.sync/src/org/eclipse/ecf/sync/IModelChange.java
+++ framework/bundles/org.eclipse.ecf.sync/src/org/eclipse/ecf/sync/IModelChange.java
@@ -1,5 +1,11 @@
 package org.eclipse.ecf.sync;
 
+/**
+ * Model change interface.  This super interface is
+ * a 'tag' interface and does not define any methods.
+ * See sub-interfaces that extend this interface.
+ * 
+ */
 public interface IModelChange {
 
 }
