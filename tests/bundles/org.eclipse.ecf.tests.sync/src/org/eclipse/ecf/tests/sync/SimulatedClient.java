4ac73b3485c9d7c138a4884b8f1d57d00196a88d
mmayworm <mmayworm>
2008-10-15 23:18:16 +0000

    A test to simulated different clients using sync API.

--- tests/bundles/org.eclipse.ecf.tests.sync/src/org/eclipse/ecf/tests/sync/SimulatedClient.java
+++ tests/bundles/org.eclipse.ecf.tests.sync/src/org/eclipse/ecf/tests/sync/SimulatedClient.java
@@ -5,36 +5,64 @@ import junit.framework.TestCase;
 import org.eclipse.ecf.core.identity.ID;
 import org.eclipse.ecf.core.identity.IDFactory;
 import org.eclipse.ecf.internal.tests.sync.Activator;
-import org.eclipse.ecf.sync.doc.IDocumentChange;
-import org.eclipse.ecf.sync.doc.IDocumentChangeMessage;
-import org.eclipse.ecf.sync.doc.IDocumentSynchronizationStrategy;
 import org.eclipse.ecf.sync.doc.IDocumentSynchronizationStrategyFactory;
-import org.eclipse.ecf.sync.doc.messages.DocumentChangeMessage;
+import org.eclipse.jface.text.Document;
 
 public class SimulatedClient extends TestCase {
-	
+
 	private static final String COLA = "cola";
 
-	public void testSimulatedShare() throws Exception{
-		
-		IDocumentSynchronizationStrategyFactory  factory = Activator.getDefault().getColaSynchronizationStrategyFactory();
-		
+	public void testSimulatedShare() throws Exception {
+
+		IDocumentSynchronizationStrategyFactory factory = Activator
+				.getDefault().getColaSynchronizationStrategyFactory();
+
 		assertNotNull(factory);
-		
+
 		ID channelId = IDFactory.getDefault().createStringID(COLA);
+
+		// TODO working in progress
+		//Simulate a typing in a one-way. Initiator -> Receiver
+
+		//Document to control
+		String text=
+			"package TestPackage;\n" +
+			"/*\n" +
+			"* comment\n" +
+			"*/\n" +
+			"	public class Class {\n" +
+			"		// comment1\n" +
+			"		public void method1() {\n" +
+			"		}\n" +
+			"		// comment2\n" +
+			"		public void method2() {\n" +
+			"		}\n" +
+			"	}\n";
+
+		Document doc = new Document(text);
 		
-		//TODO working in progress, it is still a "dummy" test
+		Initiator initiator = new Initiator(factory
+				.createDocumentSynchronizationStrategy(channelId, true), doc);
+
+		Receiver receiver = new Receiver(factory
+				.createDocumentSynchronizationStrategy(channelId, false), doc);
 		
-		IDocumentSynchronizationStrategy initiator = factory.createDocumentSynchronizationStrategy(channelId, true);
-		IDocumentSynchronizationStrategy receiver = factory.createDocumentSynchronizationStrategy(channelId, false);
-		
-		DocumentChangeMessage message = new DocumentChangeMessage(0, 4, "cola");
-		IDocumentChangeMessage messages [] = initiator.registerLocalChange(message);
-		
-		IDocumentChange[] changes = receiver.transformRemoteChange((IDocumentChange) messages[0]);
-		assertEquals("cola", changes[0].getText());
-		
-		
+
+		initiator.setReceiverQueue(receiver.getQueue());
+
+		// receiver.setInitiatorQueue(initiator.getQueue());
+
+		initiator.start();
+		receiver.start();
+
+		//FIXME
+		Thread.sleep(2000);
+
+		String initiatorText = initiator.getDocument().get();
+		String receiverText = receiver.getDocument().get();
+		assertEquals(receiverText, initiatorText);
+
+
 	}
 
 }
