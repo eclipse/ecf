c626bcc74ac06edb435585a0b32bbcfdbc8b1a9f
mkuppe <mkuppe>
2008-10-16 11:17:50 +0000

    251073:[Discovery][jSLP] Add search filter test with broken parenthesis https://bugs.eclipse.org/bugs/show_bug.cgi?id=251073

--- protocols/bundles/ch.ethz.iks.slp/runtimeTests/src/main/java/ch/ethz/iks/slp/impl/SelfDiscoveryTest.java
+++ protocols/bundles/ch.ethz.iks.slp/runtimeTests/src/main/java/ch/ethz/iks/slp/impl/SelfDiscoveryTest.java
@@ -1,26 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Versant Corp.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Markus Kuppe (mkuppe <at> versant <dot> com) - initial API and implementation
+ ******************************************************************************/
 package ch.ethz.iks.slp.impl;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
 
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import ch.ethz.iks.slp.ServiceLocationEnumeration;
 import ch.ethz.iks.slp.ServiceLocationException;
 import ch.ethz.iks.slp.ServiceType;
 import ch.ethz.iks.slp.ServiceURL;
-import junit.framework.Assert;
-import junit.framework.TestCase;
 
 public class SelfDiscoveryTest extends TestCase {
 
+	private final String HOST_AND_PORT = System.getProperty("net.slp.tests.hostAndPort", "gantenbein:123");
 	private ServiceURL service;
 	
 	public SelfDiscoveryTest() {
 		super("runTests");
 	}
 
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
 	public void setUp() throws InterruptedException {
 		try {
-			service = new ServiceURL("service:osgi://gantenbein:123", 10800);
+			service = new ServiceURL("service:osgi://" + HOST_AND_PORT, 10800);
 			Dictionary properties = new Hashtable();
 			properties.put("attr", Boolean.FALSE);
 			properties.put("other", "value");
@@ -31,6 +45,9 @@ public class SelfDiscoveryTest extends TestCase {
 		}
 	}
 
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
 	public void tearDown() throws InterruptedException {
 		try {
 			TestActivator.advertiser.deregister(service);
@@ -44,33 +61,36 @@ public class SelfDiscoveryTest extends TestCase {
 		testService();
 		testAttributes();
 		testFilter();
-		testFilter2();
+		testFilterWithWildcard();
+		testFilterWithBrokenParenthesis();
 	}
 	
+	/**
+	 * Test method for
+	 * {@link ch.ethz.iks.slp.Locator}.
+	 */
 	public void testService() throws Exception {
-
-		System.out.println("locating service:osgi");
 		int count = 0;
 		for (ServiceLocationEnumeration services = TestActivator.locator
 				.findServices(new ServiceType("service:osgi"), null, null); services
 				.hasMoreElements();) {
 			assertEquals(services.next().toString(),
-					"service:osgi://gantenbein:123");
+					"service:osgi://"  + HOST_AND_PORT);
 			count++;
 		}
 		assertEquals(count, 1);
-
 	}
 
+	/**
+	 * Test method for
+	 * {@link ch.ethz.iks.slp.Locator}.
+	 */
 	public void testAttributes() throws Exception {
-
-		System.out.println("listing the attributes for service:osgi");
 		int count = 0;
 		for (ServiceLocationEnumeration attributes = TestActivator.locator
 				.findAttributes(new ServiceType("service:osgi"), null, null); attributes
 				.hasMoreElements();) {
 			final String attribute = attributes.next().toString();
-			System.out.println("attribute " + attribute);
 			assertTrue("(attr=false)".equals(attribute)
 					|| "(other=value)".equals(attribute));
 			count++;
@@ -78,31 +98,57 @@ public class SelfDiscoveryTest extends TestCase {
 		assertEquals(count, 2);
 	}
 
+	/**
+	 * Test method for
+	 * {@link ch.ethz.iks.slp.Locator}.
+	 */
 	public void testFilter() throws Exception {
-
-		System.out.println("locating service:osgi with filter (attr=false)");
 		int count = 0;
 		for (ServiceLocationEnumeration services = TestActivator.locator
 				.findServices(new ServiceType("service:osgi"), null,
 						"(attr=false)"); services.hasMoreElements();) {
 			assertEquals(services.next().toString(),
-					"service:osgi://gantenbein:123");
+					"service:osgi://" + HOST_AND_PORT);
 			count++;
 		}
 		assertEquals(count, 1);
 	}
 
-	public void testFilter2() throws Exception {
-
-		System.out.println("locating service:osgi with filter (attr=*)");
+	/**
+	 * Test method for
+	 * {@link ch.ethz.iks.slp.Locator}.
+	 */
+	public void testFilterWithWildcard() throws Exception {
 		int count = 0;
 		for (ServiceLocationEnumeration services = TestActivator.locator
 				.findServices(new ServiceType("service:osgi"), null, "(attr=*)"); services
 				.hasMoreElements();) {
 			assertEquals(services.next().toString(),
-					"service:osgi://gantenbein:123");
+					"service:osgi://" + HOST_AND_PORT);
 			count++;
 		}
 		assertEquals(count, 1);
+	}
+	
+
+	/**
+	 * Test method for
+	 * {@link ch.ethz.iks.slp.Locator}.
+	 * 
+	 * [1] http://www.faqs.org/rfcs/rfc1960.html
+	 * Cite: If a <value> must contain one of the characters '*' or '(' or ')', these
+	 * characters should be escaped by preceding them with the backslash '\' character.
+	 */
+	public void testFilterWithBrokenParenthesis() throws Exception {
+		try {
+			// correct filter is (service-type=\(service:osgi\))
+			TestActivator.locator.findServices(new ServiceType("service:osgi"), null,
+				"(service-type=(service:osgi))");
+		} catch (ServiceLocationException e) {
+			if(e.getErrorCode() == 20) {
+				return;
+			}
+		}
+		fail();
 	}
 }
