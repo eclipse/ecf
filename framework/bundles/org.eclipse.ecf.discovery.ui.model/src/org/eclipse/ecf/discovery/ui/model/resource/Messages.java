110885115de5961c8171cbc39ad1be6ea818c658
mkuppe <mkuppe>
2008-12-12 08:12:52 +0000

    NEW - bug 256603: [Discovery][Remotesvcs] Replace org.eclipse.ecf.discovery.ui with EMF based ui (CQ 2038) https://bugs.eclipse.org/bugs/show_bug.cgi?id=256603

---
+++ framework/bundles/org.eclipse.ecf.discovery.ui.model/src/org/eclipse/ecf/discovery/ui/model/resource/Messages.java
@@ -0,0 +1,24 @@
+/****************************************************************************
+ * Copyright (c) 2008 Versant Corp. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Versant Corp. - initial API and implementation
+ *****************************************************************************/
+package org.eclipse.ecf.discovery.ui.model.resource;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.ecf.discovery.model.resource.messages"; //$NON-NLS-1$
+	public static String ServiceResource_NO_DISCOVERY_CONTAINER_AVAILABLE;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {}
+}
