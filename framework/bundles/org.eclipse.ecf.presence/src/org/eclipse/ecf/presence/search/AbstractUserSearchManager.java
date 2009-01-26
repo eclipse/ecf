67e6a5932dcfcca657b7580223a0e22a95010ff1
mmayworm <mmayworm>
2009-01-26 23:34:38 +0000

    [bug:256696] adding user search API on HEAD

---
+++ framework/bundles/org.eclipse.ecf.presence/src/org/eclipse/ecf/presence/search/AbstractUserSearchManager.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Marcelo Mayworm. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 	Marcelo Mayworm - initial API and implementation
+ *
+ ******************************************************************************/
+package org.eclipse.ecf.presence.search;
+
+import java.util.*;
+
+/**
+ * Abstract implementation of IUserSearchManager. Provides implementations of listener
+ * methods that subsclasses may use to avoid having to implement them
+ * themselves. This class may be subclassed as needed.
+ * @since 3.0
+ */
+public abstract class AbstractUserSearchManager implements IUserSearchManager {
+
+	/** keep the list of listeners */
+	private final List userSearchListeners = new ArrayList(5);
+
+	/**
+	 * Add listener to {@link IUserSearchListener}. The listener's handleEvent method will be
+	 * asynchronously called.
+	 * @param listener
+	 * 				 the IUserSearchListener to add
+	 */
+	public void addListener(IUserSearchListener l) {
+		synchronized (userSearchListeners) {
+			userSearchListeners.add(l);
+		}
+	}
+
+	/**
+	 * Remove listener from the listener list.
+	 * 
+	 * @param listener
+	 *            the IUserSearchListener to remove
+	 */
+	public void removeListener(IUserSearchListener l) {
+		synchronized (userSearchListeners) {
+			userSearchListeners.remove(l);
+		}
+	}
+
+	/**
+	 * Fires a user search event, invoking the {@link IUserSearchListener}.
+	 * 
+	 * @param event IUserSearchEvent.
+	 */
+	protected void fireUserSearchEvent(IUserSearchEvent event) {
+		List toNotify = null;
+		// Copy array
+		synchronized (userSearchListeners) {
+			toNotify = new ArrayList(userSearchListeners);
+		}
+		// Notify all in toNotify
+		for (Iterator i = toNotify.iterator(); i.hasNext();) {
+			IUserSearchListener l = (IUserSearchListener) i.next();
+			l.handleUserSearchEvent(event);
+		}
+	}
+
+	/**
+	 * Returns a specific implementation of {@link IUserSearchListener} for each provider.
+	 * This listener usually is invoked from an user search event ({@link IUserSearchEvent}),
+	 * fired for a non-blocking search.
+	 * @return IUserSearchListener. Will not be <code>null</code>
+	 */
+	protected abstract IUserSearchListener createUserSearchListener();
+
+}
---
+++ framework/bundles/org.eclipse.ecf.presence/src/org/eclipse/ecf/presence/search/UserSearchCompleteEvent.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Marcelo Mayworm. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: 	Marcelo Mayworm - initial API and implementation
+ *
+ ******************************************************************************/
+package org.eclipse.ecf.presence.search;
+
+/**
+ * 
+ * @since 3.0
+ */
+public class UserSearchCompleteEvent implements IUserSearchCompleteEvent {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1563126741187575435L;
+	private ISearch search;
+
+	public UserSearchCompleteEvent(ISearch search) {
+		this.search = search;
+	}
+
+	public ISearch getSearch() {
+		return search;
+	}
+
+}
