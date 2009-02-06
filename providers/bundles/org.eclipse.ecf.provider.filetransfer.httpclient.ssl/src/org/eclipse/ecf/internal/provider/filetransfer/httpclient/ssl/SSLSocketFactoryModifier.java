65d0dafeb172685c0ccd241df94168d4fdefc610
slewis <slewis>
2009-02-06 18:57:28 +0000

    Fixes for https://bugs.eclipse.org/bugs/show_bug.cgi?id=263869

--- providers/bundles/org.eclipse.ecf.provider.filetransfer.httpclient.ssl/src/org/eclipse/ecf/internal/provider/filetransfer/httpclient/ssl/SSLSocketFactoryModifier.java
+++ providers/bundles/org.eclipse.ecf.provider.filetransfer.httpclient.ssl/src/org/eclipse/ecf/internal/provider/filetransfer/httpclient/ssl/SSLSocketFactoryModifier.java
@@ -1,5 +1,5 @@
 /****************************************************************************
- * Copyright (c) 2008 Composent, Inc. and others.
+ * Copyright (c) 2008, 2009 Composent, Inc., IBM and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,6 +7,7 @@
  *
  * Contributors:
  *    Composent, Inc. - initial API and implementation
+ *    Henrich Kraemer - bug 263869, testHttpsReceiveFile fails using HTTP proxy
  *****************************************************************************/
 
 package org.eclipse.ecf.internal.provider.filetransfer.httpclient.ssl;
@@ -14,25 +15,19 @@ package org.eclipse.ecf.internal.provider.filetransfer.httpclient.ssl;
 import java.io.IOException;
 import java.net.*;
 import javax.net.ssl.SSLSocketFactory;
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.ConnectTimeoutException;
 import org.apache.commons.httpclient.params.HttpConnectionParams;
 import org.apache.commons.httpclient.protocol.Protocol;
-import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
-import org.eclipse.ecf.core.util.Proxy;
-import org.eclipse.ecf.core.util.ProxyAddress;
+import org.apache.commons.httpclient.protocol.SecureProtocolSocketFactory;
 import org.eclipse.ecf.internal.provider.filetransfer.httpclient.Activator;
 import org.eclipse.ecf.internal.provider.filetransfer.httpclient.ISSLSocketFactoryModifier;
 
 /**
  *
  */
-public class SSLSocketFactoryModifier implements ISSLSocketFactoryModifier, ProtocolSocketFactory {
-
-	private Proxy proxy;
+public class SSLSocketFactoryModifier implements ISSLSocketFactoryModifier, SecureProtocolSocketFactory {
 
 	public void dispose() {
-		this.proxy = null;
 		Protocol.unregisterProtocol("https"); //$NON-NLS-1$
 	}
 
@@ -40,9 +35,7 @@ public class SSLSocketFactoryModifier implements ISSLSocketFactoryModifier, Prot
 	 * @see org.apache.commons.httpclient.protocol.ProtocolSocketFactory#createSocket(java.lang.String, int)
 	 */
 	public Socket createSocket(String host, int port) throws IOException, UnknownHostException {
-		final SSLSocketFactory factory = Activator.getDefault().getSSLSocketFactory();
-		if (factory == null)
-			throw new IOException("Cannot get socket factory"); //$NON-NLS-1$
+		final SSLSocketFactory factory = getSSLSocketFactory();
 		return factory.createSocket(host, port);
 	}
 
@@ -50,53 +43,37 @@ public class SSLSocketFactoryModifier implements ISSLSocketFactoryModifier, Prot
 	 * @see org.apache.commons.httpclient.protocol.ProtocolSocketFactory#createSocket(java.lang.String, int, java.net.InetAddress, int)
 	 */
 	public Socket createSocket(String host, int port, InetAddress localAddress, int localPort) throws IOException, UnknownHostException {
-		final SSLSocketFactory factory = Activator.getDefault().getSSLSocketFactory();
-		if (factory == null)
-			throw new IOException("Cannot get socket factory"); //$NON-NLS-1$
+		final SSLSocketFactory factory = getSSLSocketFactory();
 		return factory.createSocket(host, port, localAddress, localPort);
 	}
 
-	public Socket createSocket(String remoteHost, int remotePort, InetAddress clientHost, int clientPort, HttpConnectionParams params) throws IOException, UnknownHostException, ConnectTimeoutException {
+	private SSLSocketFactory getSSLSocketFactory() throws IOException {
 		final SSLSocketFactory factory = Activator.getDefault().getSSLSocketFactory();
 		if (factory == null)
 			throw new IOException("Cannot get socket factory"); //$NON-NLS-1$
+		return factory;
+	}
+
+	public Socket createSocket(String remoteHost, int remotePort, InetAddress clientHost, int clientPort, HttpConnectionParams params) throws IOException, UnknownHostException, ConnectTimeoutException {
+		final SSLSocketFactory factory = getSSLSocketFactory();
 		if (params == null || params.getConnectionTimeout() == 0)
 			return factory.createSocket(remoteHost, remotePort, clientHost, clientPort);
 
-		if (proxy != null && !Proxy.NO_PROXY.equals(proxy)) {
-			final ProxyClient proxyClient = new ProxyClient();
-
-			final ProxyAddress address = proxy.getAddress();
-			proxyClient.getHostConfiguration().setProxy(address.getHostName(), address.getPort());
-			proxyClient.getHostConfiguration().setHost(remoteHost, remotePort);
-			final String proxyUsername = proxy.getUsername();
-			final String proxyPassword = proxy.getPassword();
-			if (proxyUsername != null && !proxyUsername.equals("")) { //$NON-NLS-1$
-				final Credentials credentials = new UsernamePasswordCredentials(proxyUsername, proxyPassword);
-				final AuthScope proxyAuthScope = new AuthScope(address.getHostName(), address.getPort(), AuthScope.ANY_REALM);
-				proxyClient.getState().setProxyCredentials(proxyAuthScope, credentials);
-			}
-
-			final ProxyClient.ConnectResponse response = proxyClient.connect();
-			if (response.getSocket() != null) {
-				// tunnel SSL via the resultant socket
-				final Socket sslsocket = factory.createSocket(response.getSocket(), remoteHost, remotePort, true);
-				return sslsocket;
-			}
-		}
-		// Direct connection
-		//		final Socket socket = factory.createSocket();
 		final Socket socket = factory.createSocket(remoteHost, remotePort, clientHost, clientPort);
 		// in httpclient, it seems like they will set the time out for you
 		//		socket.connect(new InetSocketAddress(remoteHost, remotePort), params.getConnectionTimeout());
 		return socket;
 	}
 
+	public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
+		final SSLSocketFactory factory = getSSLSocketFactory();
+		return factory.createSocket(socket, host, port, autoClose);
+	}
+
 	/* (non-Javadoc)
-	 * @see org.eclipse.ecf.internal.provider.filetransfer.httpclient.ISSLSocketFactoryModifier#getProtocolSocketFactoryForProxy(org.eclipse.ecf.core.util.Proxy)
+	 * @see org.eclipse.ecf.internal.provider.filetransfer.httpclient.ISSLSocketFactoryModifier#getProtocolSocketFactory()
 	 */
-	public ProtocolSocketFactory getProtocolSocketFactoryForProxy(Proxy p) {
-		this.proxy = p;
+	public SecureProtocolSocketFactory getProtocolSocketFactory() {
 		return this;
 	}
 
