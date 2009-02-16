da49ddcaf3353522b55d780b5feccf095c0c9d2a
mkuppe <mkuppe>
2009-02-16 09:06:33 +0000

    NEW - bug 264984: [Discovery][Composite] junit.framework.AssertionFailedError: Test listener received more than 1 discovery event expected:<1> but was:<2> https://bugs.eclipse.org/bugs/show_bug.cgi?id=264984

--- tests/bundles/org.eclipse.ecf.tests.provider.discovery/src/org/eclipse/ecf/tests/provider/discovery/CompositeDiscoveryContainerTest.java
+++ tests/bundles/org.eclipse.ecf.tests.provider.discovery/src/org/eclipse/ecf/tests/provider/discovery/CompositeDiscoveryContainerTest.java
@@ -1,13 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2008 Versant Corp.
+ * Copyright (c) 2009 Markus Alexander Kuppe.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     Markus Kuppe (mkuppe <at> versant <dot> com) - initial API and implementation
- ******************************************************************************/
+ *     Markus Alexander Kuppe (ecf-dev_eclipse.org <at> lemmster <dot> de) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.ecf.tests.provider.discovery;
 
 import java.util.List;
@@ -23,6 +23,8 @@ public class CompositeDiscoveryContainerTest extends DiscoveryTest {
 	public CompositeDiscoveryContainerTest() {
 		super("ecf.discovery.*");
 		setComparator(new CompositeServiceInfoComporator());
+		// Composite receives from more the nested IDCAs and thus may receive more than one event (ATM we have two nested providers in ECF)
+		eventsToExpect = 2;
 	}
 	
 	/**
