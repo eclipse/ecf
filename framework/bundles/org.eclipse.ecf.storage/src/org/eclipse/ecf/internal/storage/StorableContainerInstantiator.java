dbb41716f94fb22b27f52cb66d9aee6ab49273ca
slewis <slewis>
2009-02-20 01:39:45 +0000

    Added API to core for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=249240

--- framework/bundles/org.eclipse.ecf.storage/src/org/eclipse/ecf/internal/storage/StorableContainerInstantiator.java
+++ framework/bundles/org.eclipse.ecf.storage/src/org/eclipse/ecf/internal/storage/StorableContainerInstantiator.java
@@ -3,23 +3,31 @@ package org.eclipse.ecf.internal.storage;
 import org.eclipse.ecf.core.*;
 import org.eclipse.ecf.core.identity.*;
 import org.eclipse.ecf.core.provider.BaseContainerInstantiator;
+import org.eclipse.ecf.storage.IStorableContainerAdapter;
 
 public class StorableContainerInstantiator extends BaseContainerInstantiator {
 
 	private static long nextBaseContainerID = 0L;
 
-	public IContainer createInstance(ContainerTypeDescription description, Object[] parameters) throws ContainerCreateException {
+	public IContainer createInstance(ContainerTypeDescription description,
+			Object[] parameters) throws ContainerCreateException {
 		try {
 			if (parameters != null && parameters.length > 0) {
 				if (parameters[0] instanceof ID)
 					return new StorableBaseContainer((ID) parameters[0]);
 				if (parameters[0] instanceof String)
-					return new StorableBaseContainer(IDFactory.getDefault().createStringID((String) parameters[0]));
+					return new StorableBaseContainer(IDFactory.getDefault()
+							.createStringID((String) parameters[0]));
 			}
 		} catch (IDCreateException e) {
-			throw new ContainerCreateException("Could not create StorableBaseContainer"); //$NON-NLS-1$
+			throw new ContainerCreateException(
+					"Could not create StorableBaseContainer"); //$NON-NLS-1$
 		}
 		return new StorableBaseContainer(nextBaseContainerID++);
 	}
 
+	public String[] getSupportedAdapterTypes(
+			ContainerTypeDescription description) {
+		return new String[] { IStorableContainerAdapter.class.getName() };
+	}
 }
