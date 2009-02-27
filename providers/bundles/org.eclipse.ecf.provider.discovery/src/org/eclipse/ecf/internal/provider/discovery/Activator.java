991456ace4677246dea52974f7e4a814ef809906
mkuppe <mkuppe>
2009-02-27 08:05:32 +0000

    RESOLVED - bug 266428: [Discovery][Composite][JmDNS] org.osgi.framework.Constants.SERVICE_RANKING not set https://bugs.eclipse.org/bugs/show_bug.cgi?id=266428

--- providers/bundles/org.eclipse.ecf.provider.discovery/src/org/eclipse/ecf/internal/provider/discovery/Activator.java
+++ providers/bundles/org.eclipse.ecf.provider.discovery/src/org/eclipse/ecf/internal/provider/discovery/Activator.java
@@ -50,7 +50,7 @@ public class Activator implements BundleActivator {
 		final Properties props = new Properties();
 		props.put(IDiscoveryService.CONTAINER_ID, IDFactory.getDefault().createStringID("org.eclipse.ecf.provider.discovery.CompositeDiscoveryContainer")); //$NON-NLS-1$
 		props.put(IDiscoveryLocator.CONTAINER_NAME, CompositeDiscoveryContainer.NAME);
-		props.put(Constants.SERVICE_RANKING, Integer.toString(1000));
+		props.put(Constants.SERVICE_RANKING, new Integer(1000));
 		String[] clazzes = new String[] {IDiscoveryService.class.getName(), IDiscoveryLocator.class.getName(), IDiscoveryAdvertiser.class.getName()};
 		context.registerService(clazzes, new ServiceFactory() {
 
