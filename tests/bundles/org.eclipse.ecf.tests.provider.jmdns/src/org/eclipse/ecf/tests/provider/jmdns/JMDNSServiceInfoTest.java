d7b34eccaa723bfcee63ba5b6a46baba7ead64ea
mkuppe <mkuppe>
2009-03-10 16:04:47 +0000

    NEW - bug 266723: [Discovery] specify how clients can determine service uniqueness https://bugs.eclipse.org/bugs/show_bug.cgi?id=266723

--- tests/bundles/org.eclipse.ecf.tests.provider.jmdns/src/org/eclipse/ecf/tests/provider/jmdns/JMDNSServiceInfoTest.java
+++ tests/bundles/org.eclipse.ecf.tests.provider.jmdns/src/org/eclipse/ecf/tests/provider/jmdns/JMDNSServiceInfoTest.java
@@ -10,45 +10,13 @@
  ******************************************************************************/
 package org.eclipse.ecf.tests.provider.jmdns;
 
-import org.eclipse.ecf.core.identity.IDCreateException;
 import org.eclipse.ecf.core.identity.IDFactory;
-import org.eclipse.ecf.core.identity.Namespace;
-import org.eclipse.ecf.discovery.IServiceInfo;
-import org.eclipse.ecf.discovery.ServiceInfo;
-import org.eclipse.ecf.discovery.ServiceProperties;
-import org.eclipse.ecf.discovery.identity.IServiceID;
 import org.eclipse.ecf.provider.jmdns.identity.JMDNSNamespace;
-import org.eclipse.ecf.tests.discovery.DiscoveryTestHelper;
 import org.eclipse.ecf.tests.discovery.ServiceInfoTest;
 
 public class JMDNSServiceInfoTest extends ServiceInfoTest {
 
 	public JMDNSServiceInfoTest() {
-		super();
-		uri = DiscoveryTestHelper.createDefaultURI();
-		priority = DiscoveryTestHelper.PRIORITY;
-		weight = DiscoveryTestHelper.WEIGHT;
-		serviceProperties = new ServiceProperties();
-		serviceProperties.setProperty("foobar", new String("foobar"));
-		serviceProperties.setPropertyBytes("foobar1", new byte[] { 1, 2, 3 });
-		Namespace namespace = IDFactory.getDefault().getNamespaceByName(
-				JMDNSNamespace.NAME);
-		try {
-			serviceID = (IServiceID) IDFactory.getDefault().createID(namespace,
-					new Object[] {DiscoveryTestHelper.SERVICE_TYPE, DiscoveryTestHelper.getHost()});
-		} catch (IDCreateException e) {
-			fail(e.getMessage());
-		}
-
-		serviceInfo = new ServiceInfo(uri, serviceID, priority, weight,
-				serviceProperties);
+		super(IDFactory.getDefault().getNamespaceByName(JMDNSNamespace.NAME));
 	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ecf.tests.discovery.ServiceInfoTest#getServiceInfo(org.eclipse.ecf.discovery.IServiceInfo)
-	 */
-	protected IServiceInfo getServiceInfo(IServiceInfo aServiceInfo) {
-		return aServiceInfo;
-	}
-
 }
