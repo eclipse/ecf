98be8974b7ae66c80d5d85e9560d06942e81975c
slewis <slewis>
2009-04-01 18:27:21 +0000

    Added debug and timeout from patch on bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=270644

--- providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/events/socket/SocketEventCreateUtil.java
+++ providers/bundles/org.eclipse.ecf.provider.filetransfer/src/org/eclipse/ecf/provider/filetransfer/events/socket/SocketEventCreateUtil.java
@@ -14,10 +14,13 @@ package org.eclipse.ecf.provider.filetransfer.events.socket;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import org.eclipse.ecf.core.util.Trace;
 import org.eclipse.ecf.filetransfer.events.socket.ISocketEvent;
 import org.eclipse.ecf.filetransfer.events.socket.ISocketEventSource;
 import org.eclipse.ecf.filetransfer.events.socket.ISocketListener;
 import org.eclipse.ecf.filetransfer.events.socketfactory.INonconnectedSocketFactory;
+import org.eclipse.ecf.internal.filetransfer.Activator;
+import org.eclipse.ecf.internal.provider.filetransfer.DebugOptions;
 
 public class SocketEventCreateUtil {
 
@@ -29,19 +32,27 @@ public class SocketEventCreateUtil {
 	}
 
 	public static Socket createSocket(final ISocketListener spyListener, final ISocketEventSource socketEventSource, final INonconnectedSocketFactory unconnectedFactory, final InetSocketAddress remoteInetAddress, final InetSocketAddress localInetAddress, int timeout) throws IOException {
+		Trace.entering(Activator.PLUGIN_ID, DebugOptions.METHODS_ENTERING, SocketEventCreateUtil.class, "createSocket " + remoteInetAddress.toString() + " timeout=" + timeout); //$NON-NLS-1$ //$NON-NLS-2$
+
 		final Socket factorySocket = unconnectedFactory.createSocket();
 		fireEvent(spyListener, new SocketCreatedEvent(socketEventSource, factorySocket));
 		try {
+			Trace.trace(Activator.PLUGIN_ID, "bind(" + localInetAddress.toString() + ")"); //$NON-NLS-1$//$NON-NLS-2$
 			factorySocket.bind(localInetAddress);
+			Trace.trace(Activator.PLUGIN_ID, "connect(" + remoteInetAddress.toString() + ", " + timeout + ")"); //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$
 			factorySocket.connect(remoteInetAddress, timeout);
+			Trace.trace(Activator.PLUGIN_ID, "connected"); //$NON-NLS-1$
 		} catch (IOException e) {
+			Trace.catching(Activator.PLUGIN_ID, DebugOptions.EXCEPTIONS_CATCHING, SocketEventCreateUtil.class, "createSocket", e); //$NON-NLS-1$
 			fireEvent(spyListener, new SocketClosedEvent(socketEventSource, factorySocket, factorySocket));
+			Trace.throwing(Activator.PLUGIN_ID, DebugOptions.EXCEPTIONS_THROWING, SocketEventCreateUtil.class, "createSocket", e); //$NON-NLS-1$
 			throw e;
 		}
 		final Socket[] wrap = new Socket[1];
 		final Socket myWrap = new AbstractSocketWrapper(factorySocket) {
 			public void close() throws IOException {
 				try {
+					Trace.trace(Activator.PLUGIN_ID, "closing socket " + this.toString()); //$NON-NLS-1$
 					super.close();
 				} finally {
 					fireEvent(spyListener, new SocketClosedEvent(socketEventSource, factorySocket, wrap[0]));
