15ed8a752745d8be820a8f1418585ff5c245d18b
rsuen <rsuen>
2009-04-26 00:42:57 +0000

    Javadoc clarifications.

--- framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/datashare/events/IChannelContainerEvent.java
+++ framework/bundles/org.eclipse.ecf.datashare/src/org/eclipse/ecf/datashare/events/IChannelContainerEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004 Composent, Inc., Peter Nehrer, Boris Bokowski. All rights reserved. This
+ * Copyright (c) 2004, 2009 Composent, Inc., Peter Nehrer, Boris Bokowski. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -18,16 +18,16 @@ import org.eclipse.ecf.core.identity.ID;
  */
 public interface IChannelContainerEvent {
 	/**
-	 * Get the id of the channel associated with this event
+	 * Get the id of the channel associated with this event.
 	 * 
 	 * @return ID of the channel. Will not be <code>null</code>.
 	 */
 	public ID getChannelID();
 
 	/**
-	 * Get the id of the channel container associated with this event
+	 * Get the id of the channel's originating source IContainer associated with this event.
 	 * 
-	 * @return ID of the channel container. Will not be <code>null</code>.
+	 * @return ID of the channel's originating source IContainer. Will not be <code>null</code>.
 	 */
 	public ID getChannelContainerID();
 }
