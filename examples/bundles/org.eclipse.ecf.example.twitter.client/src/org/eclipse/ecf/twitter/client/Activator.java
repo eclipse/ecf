d53f190057e96dc6f44cf66eceeb7db8fd46ef9f
pperret <pperret>
2009-05-17 16:11:20 +0000

    Twitter client application for TwitterHub

---
+++ examples/bundles/org.eclipse.ecf.example.twitter.client/src/org/eclipse/ecf/twitter/client/Activator.java
@@ -0,0 +1,25 @@
+package org.eclipse.ecf.twitter.client;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	private IStatus status;
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
