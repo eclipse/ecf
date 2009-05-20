c06b5f982fde3ba499b97b7edb143521b676390d
slewis <slewis>
2009-05-20 01:41:32 +0000

    Added documentation to listener interfaces

--- compendium/bundles/org.eclipse.ecf.osgi.services.distribution/src/org/eclipse/ecf/osgi/services/distribution/IHostDistributionListener.java
+++ compendium/bundles/org.eclipse.ecf.osgi.services.distribution/src/org/eclipse/ecf/osgi/services/distribution/IHostDistributionListener.java
@@ -13,13 +13,48 @@ import org.eclipse.ecf.remoteservice.IRemoteServiceContainer;
 import org.eclipse.ecf.remoteservice.IRemoteServiceRegistration;
 import org.osgi.framework.ServiceReference;
 
+/**
+ * Listener for host distribution events. Services registered with this as their
+ * service interface will have their methods called when the distribution
+ * implementation events occur.
+ * 
+ */
 public interface IHostDistributionListener {
 
+	/**
+	 * Event indicating that a remote service has been registered, with the
+	 * given local serviceReference, the given remoteServiceContainer, and the
+	 * given remoteRegistration.
+	 * 
+	 * @param serviceReference
+	 *            the ServiceReference of the locally registered service. Will
+	 *            not be <code>null</code>.
+	 * 
+	 * @param remoteServiceContainer
+	 *            the remoteServiceContainer that is doing the distribution for
+	 *            this remote service. Will not be <code>null</code>.
+	 * 
+	 * @param remoteRegistration
+	 *            The remote service registration created with successful
+	 *            registration with the remoteServiceContainer. Will not be
+	 *            <code>null</code>.
+	 */
 	public void registered(ServiceReference serviceReference,
 			IRemoteServiceContainer remoteServiceContainer,
 			IRemoteServiceRegistration remoteRegistration);
 
+	/**
+	 * Event indicating that a remote service has been unregistered.
+	 * 
+	 * @param serviceReference
+	 *            the ServiceReference of the locally registered service. Will
+	 *            not be <code>null</code>.
+	 * 
+	 * @param remoteRegistration
+	 *            The remote service registration previously created upon
+	 *            registration. Will not be <code>null</code>.
+	 */
 	public void unregistered(ServiceReference serviceReference,
-			IRemoteServiceRegistration remoteReference);
+			IRemoteServiceRegistration remoteRegistration);
 
 }
