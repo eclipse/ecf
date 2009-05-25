a17c00dcf103c49a119a85d158f9ef753bea90a9
slewis <slewis>
2009-05-25 00:55:28 +0000

    Initial checkin of hello remoteservices example.

---
+++ examples/bundles/org.eclipse.ecf.examples.remoteservices.hello/src/org/eclipse/ecf/examples/remoteservices/hello/impl/Hello.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+* Copyright (c) 2009 EclipseSource and others. All rights reserved. This
+* program and the accompanying materials are made available under the terms of
+* the Eclipse Public License v1.0 which accompanies this distribution, and is
+* available at http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*   EclipseSource - initial API and implementation
+******************************************************************************/
+package org.eclipse.ecf.examples.remoteservices.hello.impl;
+
+import org.eclipse.ecf.examples.remoteservices.hello.IHello;
+
+public class Hello implements IHello {
+
+	public void hello() {
+		System.out.println("Got hello() from unknown");
+	}
+
+}
