c784395d47e4a30d98a88cd001d520017fd6ecc2
slewis <slewis>
2009-05-28 04:14:31 +0000

    Fixes for hello host and consumer

--- examples/bundles/org.eclipse.ecf.examples.remoteservices.hello.consumer/src/org/eclipse/ecf/internal/examples/remoteservices/hello/consumer/Activator.java
+++ examples/bundles/org.eclipse.ecf.examples.remoteservices.hello.consumer/src/org/eclipse/ecf/internal/examples/remoteservices/hello/consumer/Activator.java
@@ -3,8 +3,14 @@ package org.eclipse.ecf.internal.examples.remoteservices.hello.consumer;
 import org.eclipse.ecf.core.IContainerManager;
 import org.eclipse.ecf.examples.remoteservices.hello.IHello;
 import org.eclipse.ecf.osgi.services.distribution.IDistributionConstants;
+import org.eclipse.ecf.remoteservice.IRemoteCall;
+import org.eclipse.ecf.remoteservice.IRemoteService;
+import org.eclipse.ecf.remoteservice.RemoteServiceHelper;
+import org.eclipse.equinox.concurrent.future.IFuture;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Filter;
+import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
@@ -24,11 +30,17 @@ public class Activator implements BundleActivator, IDistributionConstants, Servi
 		// Create R-OSGi Container
 		IContainerManager containerManager = getContainerManagerService();
 		containerManager.getContainerFactory().createContainer("ecf.r_osgi.peer");
-
-		helloServiceTracker = new ServiceTracker(context,IHello.class.getName(),this);
+		// Create service tracker to track IHello instances that are REMOTE
+		helloServiceTracker = new ServiceTracker(context,createRemoteFilter(),this);
 		helloServiceTracker.open();
 	}
 
+	private Filter createRemoteFilter() throws InvalidSyntaxException {
+		// This filter looks for IHello instances that have the REMOTE property set (are remote 
+		// services as per RFC119).
+		return context.createFilter("(&("+org.osgi.framework.Constants.OBJECTCLASS+"=" + IHello.class.getName() +")(" + REMOTE + "=*))");
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
@@ -39,12 +51,29 @@ public class Activator implements BundleActivator, IDistributionConstants, Servi
 		this.context = null;
 	}
 
+	/**
+	 * Method called when a REMOTE IHello instance is registered.
+	 */
 	public Object addingService(ServiceReference reference) {
 		System.out.println("IHello service proxy being added");
 		IHello hello = (IHello) context.getService(reference);
 		// Call it
 		hello.hello();
-		System.out.println("Called hello service");
+		System.out.println("Called hello() on proxy successfully");
+		
+		// Now get remote service reference and use asynchronous 
+		// remote invocation
+		IRemoteService remoteService = (IRemoteService) reference.getProperty(REMOTE);
+		// This futureExec returns immediately
+		IFuture future = RemoteServiceHelper.futureExec(remoteService, "hello", null);
+		
+		try {
+			// This method blocks until a return 
+			future.get();
+			System.out.println("Called futureExec successfully");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return hello;
 	}
 
