235550ed84ca660a7107cdbf27d805f7523fbabd
slewis <slewis>
2009-09-15 20:19:32 +0000

    Additions to support full implementation of OSGI EventAdmin specification

--- server-side/bundles/org.eclipse.ecf.remoteservice.eventadmin/src/org/eclipse/ecf/internal/remoteservice/eventadmin/DistributedEventAdminMessage.java
+++ server-side/bundles/org.eclipse.ecf.remoteservice.eventadmin/src/org/eclipse/ecf/internal/remoteservice/eventadmin/DistributedEventAdminMessage.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2009 EclipseSource and others. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*   EclipseSource - initial API and implementation
-******************************************************************************/
+ * Copyright (c) 2009 EclipseSource and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   EclipseSource - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.ecf.internal.remoteservice.eventadmin;
 
 import java.io.NotSerializableException;
@@ -14,6 +14,7 @@ import java.io.Serializable;
 import java.util.Hashtable;
 import java.util.Map;
 
+import org.eclipse.osgi.util.NLS;
 import org.osgi.service.event.Event;
 
 public class DistributedEventAdminMessage implements Serializable {
@@ -21,28 +22,36 @@ public class DistributedEventAdminMessage implements Serializable {
 	private static final long serialVersionUID = 2743430591605178391L;
 	private String topic;
 	private Map properties;
-	
+
 	private transient Event localEvent;
-	
-	public DistributedEventAdminMessage(Event event) throws NotSerializableException {
+
+	public DistributedEventAdminMessage(Event event)
+			throws NotSerializableException {
 		this.topic = event.getTopic();
 		this.properties = createPropertiesFromEvent(event);
 	}
 
-	private Map createPropertiesFromEvent(Event event) throws NotSerializableException {
+	private Map createPropertiesFromEvent(Event event)
+			throws NotSerializableException {
 		String[] propertyNames = event.getPropertyNames();
-		Hashtable ht = (propertyNames == null)?new Hashtable(1):new Hashtable(propertyNames.length);
-		for(int i=0; i < propertyNames.length; i++) {
+		Hashtable ht = (propertyNames == null) ? new Hashtable(1)
+				: new Hashtable(propertyNames.length);
+		for (int i = 0; i < propertyNames.length; i++) {
 			Object val = event.getProperty(propertyNames[i]);
-			if (!(val instanceof Serializable)) throw new NotSerializableException("Cannot serialize property value of name="+propertyNames[i]+", value="+val);
+			if (!(val instanceof Serializable))
+				throw new NotSerializableException(
+						NLS
+								.bind(
+										"Cannot serialize property value of name={0} and value={1}",
+										propertyNames[i], val));
 			ht.put(propertyNames[i], val);
 		}
 		return ht;
 	}
-	
+
 	public synchronized Event getEvent() {
 		if (localEvent == null) {
-			localEvent = new Event(topic,properties);
+			localEvent = new Event(topic, properties);
 		}
 		return localEvent;
 	}
--- server-side/bundles/org.eclipse.ecf.remoteservice.eventadmin/src/org/eclipse/ecf/remoteservice/eventadmin/DistributedEventAdmin.java
+++ server-side/bundles/org.eclipse.ecf.remoteservice.eventadmin/src/org/eclipse/ecf/remoteservice/eventadmin/DistributedEventAdmin.java
@@ -10,9 +10,10 @@
 package org.eclipse.ecf.remoteservice.eventadmin;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.security.Permission;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.core.runtime.IProgressMonitor;
@@ -25,133 +26,105 @@ import org.eclipse.ecf.core.sharedobject.ISharedObjectContainer;
 import org.eclipse.ecf.core.sharedobject.SharedObjectAddException;
 import org.eclipse.ecf.core.sharedobject.SharedObjectMsg;
 import org.eclipse.ecf.internal.remoteservice.eventadmin.DistributedEventAdminMessage;
+import org.eclipse.ecf.internal.remoteservice.eventadmin.EventHandlerTracker;
+import org.eclipse.ecf.internal.remoteservice.eventadmin.EventHandlerWrapper;
+import org.eclipse.ecf.internal.remoteservice.eventadmin.LogTracker;
 import org.eclipse.equinox.concurrent.future.IExecutor;
 import org.eclipse.equinox.concurrent.future.IProgressRunnable;
 import org.eclipse.equinox.concurrent.future.ThreadsExecutor;
+import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Filter;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventAdmin;
 import org.osgi.service.event.EventConstants;
-import org.osgi.service.event.EventHandler;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.service.event.TopicPermission;
+import org.osgi.service.log.LogService;
 
-public class DistributedEventAdmin extends BaseSharedObject implements EventAdmin {
+public class DistributedEventAdmin extends BaseSharedObject implements
+		EventAdmin {
 
 	private BundleContext context;
-	
-	private ServiceTracker eventHandlerServiceTracker;
-	private final Object eventHandlerSTLock = new Object();
-
-	private IExecutor executor;
-
 	private String topic;
 	private ID topicID;
-	
-	public DistributedEventAdmin(BundleContext context, String topic, IExecutor executor) {
+	private LogTracker log;
+
+	private EventHandlerTracker eventHandlerTracker;
+	private IExecutor executor;
+
+	public DistributedEventAdmin(BundleContext context, String topic,
+			IExecutor executor) {
 		this.context = context;
 		Assert.isNotNull(this.context);
 		this.topic = topic;
 		Assert.isNotNull(this.topic);
 		this.topicID = IDFactory.getDefault().createStringID(getTopic());
-		this.executor = (executor == null)?new ThreadsExecutor():executor;
+		this.log = new LogTracker(context, System.out);
+		this.executor = (executor == null) ? new ThreadsExecutor() : executor;
+		this.eventHandlerTracker = new EventHandlerTracker(context, log);
+		this.eventHandlerTracker.open();
 	}
-	
+
 	public DistributedEventAdmin(BundleContext context, String topic) {
-		this(context,topic,null);
+		this(context, topic, null);
 	}
 
 	public String getTopic() {
 		return topic;
 	}
-	
+
 	public ID getTopicID() {
 		return topicID;
 	}
-	
-	public void addToContainer(ISharedObjectContainer soContainer) throws SharedObjectAddException {
-		soContainer.getSharedObjectManager().addSharedObject(topicID, this, null);
+
+	public void addToContainer(ISharedObjectContainer soContainer)
+			throws SharedObjectAddException {
+		soContainer.getSharedObjectManager().addSharedObject(topicID, this,
+				null);
 	}
 
 	public void removeFromContainer(ISharedObjectContainer soContainer) {
 		soContainer.getSharedObjectManager().removeSharedObject(getTopicID());
 	}
-	
+
 	public ServiceRegistration register(Properties props) {
-		if (props == null) props = new Properties();
+		if (props == null)
+			props = new Properties();
 		props.put(EventConstants.EVENT_TOPIC, getTopic());
-		return this.context.registerService(EventAdmin.class.getName(), this, props);
+		return this.context.registerService(EventAdmin.class.getName(), this,
+				props);
 	}
-	
+
 	public ServiceRegistration register() {
 		return register(null);
 	}
-	
+
 	public void dispose() {
-		if (eventHandlerServiceTracker != null) {
-			eventHandlerServiceTracker.close();
-			eventHandlerServiceTracker = null;
+		if (this.eventHandlerTracker != null) {
+			this.eventHandlerTracker.close();
+		}
+		if (this.log != null) {
+			this.log.close();
 		}
 		topic = null;
 		topicID = null;
 		executor = null;
 	}
-	
+
 	public void postEvent(final Event event) {
 		try {
 			sendSharedObjectMsgTo(null, SharedObjectMsg.createMsg(
 					"handlePostEvent", new DistributedEventAdminMessage(event)));
-			localPostEvent(event);
 		} catch (IOException e) {
-			logError("postEvent exception event=" + event + " not sent.", e);
+			logError(NLS.bind(
+					"IOException posting distributed event {0} to {1}", event,
+					topic), e);
 		}
+		localDispatch(event, true);
 	}
 
 	public void sendEvent(Event event) {
-		try {
-			sendSharedObjectMsgTo(null, SharedObjectMsg.createMsg(
-					"handleSendEvent", new DistributedEventAdminMessage(event)));
-			localSendEvent(event);
-		} catch (IOException e) {
-			logError("sendEvent exception event=" + event + " not sent.", e);
-		}
-	}
-
-	private ServiceReference[] getEventHandlerServiceReferences(Event event) {
-		synchronized (eventHandlerSTLock) {
-			if (eventHandlerServiceTracker == null) {
-				eventHandlerServiceTracker = new ServiceTracker(context,
-						EventHandler.class.getName(), null);
-				eventHandlerServiceTracker.open();
-			}
-			ServiceReference[] refs = eventHandlerServiceTracker
-					.getServiceReferences();
-			List results = new ArrayList();
-			if (refs == null)
-				return null;
-			for (int i = 0; i < refs.length; i++) {
-				String eventFilter = (String) refs[i]
-						.getProperty(EventConstants.EVENT_FILTER);
-				Filter filter = null;
-				if (eventFilter != null) {
-					try {
-						filter = context.createFilter(eventFilter);
-						if (event.matches(filter))
-							results.add(refs[i]);
-					} catch (InvalidSyntaxException e) {
-						logError("getEventHandlers eventFilter=" + eventFilter,
-								e);
-						continue;
-					}
-				} else
-					results.add(refs[i]);
-			}
-			return (ServiceReference[]) results
-					.toArray(new ServiceReference[] {});
-		}
+		localDispatch(event, false);
 	}
 
 	protected boolean handleSharedObjectMsg(SharedObjectMsg msg) {
@@ -164,75 +137,95 @@ public class DistributedEventAdmin extends BaseSharedObject implements EventAdmi
 		return false;
 	}
 
-	private void localSendEvent(Event event) {
-		ServiceReference[] eventHandlerRefs = getEventHandlerServiceReferences(event);
-		if (eventHandlerRefs == null) {
-			logWarning("sendEvent event=" + event
-					+ ".  No service references found to post to.");
+	protected void localDispatch(Event event, boolean isAsync) {
+		IExecutor exec = executor;
+		if (exec == null)
 			return;
+
+		if (event == null) {
+			log.log(LogService.LOG_ERROR,
+					"Null event passed to EventAdmin was ignored.");
+			// continue from here will result in an NPE below; the spec for
+			// EventAdmin does not allow for null here
 		}
+
+		String eventTopic = event.getTopic();
+
+		try {
+			SecurityManager sm = System.getSecurityManager();
+			if (sm != null)
+				sm.checkPermission(new TopicPermission(topic,
+						TopicPermission.PUBLISH));
+		} catch (SecurityException e) {
+			String msg = NLS
+					.bind(
+							"Caller bundle does not have TopicPermission to publish topic {0}",
+							eventTopic);
+			logError(msg, e);
+			// must throw a security exception here according to the EventAdmin
+			// spec
+			throw e;
+		}
+
+		Set eventHandlerWrappers = eventHandlerTracker.getHandlers(eventTopic);
+
+		SecurityManager sm = System.getSecurityManager();
+		Permission perm = (sm == null) ? null : new TopicPermission(topic,
+				TopicPermission.SUBSCRIBE);
+
 		// Now synchronously call every eventhandler
-		for (int i = 0; i < eventHandlerRefs.length; i++)
-			callSync(eventHandlerRefs[i], (EventHandler) context
-					.getService(eventHandlerRefs[i]), event);
+		if (isAsync)
+			fireAsync(exec, eventHandlerWrappers, event, perm);
+		else
+			callSync(eventHandlerWrappers, event, perm);
 
-	}
-
-	private void localPostEvent(Event event) {
-		ServiceReference[] refs = getEventHandlerServiceReferences(event);
-		if (refs == null) {
-			logWarning("localPostEvent event=" + event
-					+ ".  No service references found to post to.");
-			return;
-		}
-		for (int i = 0; i < refs.length; i++)
-			fireAsync(refs[i], (EventHandler) context.getService(refs[i]),
-					event);
 	}
 
 	void handlePostEvent(DistributedEventAdminMessage event) {
-		localPostEvent(event.getEvent());
+		localDispatch(event.getEvent(), true);
 	}
 
-	void handleSendEvent(DistributedEventAdminMessage event) {
-		localSendEvent(event.getEvent());
+	protected void callSync(Set eventHandlerWrappers, final Event event,
+			final Permission perm) {
+		for (Iterator i = eventHandlerWrappers.iterator(); i.hasNext();) {
+			final EventHandlerWrapper wrapper = (EventHandlerWrapper) i.next();
+			SafeRunner.run(new ISafeRunnable() {
+				public void handleException(Throwable exception) {
+					logError(
+							NLS
+									.bind(
+											"Exception while dispatching event {0} to handler {1}",
+											event, wrapper), exception);
+				}
+
+				public void run() throws Exception {
+					wrapper.handleEvent(event, perm);
+				}
+			});
+		}
 	}
 
-	protected void logWarning(String string) {
-		System.out.println(string);
+	protected void fireAsync(IExecutor exec, Set eventHandlerWrappers,
+			final Event event, final Permission perm) {
+		for (Iterator i = eventHandlerWrappers.iterator(); i.hasNext();) {
+			final EventHandlerWrapper wrapper = (EventHandlerWrapper) i.next();
+			exec.execute(new IProgressRunnable() {
+				public Object run(IProgressMonitor arg0) throws Exception {
+					wrapper.handleEvent(event, perm);
+					return null;
+				}
+			}, null);
+		}
 	}
 
-	protected void callSync(final ServiceReference serviceReference,
-			final EventHandler eventHandler, final Event event) {
-		SafeRunner.run(new ISafeRunnable() {
-			public void handleException(Throwable exception) {
-				logError(
-						"Exception in EventHandler.handleEvent. eventHandler="
-								+ eventHandler + ".  serviceReference="
-								+ serviceReference + ".  event=" + event,
-						exception);
-			}
-
-			public void run() throws Exception {
-				eventHandler.handleEvent(event);
-			}
-		});
-	}
-
-	protected void fireAsync(final ServiceReference serviceReference,
-			final EventHandler eventHandler, final Event event) {
-		executor.execute(new IProgressRunnable() {
-			public Object run(IProgressMonitor arg0) throws Exception {
-				eventHandler.handleEvent(event);
-				return null;
-			}
-		}, null);
-	}
-
-	protected void logError(String string, Throwable e) {
-		System.err.println(string);
-		if (e != null)
-			e.printStackTrace(System.err);
+	protected void logError(String message, Throwable exception) {
+		if (log != null) {
+			log.log(LogService.LOG_ERROR, message, exception);
+		} else {
+			System.err.println(message);
+			if (exception != null)
+				exception.printStackTrace(System.err);
+		}
 	}
 
 }
