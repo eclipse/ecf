899438de886b02c86c6fb57159dc2c4d3256b688
slewis <slewis>
2009-10-04 22:57:46 +0000

    Added copyright notice

--- providers/bundles/org.eclipse.ecf.provider.ui/src/org/eclipse/ecf/internal/provider/ui/Activator.java
+++ providers/bundles/org.eclipse.ecf.provider.ui/src/org/eclipse/ecf/internal/provider/ui/Activator.java
@@ -1,3 +1,13 @@
+/****************************************************************************
+ * Copyright (c) 2009 Composent, Inc., and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Scott Lewis <slewis@composent.com> - initial API and implementation
+ *****************************************************************************/
 package org.eclipse.ecf.internal.provider.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -13,7 +23,7 @@ public class Activator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static Activator plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -22,7 +32,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -31,7 +44,10 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
@@ -40,7 +56,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public synchronized static Activator getDefault() {
