52c0b738e2858204ac2f5c63193ada0f058b2341
hstaudacher <hstaudacher>
2009-10-06 08:21:10 +0000

    RESOLVED - bug 275722: [rest] REST abstraction for ECF https://bugs.eclipse.org/bugs/show_bug.cgi?id=275722

--- tests/bundles/org.eclipse.ecf.tests.remoteservice.rest/src/org/eclipse/ecf/tests/remoteservice/rest/RestResourceTest.java
+++ tests/bundles/org.eclipse.ecf.tests.remoteservice.rest/src/org/eclipse/ecf/tests/remoteservice/rest/RestResourceTest.java
@@ -1,37 +1,129 @@
 /******************************************************************************* 
-* Copyright (c) 2009 EclipseSource and others. All rights reserved. This
-* program and the accompanying materials are made available under the terms of
-* the Eclipse Public License v1.0 which accompanies this distribution, and is
-* available at http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*   EclipseSource - initial API and implementation
-*******************************************************************************/ 
+ * Copyright (c) 2009 EclipseSource and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   EclipseSource - initial API and implementation
+ *   Andre Dietisheim - declarative services tests
+ *******************************************************************************/
 package org.eclipse.ecf.tests.remoteservice.rest;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import junit.framework.TestCase;
 
-import org.eclipse.ecf.internal.remoteservice.rest.ResourceRepresentationFactory;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.ecf.remoteservice.rest.IRestResourceRepresentationFactory;
+import org.eclipse.ecf.remoteservice.rest.util.DSUtil;
+import org.eclipse.ecf.tests.remoteservice.Activator;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
 
 public class RestResourceTest extends TestCase {
-	
-	private ResourceRepresentationFactory resourceFactory;
+
+	private static final String BUNDLEID_ECFREST = "org.eclipse.ecf.remoteservice.rest";
+
+	private static final String BUNDLEID_DS = "org.eclipse.equinox.ds";
+	private IRestResourceRepresentationFactory resourceFactory;
+	private BundleContext context;
 
 	protected void setUp() throws Exception {
-		resourceFactory = ResourceRepresentationFactory.getDefault();
+		this.context = Activator.getDefault().getContext();
+		resourceFactory = getResourceFactory(context);
 	}
-	
+
 	public void testCreation() {
 		assertNotNull(resourceFactory);
 	}
-	
+
 	public void testResourceCreation() {
-		Object adapter = resourceFactory.getAdapter(List.class);
+		Object adapter = ((IAdaptable) resourceFactory).getAdapter(List.class);
 		assertTrue(adapter instanceof List);
-		List resources = (List) adapter;		
-		assertTrue(resources.size()>=1);
+		List resources = (List) adapter;
+		assertTrue(resources.size() >= 1);
 	}
 
+	public void testKnowsWhetherDSIsRunning()
+			throws InvalidSyntaxException, BundleException {
+		startBundle(BUNDLEID_DS);
+		assertTrue(DSUtil.isRunning(context));
+
+		stopBundle(BUNDLEID_DS);
+		assertFalse(DSUtil.isRunning(context));
+	}
+
+	private void startBundle(String bundleId) throws BundleException {
+		final Bundle bundle = Platform.getBundle(bundleId);
+		if (!isBundleActive(bundleId)) {
+			bundle.start();
+		}
+		waitForBundleState(bundle, Bundle.ACTIVE);
+	}
+
+	private void waitForBundleState(final Bundle bundle, final int bundleState) {
+		Runnable runnable = new Runnable() {
+			public void run() {
+				while (bundle.getState() != bundleState) {
+					System.err.println("waiting for state " + bundleState
+							+ ": current state = " + bundle.getState());
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Future future = executor.submit(Executors.callable(runnable));
+		// wait for bundle state
+		try {
+			future.get();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private boolean isBundleActive(String bundleId) throws BundleException {
+		Bundle bundle = Platform.getBundle(bundleId);
+		return bundle != null && bundle.getState() == Bundle.ACTIVE;
+	}
+
+	private void stopBundle(String bundleId) throws BundleException {
+		Bundle bundle = Platform.getBundle(bundleId);
+		if (isBundleActive(bundleId)) {
+			bundle.stop();
+		}
+		waitForBundleState(bundle, Bundle.RESOLVED);
+	}
+
+	private IRestResourceRepresentationFactory getResourceFactory(
+			BundleContext context) throws InvalidSyntaxException {
+		IRestResourceRepresentationFactory factory = null;
+		ServiceReference[] serviceReferences = context.getServiceReferences(
+				IRestResourceRepresentationFactory.class.getName(), null);
+		if (serviceReferences != null) {
+			for (int i = 0; i < serviceReferences.length; i++) {
+				ServiceReference serviceReference = serviceReferences[i];
+				if (BUNDLEID_ECFREST.equals(serviceReference.getBundle()
+						.getSymbolicName())) {
+					Object service = context.getService(serviceReference);
+					if (service instanceof IRestResourceRepresentationFactory) {
+						factory = (IRestResourceRepresentationFactory) service;
+						break;
+					}
+				}
+			}
+		}
+		return factory;
+	}
 }
