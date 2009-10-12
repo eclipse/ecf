72aa70ae386fddde53cc5bc224252ea593c90010
slewis <slewis>
2009-10-12 23:37:38 +0000

    Fix for externalized messages

--- providers/bundles/org.eclipse.ecf.provider.filetransfer.efs/src/org/eclipse/ecf/internal/provider/filetransfer/efs/Messages.java
+++ providers/bundles/org.eclipse.ecf.provider.filetransfer.efs/src/org/eclipse/ecf/internal/provider/filetransfer/efs/Messages.java
@@ -18,7 +18,6 @@ import org.eclipse.osgi.util.NLS;
  */
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.ecf.internal.provider.filetransfer.efs.messages"; //$NON-NLS-1$
-	public static String EFSRemoteFileSystemBrowseFactory_EXCEPTION_NOT_DIRECTORY;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
