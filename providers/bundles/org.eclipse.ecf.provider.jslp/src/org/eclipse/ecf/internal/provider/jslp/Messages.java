76b66b811ad0d3cd8cd35206b808d027964ea6b1
slewis <slewis>
2009-10-12 23:37:32 +0000

    Fix for externalized messages

--- providers/bundles/org.eclipse.ecf.provider.jslp/src/org/eclipse/ecf/internal/provider/jslp/Messages.java
+++ providers/bundles/org.eclipse.ecf.provider.jslp/src/org/eclipse/ecf/internal/provider/jslp/Messages.java
@@ -17,7 +17,6 @@ public final class Messages extends NLS {
 	public static String ContainerInstantiator_0;
 	public static String JSLPDiscoveryContainer_0;
 	public static String JSLPDiscoveryContainer_ContainerIsDisposed;
-	public static String JSLPDiscoveryJob_NO_JSLP_BUNDLE;
 	public static String JSLPDiscoveryJob_TITLE;
 	public static String JSLPNamespace_2;
 	public static String JSLPNamespace_3;
