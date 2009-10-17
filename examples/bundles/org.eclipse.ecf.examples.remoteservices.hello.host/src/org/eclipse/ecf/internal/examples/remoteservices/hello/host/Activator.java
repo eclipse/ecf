4104d84661a1051c9887f4e628bbb012555afdff
slewis <slewis>
2009-10-17 03:47:49 +0000

    Simplification/generalization of code and addition of activemq product.  Also moved product configuration files to products directory

--- examples/bundles/org.eclipse.ecf.examples.remoteservices.hello.host/src/org/eclipse/ecf/internal/examples/remoteservices/hello/host/Activator.java
+++ examples/bundles/org.eclipse.ecf.examples.remoteservices.hello.host/src/org/eclipse/ecf/internal/examples/remoteservices/hello/host/Activator.java
@@ -8,67 +8,42 @@
  * Contributors:
  *    Composent, Inc. - initial API and implementation
  *****************************************************************************/
+/****************************************************************************
+ * Copyright (c) 2009 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
 package org.eclipse.ecf.internal.examples.remoteservices.hello.host;
 
-import java.util.Properties;
-
-import org.eclipse.ecf.core.IContainerManager;
-import org.eclipse.ecf.examples.remoteservices.hello.IHello;
-import org.eclipse.ecf.examples.remoteservices.hello.impl.Hello;
-import org.eclipse.ecf.osgi.services.distribution.IDistributionConstants;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.util.tracker.ServiceTracker;
 
+public class Activator implements BundleActivator {
 
-public class Activator implements BundleActivator, IDistributionConstants {
+	private static BundleContext context;
 
-	private String containerType = System.getProperty("containerType","ecf.r_osgi.peer");
-	private BundleContext context;
-	private ServiceTracker containerManagerServiceTracker;
-	private ServiceRegistration helloRegistration;
-	
 	/*
 	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
 	 */
-	public void start(BundleContext context) throws Exception {
-		this.context = context;
-		// Create R-OSGi Container
-		IContainerManager containerManager = getContainerManagerService();
-		containerManager.getContainerFactory().createContainer(containerType);
-		
-		Properties props = new Properties();
-		// add OSGi service property indicating this
-		props.put(REMOTE_INTERFACES, REMOTE_INTERFACES_WILDCARD);
-		// register remote service
-		helloRegistration = context.registerService(IHello.class.getName(), new Hello(), props);
-		System.out.println("Host: Hello Service Registered");
+	public void start(BundleContext ctxt) throws Exception {
+		context = ctxt;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
-	 */
 	public void stop(BundleContext context) throws Exception {
-		if (helloRegistration != null) {
-			helloRegistration.unregister();
-			helloRegistration = null;
-		}
-		if (containerManagerServiceTracker != null) {
-			containerManagerServiceTracker.close();
-			containerManagerServiceTracker = null;
-		}
-		this.context = null;
+		context = null;
 	}
 
-	private IContainerManager getContainerManagerService() {
-		if (containerManagerServiceTracker == null) {
-			containerManagerServiceTracker = new ServiceTracker(context, IContainerManager.class.getName(),null);
-			containerManagerServiceTracker.open();
-		}
-		return (IContainerManager) containerManagerServiceTracker.getService();
+	public static BundleContext getContext() {
+		return context;
 	}
 
 }
