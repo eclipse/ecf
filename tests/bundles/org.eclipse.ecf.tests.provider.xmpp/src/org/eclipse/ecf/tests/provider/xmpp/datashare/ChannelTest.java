b2cfa521b8f619de3490bd9ee229297ec35e1389
slewis <slewis>
2009-10-28 19:20:13 +0000

    Added ChannelTest

---
+++ tests/bundles/org.eclipse.ecf.tests.provider.xmpp/src/org/eclipse/ecf/tests/provider/xmpp/datashare/ChannelTest.java
@@ -0,0 +1,137 @@
+/****************************************************************************
+ * Copyright (c) 2004 Composent, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.ecf.tests.provider.xmpp.datashare;
+
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.identity.IDCreateException;
+import org.eclipse.ecf.core.identity.IDFactory;
+import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.datashare.IChannel;
+import org.eclipse.ecf.datashare.IChannelContainerAdapter;
+import org.eclipse.ecf.datashare.IChannelListener;
+import org.eclipse.ecf.datashare.events.IChannelEvent;
+import org.eclipse.ecf.datashare.events.IChannelMessageEvent;
+import org.eclipse.ecf.tests.ContainerAbstractTestCase;
+import org.ecllpse.ecf.tests.provider.xmpp.XMPP;
+
+public class ChannelTest extends ContainerAbstractTestCase {
+
+	/**
+	 * 
+	 */
+	private static final String CHANNEL_NAME = "channel";
+
+	private static final int MESSAGE_COUNT = 5;
+
+	private ID channelID;
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.ecf.tests.presence.AbstractPresenceTestCase#getClientContainerName()
+	 */
+	protected String getClientContainerName() {
+		return XMPP.CONTAINER_NAME;
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		setClientCount(2);
+		clients = createClients();
+		connectClients();
+		channelID = IDFactory.getDefault().createStringID(CHANNEL_NAME);
+		for (int i = 0; i < clientCount; i++) {
+			final IChannelContainerAdapter channelContainer = getChannelContainer(i);
+			channelContainer.createChannel(channelID, getIChannelListener(getContainerID(i)), null);
+		}
+	}
+
+	protected ID getServerConnectID(int client) {
+		final IContainer container = getClient(client);
+		final Namespace connectNamespace = container.getConnectNamespace();
+		final String username = getUsername(client);
+		try {
+			return IDFactory.getDefault().createID(connectNamespace, username);
+		} catch (final IDCreateException e) {
+			fail("Could not create server connect ID");
+			return null;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		cleanUpClients();
+		super.tearDown();
+	}
+
+	public void testSendMessage() throws Exception {
+		final IChannel ch0 = getChannelContainer(0).getChannel(channelID);
+		assertNotNull(ch0);
+		ch0.sendMessage(getClient(1).getConnectedID(), new String("hello").getBytes());
+		sleep(3000);
+	}
+
+	public void testBiSendMessage() throws Exception {
+		final IChannel ch0 = getChannelContainer(0).getChannel(channelID);
+		final IChannel ch1 = getChannelContainer(1).getChannel(channelID);
+		ch0.sendMessage(getClient(1).getConnectedID(), new String("hello").getBytes());
+		ch1.sendMessage(getClient(0).getConnectedID(), new String("hello").getBytes());
+		sleep(3000);
+	}
+
+
+	public void testSendMessages() throws Exception {
+		final IChannel ch0 = getChannelContainer(0).getChannel(channelID);
+		for(int i=0; i < MESSAGE_COUNT; i++) {
+			ch0.sendMessage(getClient(1).getConnectedID(), new String("hello.  msg#="+i).getBytes());
+			sleep(500);
+		}
+		sleep(3000);
+	}
+
+	public void testBiSendMessages() throws Exception {
+		final IChannel ch0 = getChannelContainer(0).getChannel(channelID);
+		final IChannel ch1 = getChannelContainer(1).getChannel(channelID);
+		for(int i=0; i < MESSAGE_COUNT; i++) {
+			ch0.sendMessage(getClient(1).getConnectedID(), new String("hello.  msg#="+i).getBytes());
+			sleep(500);
+			ch1.sendMessage(getClient(0).getConnectedID(), new String("hello.  msg#="+i).getBytes());
+		}
+		sleep(3000);
+	}
+
+	protected IChannelListener getIChannelListener(final ID containerid) throws Exception {
+		return new IChannelListener() {
+			public void handleChannelEvent(IChannelEvent event) {
+				if (event instanceof IChannelMessageEvent) {
+					IChannelMessageEvent cme = (IChannelMessageEvent) event;
+					System.out.println("receivercontainerid="+containerid+"; fromcontainerid="+cme.getFromContainerID()+"; channelid="+cme.getChannelID());
+					System.out.println("   event="+event);
+					System.out.println("   message="+new String(cme.getData()));
+				}
+			}
+		};
+	}
+
+	private ID getContainerID(int clientIndex) {
+		return getClients()[clientIndex].getID();
+	}
+
+	private IChannelContainerAdapter getChannelContainer(int clientIndex) {
+		return (IChannelContainerAdapter) getClients()[clientIndex].getAdapter(IChannelContainerAdapter.class);
+	}
+
+
+}
