c863b478712c91c0d666802964dad42be7b008d9
Markus Alexander Kuppe <bugs.eclipse.org@lemmster.de>
2010-10-26 21:21:57 +0200

    RESOLVED - bug 328759: IDFactory(Service)Test fail in automated test environment https://bugs.eclipse.org/bugs/show_bug.cgi?id=328759

--- tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/core/identity/IDFactoryTest.java
+++ tests/bundles/org.eclipse.ecf.tests/src/org/eclipse/ecf/tests/core/identity/IDFactoryTest.java
@@ -11,80 +11,17 @@
 
 package org.eclipse.ecf.tests.core.identity;
 
-import java.util.List;
-
-import org.eclipse.ecf.core.identity.GUID;
 import org.eclipse.ecf.core.identity.IDFactory;
-import org.eclipse.ecf.core.identity.IIDFactory;
-import org.eclipse.ecf.core.identity.LongID;
-import org.eclipse.ecf.core.identity.Namespace;
-import org.eclipse.ecf.core.identity.StringID;
 
-public class IDFactoryTest extends NamespaceTest {
-
-	IIDFactory fixture = null;
-
+public class IDFactoryTest extends IIDNamespaceTest {
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see junit.framework.TestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp();
 		fixture = IDFactory.getDefault();
 		assertNotNull(fixture);
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see junit.framework.TestCase#tearDown()
-	 */
-	protected void tearDown() throws Exception {
-		super.tearDown();
-		fixture = null;
-		assertNull(fixture);
-	}
-
-	public void testIDFactoryGetNamespaces() {
-		List namespaces = fixture.getNamespaces();
-		assertNotNull(namespaces);
-		assertTrue(namespaces.size() > 0);
-	}
-
-	public void testIDFactoryGetStringIDNamespaceByName() {
-		Namespace namespace = fixture.getNamespaceByName(StringID.class
-				.getName());
-		assertNotNull(namespace);
-	}
-
-	public void testIDFactoryGetGUIDIDNamespaceByName() {
-		Namespace namespace = fixture.getNamespaceByName(GUID.class.getName());
-		assertNotNull(namespace);
-	}
-
-	public void testIDFactoryGetLongIDNamespaceByName() {
-		Namespace namespace = fixture
-				.getNamespaceByName(LongID.class.getName());
-		assertNotNull(namespace);
-	}
-
-	public void testIDFactoryAddNamespace() {
-		Namespace namespace = createNamespace();
-		int currentSize = fixture.getNamespaces().size();
-		Namespace ns = fixture.addNamespace(namespace);
-		assertNull(ns);
-		assertTrue(fixture.getNamespaces().size() == (currentSize + 1));
-	}
-
-	public void testIDFactoryRemoveNamespace() {
-		Namespace namespace = fixture.getNamespaceByName(StringID.class
-				.getName());
-		int currentSize = fixture.getNamespaces().size();
-		Namespace removed = fixture.removeNamespace(namespace);
-		assertNotNull(removed);
-		assertTrue(fixture.getNamespaces().size() == (currentSize - 1));
-		// Put it back
-		fixture.addNamespace(removed);
 	}
 }
