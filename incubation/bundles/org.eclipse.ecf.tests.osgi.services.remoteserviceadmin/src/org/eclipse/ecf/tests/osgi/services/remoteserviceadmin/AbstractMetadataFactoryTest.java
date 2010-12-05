4c202badafe78c1061c4e7e1b2c3d2de88775f67
slewis@composent.com <slewis@composent.com>
2010-12-05 15:36:31 -0800

    rsa test refactorings

--- incubation/bundles/org.eclipse.ecf.tests.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/tests/osgi/services/remoteserviceadmin/AbstractMetadataFactoryTest.java
+++ incubation/bundles/org.eclipse.ecf.tests.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/tests/osgi/services/remoteserviceadmin/AbstractMetadataFactoryTest.java
@@ -98,7 +98,7 @@ public abstract class AbstractMetadataFactoryTest extends ECFAbstractTestCase {
 		Long remoteServiceId = createECFRemoteServiceId(props);
 		// Add extra properties
 		addExtraProperties(props);
-		return new EndpointDescription(props,containerID,remoteServiceId,null,null,null);
+		return new EndpointDescription(props,containerID.getNamespace().getName(),remoteServiceId,null,null,null);
 	}
 	
 	protected EndpointDescription createFullEndpointDescription() {
@@ -115,7 +115,7 @@ public abstract class AbstractMetadataFactoryTest extends ECFAbstractTestCase {
 		String rsFilter = createECFRSFilter(props);
 		// Add extra properties
 		addExtraProperties(props);
-		return new EndpointDescription(props,containerID, remoteServiceId,targetID,idFilter,rsFilter);
+		return new EndpointDescription(props,containerID.getNamespace().getName(), remoteServiceId,targetID,idFilter,rsFilter);
 	}
 
 	protected void addExtraProperties(Map<String, Object> props) {
@@ -130,7 +130,7 @@ public abstract class AbstractMetadataFactoryTest extends ECFAbstractTestCase {
 		ID containerID = createECFContainerID(props);
 		Long remoteServiceId = createECFRemoteServiceId(props);
 		// This should throw a runtime exception 
-		return new EndpointDescription(props,containerID,remoteServiceId.longValue(),null,null,null);
+		return new EndpointDescription(props,containerID.getNamespace().getName(),remoteServiceId.longValue(),null,null,null);
 	}
 	
 	protected EndpointDescription createBadECFEndpointDescrption() throws Exception {
