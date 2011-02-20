c6ae9f7e159d96d9eb2f94ffda1a51332cad6c5d
slewis <slewis@composent.com>
2011-02-20 08:36:52 -0800

    Removal of mistakenly released experimental code.

--- framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/server/IRemoteServiceServerProvider.java
+++
@@ -1,5 +0,0 @@
-package org.eclipse.ecf.remoteservice.server;
-
-public interface IRemoteServiceServerProvider {
-	// No methods for this root interface.  Subinterfaces (e.g. for REST) will declare
-}
