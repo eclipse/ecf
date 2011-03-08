be8228a120c82ea7f7cab13e46a26ef529f8b36b
Wim Jongman <wim.jongman@remainsoftware.com>
2011-03-08 11:23:42 +0100

    [zoodiscovery] Remote service undiscovered and not rediscovered due to concurrency bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=337973
    Fixed ReadRoot to better support concurrency and added some
    documentation to IService which is not related to this bug.
    
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- providers/bundles/org.eclipse.ecf.provider.zookeeper/src/org/eclipse/ecf/provider/zookeeper/core/internal/IService.java
+++ providers/bundles/org.eclipse.ecf.provider.zookeeper/src/org/eclipse/ecf/provider/zookeeper/core/internal/IService.java
@@ -12,23 +12,45 @@
 package org.eclipse.ecf.provider.zookeeper.core.internal;
 
 import org.eclipse.ecf.discovery.IServiceInfo;
+import org.eclipse.ecf.discovery.identity.IServiceTypeID;
 
 public interface IService extends IServiceInfo {
 
-	String LOCATION = "discovery.service.location"; //$NON-NLS-1$	
-	String WEIGHT = "discovery.service.weight"; //$NON-NLS-1$
-	String PRIORITY = "discovery.service.priority"; //$NON-NLS-1$
-	String PROTOCOLS = "discovery.service.protocol"; //$NON-NLS-1$
-	Object SERVICE_NAME = "discovery.service.name";;
-
-	// Properties getProperties();
+	/**
+	 * Holds the service location ( {@link IServiceInfo#getLocation()} ) in the
+	 * zooKeeper node
+	 **/
+	String LOCATION = "discovery.service.location"; //$NON-NLS-1$ 
 
 	/**
-	 * Stored as value of a zookeeper node
+	 * Holds the service weight ( {@link IServiceInfo#getWeight()} ) in the
+	 * zooKeeper node
+	 **/
+	String WEIGHT = "discovery.service.weight"; //$NON-NLS-1$
+
+	/**
+	 * Holds the service priority ({@link IServiceInfo#getPriority()()} ) in the
+	 * zooKeeper node
+	 **/
+	String PRIORITY = "discovery.service.priority"; //$NON-NLS-1$
+
+	/**
+	 * Holds the service-type protocols ({@link IServiceTypeID#getProtocols()} )
+	 * in the zooKeeper node
+	 **/
+	String PROTOCOLS = "discovery.service.protocol"; //$NON-NLS-1$
+
+	/**
+	 * Holds the service name ({@link IServiceInfo#getServiceName()}) in the
+	 * zooKeeper node
+	 **/
+	String SERVICE_NAME = "discovery.service.name"; //$NON-NLS-1$
+
+	/**
+	 * The byte representation of the service properties, appropriate to be
+	 * stored in the zooKeeper node
 	 * 
-	 * @return byte value of string
+	 * @return byte representation of the properties
 	 */
 	byte[] getPropertiesAsBytes();
-
-	// String getPropertiesAsString();
 }
