af351781639f9798e185a329186970771ee24680
slewis <slewis@composent.com>
2011-08-24 09:38:17 -0700

    Proposed fix/workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=353835

--- providers/bundles/org.eclipse.ecf.provider.zookeeper/src/org/eclipse/ecf/provider/zookeeper/node/internal/WatchManager.java
+++ providers/bundles/org.eclipse.ecf.provider.zookeeper/src/org/eclipse/ecf/provider/zookeeper/node/internal/WatchManager.java
@@ -75,25 +75,21 @@ public class WatchManager implements BundleStoppingListener {
 		nodeWriter.publish();
 	}
 
-	public synchronized void publish(final AdvertisedService published) {
+	public void publish(AdvertisedService published) {
 		Assert.isNotNull(published);
 		String serviceid = published.getServiceID().getServiceTypeID()
 				.getInternal();
-		if (WatchManager.this.getNodeWriters().containsKey(serviceid))
+		if (getNodeWriters().containsKey(serviceid))
 			return;
-		if (!WatchManager.this.writeRootLock.isOpen()) {
-			synchronized (WatchManager.this.writeRootLock) {
-				try {
-					/* wait for the server to get ready */
-					WatchManager.this.writeRootLock.wait();
-				} catch (InterruptedException e) {
-					Logger.log(LogService.LOG_DEBUG, e.getMessage(), e);
-				}
-			}
+		try {
+			/* wait for the server to get ready */
+			while (!writeRootLock.isOpen())
+				Thread.sleep(300);
+		} catch (InterruptedException e) {
+			Logger.log(LogService.LOG_DEBUG, e.getMessage(), e);
 		}
-		NodeWriter nodeWriter = new NodeWriter(published,
-				WatchManager.this.writeRoot);
-		WatchManager.this.getNodeWriters().put(serviceid, nodeWriter);
+		NodeWriter nodeWriter = new NodeWriter(published, writeRoot);
+		getNodeWriters().put(serviceid, nodeWriter);
 		allKnownServices.put(published.getServiceID().getName(), published);
 		nodeWriter.publish();
 	}
