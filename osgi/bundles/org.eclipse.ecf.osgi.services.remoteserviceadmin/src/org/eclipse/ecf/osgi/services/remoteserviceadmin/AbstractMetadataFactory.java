db2e04a7e0f8c3b99f78045c6d3768af3a54efdc
slewis <slewis@composent.com>
2013-05-19 01:14:53 -0700

    Fix for bug 408450

--- osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/osgi/services/remoteserviceadmin/AbstractMetadataFactory.java
+++ osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/osgi/services/remoteserviceadmin/AbstractMetadataFactory.java
@@ -48,11 +48,10 @@ public abstract class AbstractMetadataFactory {
 		result.setPropertyString(name, value.toString());
 	}
 
-	private static final Long DEFAULT_LONG = new Long(0);
-
+	private final Long DEFAULT_LONG = new Long(0);
+	
 	protected Long decodeLong(IServiceProperties props, String name) {
-		// This is for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=408450
-		Object o = props.getPropertyString(name);
+		Object o = props.getProperty(name);
 		if (o == null)
 			return DEFAULT_LONG;
 		if (o instanceof Long)
