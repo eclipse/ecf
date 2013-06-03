c0e1027fecabb723788f9ed2c0c0de80b08a68c9
slewis <slewis@composent.com>
2013-06-02 19:56:40 -0700

    Removed deprecated update manager code.

--- server-side/examples/bundles/org.eclipse.ecf.examples.updatesite.client/src/org/eclipse/ecf/internal/examples/updatesite/client/UpdateSiteServiceAccessHandler.java
+++ server-side/examples/bundles/org.eclipse.ecf.examples.updatesite.client/src/org/eclipse/ecf/internal/examples/updatesite/client/UpdateSiteServiceAccessHandler.java
@@ -12,39 +12,13 @@
 
 package org.eclipse.ecf.internal.examples.updatesite.client;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import org.eclipse.core.commands.*;
-import org.eclipse.ecf.discovery.IServiceInfo;
-import org.eclipse.ecf.discovery.ui.DiscoveryHandlerUtil;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.update.internal.ui.UpdateUI;
-import org.eclipse.update.internal.ui.model.SiteBookmark;
-import org.eclipse.update.internal.ui.model.UpdateModel;
-import org.eclipse.update.ui.UpdateManagerUI;
 
 public class UpdateSiteServiceAccessHandler extends AbstractHandler {
-	private static final String NAME = "name"; //$NON-NLS-1$
-
 	/* (non-Javadoc)
 	 * @see org.eclipse.core.commands.AbstractHandler#execute(org.eclipse.core.commands.ExecutionEvent)
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IServiceInfo serviceInfo = DiscoveryHandlerUtil.getActiveIServiceInfoChecked(event);
-
-		try {
-			final URI location = serviceInfo.getLocation();
-			final String name = serviceInfo.getServiceProperties().getPropertyString(NAME);
-
-			final UpdateModel model = UpdateUI.getDefault().getUpdateModel();
-			final SiteBookmark bookmark = new SiteBookmark(name, location.toURL(), false);
-			bookmark.setSelected(true);
-			model.addBookmark(bookmark);
-			model.saveBookmarks();
-			UpdateManagerUI.openInstaller(Display.getDefault().getActiveShell());
-		} catch (MalformedURLException e) {
-			throw new ExecutionException(e.getMessage(), e);
-		}
-		return null;
+		throw new ExecutionException("UpdateSiteServiceAccessHandler no longer supported"); //$NON-NLS-1$
 	}
 }
