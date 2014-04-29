3d17306256336bee5d0ea37160afeae03e727124
slewis <slewis@composent.com>
2014-04-28 19:55:15 -0700

    For Raspberry Pi demo, changed jmdns out for jslp, fixed .sh startup script (for Rpi/Linux)
    Change-Id: Ib8cdf381a02f2b3eda5fcc33b0be8d8828ae2994

--- examples/bundles/org.eclipse.ecf.examples.raspberrypi.management.host.feature/rootfiles/rpimgmthost.sh
+++ examples/bundles/org.eclipse.ecf.examples.raspberrypi.management.host.feature/rootfiles/rpimgmthost.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 JAVAPROG=java
-GENERICHOSTNAME=localhost
-GENERICPORT=3288
 if [ "$#" -eq  "0" ];then
- echo "No arguments supplied. Usage:  rpimgmthost.sh <hostname> [<port]"
+ echo "No arguments supplied. Usage:  rpimgmthost.sh hostname port"
  exit 1
-elif [ "$#" -eq "1" ]
-then
-   GENERICHOSTNAME=$1
-   echo "Generic Server Hostname set to ${GENERICHOSTNAME}"
 fi
+GENERICHOSTNAME="${1}"
+if [ "${2}" != "" ];then
+  GENERICPORT="${2}"
+else
+  GENERICPORT=3288
+fi
+echo "Hostname: ${GENERICHOSTNAME}"
+echo "Port: ${GENERICPORT}"
 JAVAPROPS="-Decf.generic.server.hostname=${GENERICHOSTNAME} -Decf.generic.server.port=${GENERICPORT} -Declipse.ignoreApp=true -Dosgi.noShutdown=true"
 echo "javaprops=${JAVAPROPS}"
 EQUINOXJAR=plugins/org.eclipse.osgi_3.10.0.v20140407-2102.jar
-echo "equinoxjar=${EQUINOXJAR}"
-ARGS="-configuration file:configuration -os linux -ws gtk -arch arm -console -consoleLog -debug"
-echo "program args=${ARGS}"
-echo "------------executing java-----------"
-${JAVAPROG} ${JAVAPROPS} -jar ${EQUINOXJAR} ${ARGS}
+echo "equinox=${EQUINOXJAR}"
+PROGARGS="-configuration file:configuration -os linux -ws gtk -arch arm -console -consoleLog -debug"
+${JAVAPROG} ${JAVAPROPS} -jar ${EQUINOXJAR} ${PROGARGS}
 
