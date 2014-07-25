aaf3b53146391ce925c49210008d42252b526d61
slewis <slewis@composent.com>
2014-07-24 18:58:48 -0700

    Initial checkin of new examples: com.mycorp.examples.timeservice.host.generic.auth (host), and com.mycorp.examples.timeservice.consumer.ds.generic.auth (consumer/client).  These are intended to address enhancement
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=439577
    
    Change-Id: Ic78e3d9cf332ab8632d3717b509271f39a30e3bc

---
+++ examples/bundles/com.mycorp.examples.timeservice.consumer.ds.generic.auth/src/com/mycorp/examples/timeservice/consumer/ds/generic/auth/GenericAuthConsumerContainerSelector.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Scott Lewis - initial API and implementation
+ ******************************************************************************/
+package com.mycorp.examples.timeservice.consumer.ds.generic.auth;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.ecf.core.ContainerTypeDescription;
+import org.eclipse.ecf.core.IContainer;
+import org.eclipse.ecf.core.identity.ID;
+import org.eclipse.ecf.core.security.IConnectContext;
+import org.eclipse.ecf.core.security.IConnectInitiatorPolicy;
+import org.eclipse.ecf.core.sharedobject.ISharedObjectContainerClient;
+import org.eclipse.ecf.osgi.services.remoteserviceadmin.ConsumerContainerSelector;
+import org.eclipse.ecf.osgi.services.remoteserviceadmin.SelectContainerException;
+import org.eclipse.ecf.remoteservice.IRemoteServiceContainer;
+
+public class GenericAuthConsumerContainerSelector extends
+		ConsumerContainerSelector {
+
+	public GenericAuthConsumerContainerSelector() {
+		super(true);
+	}
+
+	@Override
+	protected IRemoteServiceContainer createContainer(
+			ContainerTypeDescription containerTypeDescription,
+			String containerTypeDescriptionName,
+			@SuppressWarnings("rawtypes") Map properties)
+			throws SelectContainerException {
+		IRemoteServiceContainer result = super.createContainer(
+				containerTypeDescription, containerTypeDescriptionName,
+				properties);
+
+		ISharedObjectContainerClient client = (ISharedObjectContainerClient) result
+				.getContainer().getAdapter(ISharedObjectContainerClient.class);
+		if (client != null) {
+			client.setConnectInitiatorPolicy(new IConnectInitiatorPolicy() {
+				public void refresh() {
+				}
+
+				public Object createConnectData(IContainer container,
+						ID targetID, IConnectContext context) {
+					// This is called when the client.connect method
+					// is called. Here is our chance to send the username
+					// and password to the server.
+					return getConnectData();
+				}
+
+				public int getConnectTimeout() {
+					return 30000;
+				}
+			});
+		}
+		return result;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	Object getConnectData() {
+		Map results = new HashMap();
+		// Get system properties values, if present
+		String username = System.getProperty("myUsername");
+		if (username != null) {
+			results.put("username",username);
+			String password = System.getProperty("myPassword");
+			if (password != null)
+				results.put("password",password);
+		}
+		if (results.size() > 0)
+			return results;
+		return null;
+	}
+}
---
+++ examples/bundles/com.mycorp.examples.timeservice.host.generic.auth/src/com/mycorp/examples/timeservice/host/generic/auth/TimeServiceImpl.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Composent, Inc. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors: Scott Lewis - initial API and implementation
+ ******************************************************************************/
+package com.mycorp.examples.timeservice.host.generic.auth;
+
+import com.mycorp.examples.timeservice.ITimeService;
+
+public class TimeServiceImpl implements ITimeService {
+
+	/**
+	 * Implementation of my time service. 
+	 */
+	public Long getCurrentTime() {
+		// Print out to host std out that a call to this service was received.
+		System.out.println("TimeServiceImpl.  Received call to getCurrentTime()");
+		// Eventually, this should (e.g.) contact NIST time server and return more
+		// accurate time.  For the time being, we will return the System time for
+		// this host.
+		return new Long(System.currentTimeMillis());
+	}
+
+}
