3372edaf2c278e6b5b04a795c30a23c209791fee
slewis <slewis@composent.com>
2015-07-02 15:04:09 -0700

    Small wording change in remote service host output
    Change-Id: I5f0251287021f1ebaf3c78c77e8af68a8f336241

--- examples/bundles/com.mycorp.examples.timeservice.host/src/com/mycorp/examples/timeservice/host/TimeServiceImpl.java
+++ examples/bundles/com.mycorp.examples.timeservice.host/src/com/mycorp/examples/timeservice/host/TimeServiceImpl.java
@@ -17,7 +17,7 @@ public class TimeServiceImpl implements ITimeService {
 	 */
 	public Long getCurrentTime() {
 		// Print out to host std out that a call to this service was received.
-		System.out.println("TimeServiceImpl.  Received call to getCurrentTime()");
+		System.out.println("TimeServiceImpl:  Received call to getCurrentTime()");
 		// Eventually, this should (e.g.) contact NIST time server and return more
 		// accurate time.  For the time being, we will return the System time for
 		// this host.
