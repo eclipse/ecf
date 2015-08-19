6697574371461fa8cd34063231fae144be5b11b4
slewis <slewis@composent.com>
2015-08-19 15:44:57 -0700

    Improved design of remote service provider API
    Change-Id: Ic4c7d2316ae4e055649a4f2b364e73d437ae30f3

--- framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/provider/RemoteServiceDistributionProvider.java
+++ framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/provider/RemoteServiceDistributionProvider.java
@@ -1,8 +1,10 @@
 package org.eclipse.ecf.remoteservice.provider;
 
 import java.util.Dictionary;
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.ecf.core.ContainerTypeDescription;
 import org.eclipse.ecf.core.identity.Namespace;
+import org.eclipse.ecf.core.provider.IContainerInstantiator;
 
 /**
  * Basic implementation of IRemoteServiceDistributionProvider.  Intended to be subclassed by distribution
@@ -10,9 +12,78 @@ import org.eclipse.ecf.core.identity.Namespace;
  * 
  * @since 8.7
  */
-public abstract class RemoteServiceDistributionProvider implements IRemoteServiceDistributionProvider {
+public class RemoteServiceDistributionProvider implements IRemoteServiceDistributionProvider {
 
-	public abstract ContainerTypeDescription createContainerTypeDescription();
+	private String name;
+	private IContainerInstantiator instantiator;
+	private String description;
+	private boolean server;
+	private boolean hidden;
+
+	protected RemoteServiceDistributionProvider(String name, IContainerInstantiator instantiator) {
+		setName(name).setInstantiator(instantiator);
+	}
+
+	protected String getName() {
+		return this.name;
+	}
+
+	protected RemoteServiceDistributionProvider setName(String name) {
+		Assert.isNotNull(name);
+		this.name = name;
+		return this;
+	}
+
+	protected IContainerInstantiator getInstantiator() {
+		return instantiator;
+	}
+
+	protected RemoteServiceDistributionProvider setInstantiator(IContainerInstantiator instantiator) {
+		Assert.isNotNull(instantiator);
+		this.instantiator = instantiator;
+		return this;
+	}
+
+	protected String getDescription() {
+		return this.description;
+	}
+
+	protected RemoteServiceDistributionProvider setDescription(String desc) {
+		this.description = desc;
+		return this;
+	}
+
+	protected boolean isServer() {
+		return this.server;
+	}
+
+	protected RemoteServiceDistributionProvider setServer(boolean server) {
+		this.server = server;
+		return this;
+	}
+
+	protected boolean isHidden() {
+		return this.hidden;
+	}
+
+	protected RemoteServiceDistributionProvider setHidden(boolean hidden) {
+		this.hidden = hidden;
+		return this;
+	}
+
+	public void validateComplete() throws NullPointerException {
+		String ctdName = getName();
+		if (ctdName == null)
+			throw new NullPointerException("Container type description name cannot be null"); //$NON-NLS-1$
+		IContainerInstantiator ctdInstantiator = getInstantiator();
+		if (ctdInstantiator == null)
+			throw new NullPointerException("Container type description instantiator cannot be null"); //$NON-NLS-1$
+	}
+
+	public ContainerTypeDescription createContainerTypeDescription() {
+		validateComplete();
+		return new ContainerTypeDescription(getName(), getInstantiator(), getDescription(), isServer(), isHidden());
+	}
 
 	public Dictionary<String, ?> getContainerTypeDescriptionProperties() {
 		return null;
