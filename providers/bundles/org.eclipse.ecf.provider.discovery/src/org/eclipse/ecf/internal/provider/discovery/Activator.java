604ba18cec07b47cb604d91ae69814383d49067a
slewis <slewis@composent.com>
2016-01-07 20:59:08 -0800

    Disabled org.eclipse.ecf.provider.discovery bundle for testing.
    Change-Id: Ie3b50b69e0a2c904084bcfae1a09fddacf678726

--- providers/bundles/org.eclipse.ecf.provider.discovery/src/org/eclipse/ecf/internal/provider/discovery/Activator.java
+++ providers/bundles/org.eclipse.ecf.provider.discovery/src/org/eclipse/ecf/internal/provider/discovery/Activator.java
@@ -10,20 +10,8 @@
  ******************************************************************************/
 package org.eclipse.ecf.internal.provider.discovery;
 
-import java.util.HashSet;
-import java.util.Properties;
-import org.eclipse.core.runtime.SafeRunner;
-import org.eclipse.ecf.core.ContainerConnectException;
-import org.eclipse.ecf.core.ContainerTypeDescription;
-import org.eclipse.ecf.core.identity.Namespace;
-import org.eclipse.ecf.core.util.ExtensionRegistryRunnable;
-import org.eclipse.ecf.core.util.Trace;
-import org.eclipse.ecf.discovery.IDiscoveryAdvertiser;
-import org.eclipse.ecf.discovery.IDiscoveryLocator;
-import org.eclipse.ecf.discovery.service.IDiscoveryService;
-import org.eclipse.ecf.provider.discovery.*;
-import org.osgi.framework.*;
-import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 	// The shared instance
@@ -51,90 +39,81 @@ public class Activator implements BundleActivator {
 	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(final BundleContext context) throws Exception {
-
-		SafeRunner.run(new ExtensionRegistryRunnable(context) {
-			protected void runWithoutRegistry() throws Exception {
-				context.registerService(Namespace.class, new CompositeNamespace(), null);
-				context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container", true, false), null); //$NON-NLS-1$ //$NON-NLS-2$
-				context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.singleton.discovery", new SingletonDiscoveryContainerInstantiator(), "Composite Discovery Container Locator", true, false), null); //$NON-NLS-1$ //$NON-NLS-2$
-				context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite.locator", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container Locator"), null); //$NON-NLS-1$ //$NON-NLS-2$
-				context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite.advertiser", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container Advertiser"), null); //$NON-NLS-1$ //$NON-NLS-2$
-			}
-		});
-
-		final Properties props = new Properties();
-		props.put(IDiscoveryLocator.CONTAINER_NAME, CompositeDiscoveryContainer.NAME);
-		props.put(Constants.SERVICE_RANKING, new Integer(1000));
-		String[] clazzes = new String[] {IDiscoveryService.class.getName(), IDiscoveryLocator.class.getName(), IDiscoveryAdvertiser.class.getName()};
-		context.registerService(clazzes, new ServiceFactory() {
-
-			/* (non-Javadoc)
-			 * @see org.osgi.framework.ServiceFactory#getService(org.osgi.framework.Bundle, org.osgi.framework.ServiceRegistration)
-			 */
-			public Object getService(final Bundle bundle, final ServiceRegistration registration) {
-
-				// register the composite discovery service)
-				final CompositeDiscoveryContainer cdc;
-				cdc = new CompositeDiscoveryContainer(new HashSet());
-				try {
-					cdc.connect(null, null);
-				} catch (final ContainerConnectException e) {
-					Trace.catching(Activator.PLUGIN_ID, Activator.PLUGIN_ID + "/debug/methods/catching", this.getClass(), "getService(Bundle, ServiceRegistration)", e); //$NON-NLS-1$ //$NON-NLS-2$
-					return null;
-				}
-
-				Filter filter = null;
-				// add a service listener to add/remove IDS dynamically 
-				try {
-					final String filter2 = "(&(" + Constants.OBJECTCLASS + "=" + IDiscoveryAdvertiser.class.getName() + ")(!(" + IDiscoveryLocator.CONTAINER_NAME + "=" + CompositeDiscoveryContainer.NAME + ")))"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
-					filter = context.createFilter(filter2);
-					context.addServiceListener(new ServiceListener() {
-						/* (non-Javadoc)
-						 * @see org.osgi.framework.ServiceListener#serviceChanged(org.osgi.framework.ServiceEvent)
-						 */
-						public void serviceChanged(final ServiceEvent arg0) {
-							final Object anIDS = context.getService(arg0.getServiceReference());
-							switch (arg0.getType()) {
-								case ServiceEvent.REGISTERED :
-									cdc.addContainer(anIDS);
-									break;
-								case ServiceEvent.UNREGISTERING :
-									cdc.removeContainer(anIDS);
-									break;
-								default :
-									break;
+		/*
+				SafeRunner.run(new ExtensionRegistryRunnable(context) {
+					protected void runWithoutRegistry() throws Exception {
+						context.registerService(Namespace.class, new CompositeNamespace(), null);
+						context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container", true, false), null); //$NON-NLS-1$ //$NON-NLS-2$
+						context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.singleton.discovery", new SingletonDiscoveryContainerInstantiator(), "Composite Discovery Container Locator", true, false), null); //$NON-NLS-1$ //$NON-NLS-2$
+						context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite.locator", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container Locator"), null); //$NON-NLS-1$ //$NON-NLS-2$
+						context.registerService(ContainerTypeDescription.class, new ContainerTypeDescription("ecf.discovery.composite.advertiser", new CompositeDiscoveryContainerInstantiator(), "Composite Discovery Container Advertiser"), null); //$NON-NLS-1$ //$NON-NLS-2$
+					}
+				});
+		
+				final Properties props = new Properties();
+				props.put(IDiscoveryLocator.CONTAINER_NAME, CompositeDiscoveryContainer.NAME);
+				props.put(Constants.SERVICE_RANKING, new Integer(1000));
+				String[] clazzes = new String[] {IDiscoveryService.class.getName(), IDiscoveryLocator.class.getName(), IDiscoveryAdvertiser.class.getName()};
+				context.registerService(clazzes, new ServiceFactory() {
+		
+					public Object getService(final Bundle bundle, final ServiceRegistration registration) {
+		
+						// register the composite discovery service)
+						final CompositeDiscoveryContainer cdc;
+						cdc = new CompositeDiscoveryContainer(new HashSet());
+						try {
+							cdc.connect(null, null);
+						} catch (final ContainerConnectException e) {
+							Trace.catching(Activator.PLUGIN_ID, Activator.PLUGIN_ID + "/debug/methods/catching", this.getClass(), "getService(Bundle, ServiceRegistration)", e); //$NON-NLS-1$ //$NON-NLS-2$
+							return null;
+						}
+		
+						Filter filter = null;
+						// add a service listener to add/remove IDS dynamically 
+						try {
+							final String filter2 = "(&(" + Constants.OBJECTCLASS + "=" + IDiscoveryAdvertiser.class.getName() + ")(!(" + IDiscoveryLocator.CONTAINER_NAME + "=" + CompositeDiscoveryContainer.NAME + ")))"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+							filter = context.createFilter(filter2);
+							context.addServiceListener(new ServiceListener() {
+								public void serviceChanged(final ServiceEvent arg0) {
+									final Object anIDS = context.getService(arg0.getServiceReference());
+									switch (arg0.getType()) {
+										case ServiceEvent.REGISTERED :
+											cdc.addContainer(anIDS);
+											break;
+										case ServiceEvent.UNREGISTERING :
+											cdc.removeContainer(anIDS);
+											break;
+										default :
+											break;
+									}
+								}
+		
+							}, filter2);
+						} catch (final InvalidSyntaxException e) {
+							// nop
+						}
+		
+						// get all previously registered IDS from OSGi (but not this one)
+						final ServiceTracker tracker = new ServiceTracker(context, filter, null);
+						tracker.open();
+						final Object[] services = tracker.getServices();
+						tracker.close();
+						if (services != null) {
+							for (int i = 0; i < services.length; i++) {
+								final Object obj = services[i];
+								if (obj != cdc)
+									cdc.addContainer(obj);
 							}
 						}
-
-					}, filter2);
-				} catch (final InvalidSyntaxException e) {
-					// nop
-				}
-
-				// get all previously registered IDS from OSGi (but not this one)
-				final ServiceTracker tracker = new ServiceTracker(context, filter, null);
-				tracker.open();
-				final Object[] services = tracker.getServices();
-				tracker.close();
-				if (services != null) {
-					for (int i = 0; i < services.length; i++) {
-						final Object obj = services[i];
-						if (obj != cdc)
-							cdc.addContainer(obj);
+		
+						return cdc;
 					}
-				}
-
-				return cdc;
-			}
-
-			/* (non-Javadoc)
-			 * @see org.osgi.framework.ServiceFactory#ungetService(org.osgi.framework.Bundle, org.osgi.framework.ServiceRegistration, java.lang.Object)
-			 */
-			public void ungetService(final Bundle bundle, final ServiceRegistration registration, final Object service) {
-				((CompositeDiscoveryContainer) service).dispose();
-			}
-		}, props);
-
+		
+					public void ungetService(final Bundle bundle, final ServiceRegistration registration, final Object service) {
+						((CompositeDiscoveryContainer) service).dispose();
+					}
+				}, props);
+		*/
 	}
 
 	/*
