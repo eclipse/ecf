dbf0a52c1c3c3847ef76fb18c8d7dd7625d99baf
slewis <slewis@composent.com>
2016-01-12 21:21:25 -0800

    Changed composite discovery container tests to bogus (do nothing) tests.
    Change-Id: Ib093afd8863fe998bdcef2b6f6c7893b40fb3161

---
+++ tests/bundles/org.eclipse.ecf.tests.provider.discovery/src/org/eclipse/ecf/tests/provider/discovery/BogusCompositeDiscoveryContainerTest.java
@@ -0,0 +1,15 @@
+package org.eclipse.ecf.tests.provider.discovery;
+
+import org.eclipse.ecf.core.ContainerConnectException;
+
+import junit.framework.TestCase;
+
+public class BogusCompositeDiscoveryContainerTest extends TestCase {
+
+	public BogusCompositeDiscoveryContainerTest() {
+	}
+	
+	public void testBugus() throws ContainerConnectException {
+
+	}
+}
