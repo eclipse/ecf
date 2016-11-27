75de721dc6066228f2925be1a6bb5b6677462e3a
slewis <slewis@composent.com>
2016-11-26 17:32:34 -0800

    Fix to use ds annotations for example.
    Change-Id: I5ed8bbc78ba694d8d56b774bca5c572f530ef327

--- examples/bundles/com.mycorp.examples.timeservice.consumer.ds.async/src/com/mycorp/examples/timeservice/consumer/ds/async/TimeServiceComponentAsync.java
+++ examples/bundles/com.mycorp.examples.timeservice.consumer.ds.async/src/com/mycorp/examples/timeservice/consumer/ds/async/TimeServiceComponentAsync.java
@@ -10,15 +10,26 @@ package com.mycorp.examples.timeservice.consumer.ds.async;
 
 import java.util.concurrent.CompletableFuture;
 
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+
 import com.mycorp.examples.timeservice.ITimeServiceAsync;
 
+@Component(immediate=true)
 public class TimeServiceComponentAsync {
 
+	@Reference(cardinality=ReferenceCardinality.AT_LEAST_ONE,policy=ReferencePolicy.DYNAMIC)
 	void bindTimeService(ITimeServiceAsync timeService) {
 		System.out.println("Discovered ITimeServiceAsync via DS");
 		// Get the CompletableFuture...no blocking here
 		CompletableFuture<Long> cf = timeService.getCurrentTimeAsync();
 		// print out time when done...no blocking anywhere!
 		cf.thenAccept((time) -> System.out.println("Remote time is: " + time));
+	}
+	
+	void unbindTimeService(ITimeServiceAsync timeService) {
+		System.out.println("Undiscovered ITimeServiceAsync via DS");
 	}
 }
