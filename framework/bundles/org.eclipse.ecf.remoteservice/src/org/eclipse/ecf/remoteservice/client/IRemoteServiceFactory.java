a4b1eb1b0e7fb18ee67f7ece8015f89656e98fa1
slewis <slewis@composent.com>
2017-03-13 20:23:56 -0700

    Added documentation
    Change-Id: I1492e896640cde665b7d98fe1136084884eb1487

--- framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/client/IRemoteServiceFactory.java
+++ framework/bundles/org.eclipse.ecf.remoteservice/src/org/eclipse/ecf/remoteservice/client/IRemoteServiceFactory.java
@@ -3,6 +3,8 @@ package org.eclipse.ecf.remoteservice.client;
 import org.eclipse.ecf.remoteservice.IRemoteService;
 
 /**
+ * Create a remote service instance for a given RemoteServiceClientRegistration.
+ * 
  * @since 8.12
  */
 public interface IRemoteServiceFactory {
