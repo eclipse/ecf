a2c6a6429af436a2e30749f75c8a9f88fca9fa45
slewis <slewis@composent.com>
2018-03-09 11:06:54 -0800

    Moved target platform to oxygen for tycho-based build.   Also added access to org.osgi.util.promise package (in oxygen only) to support osgi.async intent (R7)
    Change-Id: I0000000000000000000000000000000000000000

--- framework/bundles/org.eclipse.ecf.remoteservice.asyncproxy.j8/src/org/eclipse/ecf/remoteservice/asyncproxy/AbstractAsyncProxyRemoteService.java
+++ framework/bundles/org.eclipse.ecf.remoteservice.asyncproxy.j8/src/org/eclipse/ecf/remoteservice/asyncproxy/AbstractAsyncProxyRemoteService.java
@@ -10,10 +10,14 @@
  *****************************************************************************/
 package org.eclipse.ecf.remoteservice.asyncproxy;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Future;
 
 import org.eclipse.equinox.concurrent.future.IFuture;
+import org.osgi.util.promise.Deferred;
+import org.osgi.util.promise.Promise;
 
 public abstract class AbstractAsyncProxyRemoteService {
 
@@ -25,11 +29,27 @@ public abstract class AbstractAsyncProxyRemoteService {
 	
 	protected abstract void callCompletableAsync(AbstractAsyncProxyRemoteCall call, IAsyncProxyCompletable completable);
 
+	protected boolean isReturnAsync(Object proxy, Method method, Object[] args) {
+		@SuppressWarnings("rawtypes")
+		Class returnType = method.getReturnType();
+		return (CompletableFuture.class.isAssignableFrom(returnType) || CompletionStage.class.isAssignableFrom(returnType) ||
+				Future.class.isAssignableFrom(returnType) || IFuture.class.isAssignableFrom(returnType));
+	}
+	
 	@SuppressWarnings("unchecked")
 	protected Object callFuture(AbstractAsyncProxyRemoteCall call, @SuppressWarnings("rawtypes") Class returnType) {
+		if (Promise.class.isAssignableFrom(returnType)) {
+			@SuppressWarnings("rawtypes")
+			Deferred d = new Deferred();
+			callCompletableAsync(call, (r,hadException,exception) -> {
+				if (hadException) d.fail(exception);
+				else d.resolve(r);
+			});
+			return d.getPromise();
+		}
 		// If the result value is a CompletableFuture then
 		// we callCompletableAsync
-		if (CompletableFuture.class.isAssignableFrom(returnType)) {
+		if (CompletableFuture.class.isAssignableFrom(returnType) || CompletionStage.class.isAssignableFrom(returnType)) {
 			@SuppressWarnings("rawtypes")
 		    CompletableFuture result = new CompletableFuture();
 			callCompletableAsync(call, (r,hadException,exception) -> {
