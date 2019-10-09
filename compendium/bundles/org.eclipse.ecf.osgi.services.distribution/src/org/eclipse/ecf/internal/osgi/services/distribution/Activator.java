beac2d63527012932d01c3fcbd6b32c21ca6dc21
slewis <slewis@composent.com>
2019-10-08 22:14:27 -0700

    Fix for bug https://bugs.eclipse.org/bugs/show_bug.cgi?id=551944
    Change-Id: Iefa1af6a247071d85b59348c678e3adb0a19f102

---
+++ compendium/bundles/org.eclipse.ecf.osgi.services.distribution/src/org/eclipse/ecf/internal/osgi/services/distribution/Activator.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Composent, Inc. and others. All rights reserved. This
+ * program and the accompanying materials are made available under the terms of
+ * the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Composent, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.ecf.internal.osgi.services.distribution;
+
+import org.eclipse.ecf.core.util.BundleStarter;
+import org.eclipse.ecf.osgi.services.remoteserviceadmin.ITopologyManager;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.framework.hooks.service.EventListenerHook;
+import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
+
+public class Activator implements BundleActivator {
+
+	private static final String[] DEPENDENTS = new String[] { "org.eclipse.ecf", //$NON-NLS-1$
+			"org.eclipse.ecf.osgi.services.remoteserviceadmin" }; //$NON-NLS-1$
+	private BasicTopologyManager topologyManager;
+	private ServiceRegistration topologyManagerRegistration;
+
+	public void start(BundleContext context) throws Exception {
+		BundleStarter.startDependents(context, DEPENDENTS, Bundle.RESOLVED | Bundle.STARTING);
+		this.topologyManager = new BasicTopologyManager();
+		this.topologyManager.start(context);
+		this.topologyManagerRegistration = context.registerService(new String[] { EventListenerHook.class.getName(),
+				RemoteServiceAdminListener.class.getName(), ITopologyManager.class.getName() }, this.topologyManager,
+				null);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		if (topologyManagerRegistration != null) {
+			topologyManagerRegistration.unregister();
+			topologyManagerRegistration = null;
+			this.topologyManager.deactivate();
+			this.topologyManager = null;
+		}
+	}
+
+}
--- compendium/bundles/org.eclipse.ecf.osgi.services.distribution/src/org/eclipse/ecf/internal/osgi/services/distribution/BasicTopologyManager.java
+++ compendium/bundles/org.eclipse.ecf.osgi.services.distribution/src/org/eclipse/ecf/internal/osgi/services/distribution/BasicTopologyManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Composent, Inc. and others. All rights reserved. This
+ * Copyright (c) 2019 Composent, Inc. and others. All rights reserved. This
  * program and the accompanying materials are made available under the terms of
  * the Eclipse Public License v1.0 which accompanies this distribution, and is
  * available at http://www.eclipse.org/legal/epl-v10.html
@@ -9,27 +9,30 @@
  ******************************************************************************/
 package org.eclipse.ecf.internal.osgi.services.distribution;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.eclipse.ecf.osgi.services.remoteserviceadmin.ITopologyManager;
 import org.eclipse.ecf.osgi.services.remoteserviceadmin.TopologyManager;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.hooks.service.EventListenerHook;
-import org.osgi.service.component.annotations.Activate;
-import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.remoteserviceadmin.RemoteServiceAdminListener;
 
-@Component(immediate = true)
 public class BasicTopologyManager extends TopologyManager
 		implements EventListenerHook, RemoteServiceAdminListener, ITopologyManager {
 
-	@Activate
+	public void start(BundleContext context) throws Exception {
+		activate(context, new HashMap<String, Object>());
+	}
+
 	@Override
 	protected void activate(BundleContext context, Map<String, ?> properties) throws Exception {
 		super.activate(context, properties);
 	}
 
-	@Deactivate
+	public void stop() {
+		deactivate();
+	}
+
 	@Override
 	protected void deactivate() {
 		super.deactivate();
