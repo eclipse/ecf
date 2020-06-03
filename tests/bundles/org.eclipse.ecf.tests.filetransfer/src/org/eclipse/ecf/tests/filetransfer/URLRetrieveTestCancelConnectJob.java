2d0a5fee7e94dcb55aa588209b85311cc43a08d9
Mat Booth <mat.booth@redhat.com>
2020-06-04 00:00:20 +0100

    Bug 560602 - Switch to EPL version 2.0
    Update license headers in test bundles
    
    Change-Id: I61726a06e725f6d3581329cea27b78fcb72995a5
    Signed-off-by: Mat Booth <mat.booth@redhat.com>

--- tests/bundles/org.eclipse.ecf.tests.filetransfer/src/org/eclipse/ecf/tests/filetransfer/URLRetrieveTestCancelConnectJob.java
+++ tests/bundles/org.eclipse.ecf.tests.filetransfer/src/org/eclipse/ecf/tests/filetransfer/URLRetrieveTestCancelConnectJob.java
@@ -1,11 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2009 IBM, and others. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/****************************************************************************
+ * Copyright (c) 2009 IBM, and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors: IBM Corporation - initial API and implementation
- ******************************************************************************/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *****************************************************************************/
 /**
  * Removed from test suite by slewis on 5/30/2019 because it is not relevant for
  * now default httpclient45 provider
--- tests/bundles/org.eclipse.ecf.tests.provider.jslp/src/org/eclipse/ecf/tests/provider/jslp/JSLPDiscoveryWithoutRegTest.java
+++ tests/bundles/org.eclipse.ecf.tests.provider.jslp/src/org/eclipse/ecf/tests/provider/jslp/JSLPDiscoveryWithoutRegTest.java
@@ -1,13 +1,15 @@
-/*******************************************************************************
+/****************************************************************************
  * Copyright (c) 2009 Markus Alexander Kuppe.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
  *
  * Contributors:
  *     Markus Alexander Kuppe (ecf-dev_eclipse.org <at> lemmster <dot> de) - initial API and implementation
- ******************************************************************************/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *****************************************************************************/
 package org.eclipse.ecf.tests.provider.jslp;
 
 import org.eclipse.ecf.provider.jslp.container.JSLPDiscoveryContainer;
