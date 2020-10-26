530e927de33f2fc12875fcba54980af82e3af21b
slewis <slewis@composent.com>
2020-10-26 13:20:03 -0700

    Fix for https://bugs.eclipse.org/bugs/show_bug.cgi?id=568265
    Change-Id: Ia0d4343654b317638f2be3548de1660f088f6037

--- osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/osgi/services/remoteserviceadmin/EndpointDescriptionReader.java
+++ osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/osgi/services/remoteserviceadmin/EndpointDescriptionReader.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import org.eclipse.ecf.internal.osgi.services.remoteserviceadmin.DebugOptions;
 import org.eclipse.ecf.internal.osgi.services.remoteserviceadmin.EndpointDescriptionParser;
 import org.eclipse.ecf.internal.osgi.services.remoteserviceadmin.LogUtility;
+import org.eclipse.ecf.internal.osgi.services.remoteserviceadmin.PropertiesUtil;
 
 /**
  * Default implementation of {@link IEndpointDescriptionReader}.
@@ -28,32 +29,51 @@ import org.eclipse.ecf.internal.osgi.services.remoteserviceadmin.LogUtility;
  */
 public class EndpointDescriptionReader implements IEndpointDescriptionReader {
 
-	public org.osgi.service.remoteserviceadmin.EndpointDescription[] readEndpointDescriptions(
-			InputStream input) throws IOException {
+	public org.osgi.service.remoteserviceadmin.EndpointDescription[] readEndpointDescriptions(InputStream input)
+			throws IOException {
+		return readAndMergeEndpointDescriptions(input, null);
+	}
+
+	/**
+	 * @since 4.7
+	 */
+	public org.osgi.service.remoteserviceadmin.EndpointDescription[] readAndMergeEndpointDescriptions(InputStream ins,
+			Map<String, Object> mergeProperties) throws IOException {
 		// First create parser
 		EndpointDescriptionParser parser = new EndpointDescriptionParser();
 		// Parse input stream
-		parser.parse(input);
+		parser.parse(ins);
 		// Get possible endpoint descriptions
-		List<EndpointDescriptionParser.EndpointDescription> parsedDescriptions = parser
-				.getEndpointDescriptions();
+		List<EndpointDescriptionParser.EndpointDescription> parsedDescriptions = parser.getEndpointDescriptions();
 		List<org.osgi.service.remoteserviceadmin.EndpointDescription> results = new ArrayList<org.osgi.service.remoteserviceadmin.EndpointDescription>();
 		// For each one parsed, get properties and
 		for (EndpointDescriptionParser.EndpointDescription ed : parsedDescriptions) {
-			Map parsedProperties = ed.getProperties();
+			Map<String, Object> parsedProperties = ed.getProperties();
+			Map<String, Object> mergedProperties = null;
+			if (mergeProperties != null) {
+				mergedProperties = mergeWithParsed(parsedProperties, mergeProperties);
+			} else {
+				mergedProperties = parsedProperties;
+			}
 			try {
-				results.add(new EndpointDescription(parsedProperties));
+				results.add(new EndpointDescription(mergedProperties));
 			} catch (Exception e) {
-				LogUtility.logError(
-						"readEndpointDescriptions", //$NON-NLS-1$
-						DebugOptions.ENDPOINT_DESCRIPTION_READER,
-						this.getClass(),
+				LogUtility.logError("readEndpointDescriptions", //$NON-NLS-1$
+						DebugOptions.ENDPOINT_DESCRIPTION_READER, this.getClass(),
 						"Exception parsing endpoint description properties", e); //$NON-NLS-1$
 				throw new IOException("Error creating endpoint description: " //$NON-NLS-1$
 						+ e.getMessage());
 			}
 		}
 		return results.toArray(new EndpointDescription[results.size()]);
+	}
+
+	/**
+	 * @since 4.7
+	 */
+	protected Map<String, Object> mergeWithParsed(Map<String, Object> parsedProperties,
+			Map<String, Object> mergeProperties) {
+		return PropertiesUtil.mergeProperties(parsedProperties, mergeProperties);
 	}
 
 }
