a79af9cebee714413e99da263a78065d8b065598
slewis <slewis@composent.com>
2021-01-01 13:58:26 -0800

    Removed unused import
    Change-Id: I7fefbe105507f09e86b2efd6e99b6a2e129a9224

--- osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/internal/osgi/services/remoteserviceadmin/PropertiesUtil.java
+++ osgi/bundles/org.eclipse.ecf.osgi.services.remoteserviceadmin/src/org/eclipse/ecf/internal/osgi/services/remoteserviceadmin/PropertiesUtil.java
@@ -25,7 +25,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 import java.util.TreeMap;
 
 import org.eclipse.ecf.osgi.services.remoteserviceadmin.RemoteConstants;
