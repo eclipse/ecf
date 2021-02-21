e6e4a241fafb61cd18d4fa8ccebe1d938a3f07ca
slewis <slewis@composent.com>
2021-02-21 14:32:53 -0800

    Initial checkin of httpclient5 test bundle
    Change-Id: Ie09985155136e40ca318fc6ef1c620dce19c4f6a

---
+++ tests/bundles/org.eclipse.ecf.tests.filetransfer.httpclient5/src/org/eclipse/ecf/tests/filetransfer/httpclient5/HttpClient5TestSuite.java
@@ -0,0 +1,42 @@
+/****************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *    Red Hat, Inc. - initial API and implementation
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *****************************************************************************/
+
+package org.eclipse.ecf.tests.filetransfer.httpclient5;
+
+import org.eclipse.ecf.tests.filetransfer.FileBrowseTest;
+import org.eclipse.ecf.tests.filetransfer.FileIDFactoryTest;
+import org.eclipse.ecf.tests.filetransfer.FileSendTest;
+import org.eclipse.ecf.tests.filetransfer.GetRemoteFileNameTest;
+import org.eclipse.ecf.tests.filetransfer.NamespaceTest;
+import org.eclipse.ecf.tests.filetransfer.URIProtocolFactoryRetrieveTest;
+import org.eclipse.ecf.tests.filetransfer.URLBrowseTest;
+import org.eclipse.ecf.tests.filetransfer.URLCancelTest;
+import org.eclipse.ecf.tests.filetransfer.URLConnectionTestSuite;
+import org.eclipse.ecf.tests.filetransfer.URLPartialRetrieveTest;
+import org.eclipse.ecf.tests.filetransfer.URLRetrievePauseResumeTest;
+import org.eclipse.ecf.tests.filetransfer.URLRetrieveTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * A test suite that includes tests specific to httpclient filetransfer
+ * providers and is run against the httpclient45 provider.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ FileBrowseTest.class, FileIDFactoryTest.class, FileSendTest.class, GetRemoteFileNameTest.class,
+		NamespaceTest.class, URIProtocolFactoryRetrieveTest.class, URLBrowseTest.class, URLCancelTest.class,
+		URLPartialRetrieveTest.class, URLRetrievePauseResumeTest.class, URLRetrieveTest.class,
+		HttpClientGetPortFromURLTest.class })
+public class HttpClient5TestSuite extends URLConnectionTestSuite {
+
+}
---
+++ tests/bundles/org.eclipse.ecf.tests.filetransfer.httpclient5/src/org/eclipse/ecf/tests/filetransfer/httpclient5/HttpClientGetPortFromURLTest.java
@@ -0,0 +1,89 @@
+/****************************************************************************
+ * Copyright (c) 2012, 2020 Composent, Inc. and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * Contributors:
+ *    Composent, Inc. - initial API and implementation
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *****************************************************************************/
+
+package org.eclipse.ecf.tests.filetransfer.httpclient5;
+
+import org.eclipse.ecf.provider.filetransfer.httpclient5.HttpClientRetrieveFileTransfer;
+
+import junit.framework.TestCase;
+
+public class HttpClientGetPortFromURLTest extends TestCase {
+
+	public static class HttpClientRetrieveTest extends HttpClientRetrieveFileTransfer {
+		
+		public HttpClientRetrieveTest() {
+			super(null);
+		}
+
+		public static int getPortFromURLTest(String url) {
+			return getPortFromURL(url);
+		}
+	}
+	
+	public void testHttp() {
+		int port;
+		String url = "http://www.test.com";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(80, port);
+	}
+
+	public void testHttpWithPort() {
+		int port;
+		String url = "http://www.test.com:80/";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(80, port);
+	}
+
+	public void testHttps() {
+		int port;
+		String url = "https://www.test.com/";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(443, port);
+	}
+
+	public void testHttpsPortNOK() {
+		int port;
+		String url = "https://www.test.com:440/";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertFalse(443 == port);
+	}
+
+	public void testHttpWithSearchpart() {
+		int port;
+		String url = "https://www.test.com/test?value=yes";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(443, port);
+	}
+
+	public void testHttpWithCredentialsAndSearchpart() {
+		int port;
+		String url = "https://testuser:testpasswd@www.test.com/test?value=yes";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(443, port);
+	}
+
+	public void testHttpWithCredentialsPortSearchpart() {
+		int port;
+		String url = "http://testuser:testpasswd@www.test.com:8080/test?value=yes";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(8080, port);
+	}
+
+	public void testHttpWithCredentialsPortLongPath() {
+		int port;
+		String url = "https://testuser:testpasswd@host.domain.eu:443/pu/fisgui/svn/repository/fisgui/source-internal/trunk/target-platform/fisgui-5.8.1/content.xml";
+		port = HttpClientRetrieveTest.getPortFromURLTest(url);
+		assertEquals(443, port);
+	}
+
+}
