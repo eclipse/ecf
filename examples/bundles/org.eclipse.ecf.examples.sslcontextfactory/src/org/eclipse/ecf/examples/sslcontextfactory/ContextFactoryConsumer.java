8915e3b2e82c6afc6b8d721650f2d16a94ffa30f
Scott Lewis <scottslewis@gmail.com>
2025-02-20 15:55:28 -0800

    Enhanced the ContextFactoryConsumer

--- examples/bundles/org.eclipse.ecf.examples.sslcontextfactory/src/org/eclipse/ecf/examples/sslcontextfactory/ContextFactoryConsumer.java
+++ examples/bundles/org.eclipse.ecf.examples.sslcontextfactory/src/org/eclipse/ecf/examples/sslcontextfactory/ContextFactoryConsumer.java
@@ -1,35 +1,25 @@
 package org.eclipse.ecf.examples.sslcontextfactory;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-
-import javax.net.ssl.SSLContext;
-
 import org.eclipse.ecf.core.security.SSLContextFactory;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
 @Component(immediate=true)
 public class ContextFactoryConsumer {
 	
-	@Reference
+	private static SSLContextFactory factory;
+	
+	@Reference(cardinality = ReferenceCardinality.MANDATORY)
 	void bindSSLContextFactory(SSLContextFactory sslContextFactory) {
-		System.out.println("Got it "+ sslContextFactory);
-		// Now get PKIJoe SSLContext
-		try {
-			SSLContext sslContext = sslContextFactory.getInstance("TLS", "PKIJoe");
-			// # do stuff with sslContext here!
-			System.out.println("sslContext="+sslContext);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (NoSuchProviderException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		factory = sslContextFactory;
 	}
 	
 	void unbindSSLContextFactory(SSLContextFactory sslContextFactory) {
-		System.out.println("Ungot it "+ sslContextFactory);
+		factory = null;
+	}
+	
+	public static SSLContextFactory getSSLContextFactory() {
+		return factory;
 	}
 }
